f5d20e4211ea7d3b70011d0e95e685b942710c6c
eitangol <eitang@lunar-shots.com>
2023-08-15 14:03:58 +0300

    Update repo-sync.yml

---
+++ proxy/src/services/aggregation-output-plugin/remedy/runner.go
@@ -0,0 +1,51 @@
+package remedy
+
+import (
+	"errors"
+	"lunar/aggregation-plugin/common"
+	"lunar/aggregation-plugin/utils"
+	"lunar/toolkit-core/clock"
+
+	"github.com/rs/zerolog/log"
+)
+
+func Run(
+	state *State,
+	records []common.AccessLog,
+	tree common.SimpleURLTreeI,
+	clock clock.Clock,
+) error {
+	if len(records) == 0 {
+		return nil
+	}
+	combinedAggsToPersist := GetUpdatedAggregations(
+		*state.aggregation,
+		records,
+		tree,
+		clock,
+	)
+
+	log.Debug().Msgf("📦 [remedy-stats] Combined: %+v\n", combinedAggsToPersist)
+
+	err := state.UpdateAggregation(&combinedAggsToPersist)
+	if err != nil {
+		return errors.Join(common.ErrCouldNotDumpCombinedAgg, err)
+	}
+
+	return nil
+}
+
+func GetUpdatedAggregations(
+	currentAgg Aggregation,
+	records []common.AccessLog,
+	tree common.SimpleURLTreeI,
+	clock clock.Clock,
+) Aggregation {
+	accessLogs := []AccessLog{}
+	for _, record := range records {
+		accessLogs = append(accessLogs, AccessLog(record))
+	}
+	newAggs := ExtractAggFromBatch(accessLogs, tree, clock)
+	combinedAggs := utils.Combine(currentAgg, newAggs)
+	return combinedAggs
+}
