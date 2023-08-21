76843896e5e65f4c6aa6b3ff95433821a4a78257
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2023-08-21 15:31:02 +0300

    ResponseBasedThrottling Sbx: Retry After Counter + X-Lunar-Generated Rate (#49)

---
+++ proxy/src/libs/toolkit-core/configuration/yaml.go
@@ -0,0 +1,27 @@
+package configuration
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v3"
+)
+
+func DecodeYAML[T any](path string) (*T, error) {
+	data, readErr := os.ReadFile(path)
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	if log.Debug().Enabled() {
+		log.Debug().Msgf("Read raw YAML: %s", string(data))
+	}
+
+	var target T
+
+	if unmarshalErr := yaml.Unmarshal(data, &target); unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	return &target, nil
+}
