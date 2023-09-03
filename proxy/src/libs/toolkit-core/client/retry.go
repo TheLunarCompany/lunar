873d8f0a82c3daa80aa810b14fc83f93eeba627d
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2023-09-03 17:08:40 +0300

    Remedy Chaining Bugfix (Attempt #2) (#69)

---
+++ proxy/src/libs/toolkit-core/client/retry.go
@@ -0,0 +1,43 @@
+package client
+
+import (
+	"fmt"
+	"lunar/toolkit-core/clock"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+type RetryConfig struct {
+	Attempts           int
+	SleepMillis        int
+	WithInitialSleep   bool
+	InitialSleepMillis int
+	FailedAttemptLog   string
+	FailureLog         string
+}
+
+func WithRetry[T any](
+	clock clock.Clock,
+	config *RetryConfig,
+	f func() (T, error), //nolint:varnamelen
+) (T, error) {
+	if config.WithInitialSleep {
+		clock.Sleep(time.Duration(config.InitialSleepMillis) * time.Millisecond)
+	}
+	var err error
+	var res T
+	for i := 0; i < config.Attempts; i++ {
+		res, err = f()
+		if err != nil && config.FailedAttemptLog != "" {
+			log.Debug().Msgf("%s (error: %s). Attempt %d/%d",
+				config.FailedAttemptLog, err, i+1, config.Attempts)
+		} else if err == nil {
+			return res, nil
+		}
+		clock.Sleep(time.Duration(config.SleepMillis) * time.Millisecond)
+	}
+
+	log.Debug().Msgf("will return error")
+	return res, fmt.Errorf("%s, (error: %s)", config.FailureLog, err)
+}
