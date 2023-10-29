8411c2c0d2d1fbd0f966a17b1698f9b8d5fac06b
eitangol <108608289+eitangol@users.noreply.github.com>
2023-10-29 15:51:02 +0200

    Added requests to the pPython Interceptor (#113)
    * Added requests to the pPython Interceptor
    
    * Added a new tornado client tests for requests
    
    * Added the requests module to the docs

---
+++ proxy/src/services/lunar-engine/actions/response_action_transformers.go
@@ -0,0 +1,41 @@
+package actions
+
+import (
+	"lunar/engine/messages"
+	"lunar/engine/utils"
+	sharedActions "lunar/shared-model/actions"
+
+	spoe "github.com/TheLunarCompany/haproxy-spoe-go"
+)
+
+const ModifyResponseActionName = "modify_response"
+
+// ModifyResponseAction
+func (action *ModifyResponseAction) RespToSpoeActions() []spoe.Action {
+	actions := []spoe.Action{
+		spoe.ActionSetVar{
+			Name:  ModifyResponseActionName,
+			Scope: spoe.VarScopeResponse,
+			Value: true,
+		},
+		spoe.ActionSetVar{
+			Name:  ResponseHeadersActionName,
+			Scope: spoe.VarScopeResponse,
+			Value: utils.DumpHeaders(action.HeadersToSet),
+		},
+	}
+
+	return actions
+}
+
+func (action *ModifyResponseAction) RespRunResult() sharedActions.RemedyRespRunResult { //nolint:lll
+	return sharedActions.RespModifiedResponse
+}
+
+func (action *ModifyResponseAction) EnsureResponseIsUpdated(
+	onResponse *messages.OnResponse,
+) {
+	for name, value := range action.HeadersToSet {
+		onResponse.Headers[name] = value
+	}
+}
