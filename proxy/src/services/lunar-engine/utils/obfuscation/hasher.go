09aac445b2ee2767e1ff33017580ad9a97334f9a
AlexBLunar <142395966+AlexBLunar@users.noreply.github.com>
2023-11-26 15:02:26 +0200

    Fixing the graph to represent sent requests by header (#153)
    * Fixing the graph to represent sent requests by header

---
+++ proxy/src/services/lunar-engine/utils/obfuscation/hasher.go
@@ -0,0 +1,33 @@
+package obfuscation
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+type Hasher interface {
+	HashBytes(raw []byte) string
+}
+
+type MD5Hasher struct{}
+
+func (hasher MD5Hasher) HashBytes(raw []byte) string {
+	hash := md5.Sum(raw)
+	return hex.EncodeToString(hash[:])
+}
+
+type FixedHasher struct {
+	Value string
+}
+
+// The simplest, lossy hasher, used in tests
+func (hasher FixedHasher) HashBytes(_ []byte) string {
+	return hasher.Value
+}
+
+// Hasher which returns the input as-is
+type IdentityHasher struct{}
+
+func (hasher IdentityHasher) HashBytes(raw []byte) string {
+	return string(raw)
+}
