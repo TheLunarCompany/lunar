11154ad224812d16642e37bd62820ddddc571553
eitangol <108608289+eitangol@users.noreply.github.com>
2023-12-17 15:14:51 +0200

    Added Header based traffic filter (#165)
    * Added Header based traffic filter

---
+++ proxy/src/services/lunar-engine/utils/limit/rate_limit_state.go
@@ -0,0 +1,19 @@
+package limit
+
+import (
+	"time"
+)
+
+type RequestArguments struct {
+	LimiterID string
+	Grouping  Grouping
+	GroupID   GroupID
+}
+
+type IncrementableRateLimitState interface {
+	Increment(
+		requestArgs RequestArguments,
+		windowSize time.Duration,
+	) (int, error)
+	Counters() map[RequestArguments]int
+}
