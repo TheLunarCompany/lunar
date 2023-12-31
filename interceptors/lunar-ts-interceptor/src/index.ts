b8b94720dc35134c6bb4cad3afcf9809bbd0584c
eitangol <108608289+eitangol@users.noreply.github.com>
2023-12-31 11:22:21 +0200

    Mk 714 improve the logging on unsupported node js versions (#188)
    * Added more logs and version validation to NodeJS Interceptor

---
+++ interceptors/lunar-ts-interceptor/src/index.ts
@@ -0,0 +1,22 @@
+import { logger } from './logger'
+import { getInterceptor } from './interceptor'
+import { loadConnectionInformation, isEngineVersionSupported, getEngineVersion } from "./helper"
+
+const engineVersion = getEngineVersion()
+
+if (engineVersion === null) {
+    logger.error("Could not determine the version of NodeJS, Lunar Interceptor is disabled.")
+} else if (isEngineVersionSupported(engineVersion))
+{
+    const interceptor = getInterceptor()
+    // TODO: We should here make the handshake with the Proxy using makeProxyConnection() instead of loadConnectionInformation()
+    interceptor.setOptions(loadConnectionInformation())
+} else { // Unsupported node version.
+    logger.error(`
+    Lunar Interceptor could not be loaded because the installed Node.js version is unsupported. 
+        Your Node.js engine version: "${engineVersion.major}.${engineVersion.minor}.${engineVersion.minor}".
+        Please ensure that your Node.js version is within the supported range.\n
+        For a list of supported Node.js versions, refer to the Lunar Interceptor documentation:
+        https://docs.lunar.dev/installation-configuration/interceptors/#supported-languages
+        `)
+}
