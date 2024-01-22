7779ec6e46721a808a618d319eab28b3460c1e71
eitangol <108608289+eitangol@users.noreply.github.com>
2024-01-22 16:16:35 +0200

    Mk 700 web service signed in authentication flow (#217)
    * Initialisation for the auth
    
    * Added the creation and get for users
    
    * Update index.ts
    
    * Added routes and shutdown
    
    * Update ws.it.test.ts
    
    * Update ws.ts
    
    * Linter Fixes
    
    * Added test option
    
    * Update auth.ts
    
    * Update ws.it.test.ts
    
    * Linter fix
    
    * Added users create and get tests
    
    * Remove unneeded dep
    
    * Added middleware verification
    
    * Small adjustments
    
    * Remove secrets and added them to the git secrets
    
    * Update webapp-tests.yml
    
    * Test fixes
    
    * Removed secrets and unneeded lines
    
    * CR Fixes
    
    * CR Fixes
    
    * Small CR fixes
    
    * Small adjustment

---
+++ proxy/src/services/aggregation-output-plugin/remedy/persistence.model.go
@@ -0,0 +1,41 @@
+package remedy
+
+import (
+	sharedConfig "lunar/shared-model/config"
+)
+
+// The persisted aggregation model is similar to the aggregation model
+// but avoids structs as map keys, which are not supported in JSON.
+// They are highly useful and efficient in the aggregation model
+// since they play very well with the semigroup pattern.
+
+type Output struct {
+	RemedyStats       []OutputStats                `json:"remedy_stats"`
+	RemedyActionStats map[Action]OutputActionStats `json:"remedy_action_stats"`
+	MaxTime           string                       `json:"max_time"`
+	MinTime           string                       `json:"min_time"`
+}
+
+type Internal struct {
+	TotalCount Int `json:"total_count"`
+}
+type OutputStats struct {
+	Remedy                  sharedConfig.RemedyType       `json:"remedy"`
+	Action                  Action                        `json:"action"`
+	AffectedCount           Int                           `json:"affected_count"`
+	AffectedRatio           float32                       `json:"affected_ratio"`
+	AffectedStatsByEndpoint []OutputEndpointAffectedStats `json:"affected_stats_by_endpoint"` //nolint:lll
+}
+
+type OutputEndpointAffectedStats struct {
+	Method            string         `json:"method"`
+	URL               string         `json:"url"`
+	Count             Int            `json:"count"`
+	CountByStatusCode map[string]Int `json:"count_by_status_code"`
+}
+
+type OutputActionStats struct {
+	Count             Int                `json:"count"`
+	Ratio             float32            `json:"ratio"`
+	RatioByStatusCode map[string]float32 `json:"ratio_by_status_code"`
+}
