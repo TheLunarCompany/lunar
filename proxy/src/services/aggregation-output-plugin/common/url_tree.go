31dee67dd260e7e91824bb97867b48246ce100ce
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2024-01-24 17:32:05 +0200

    Webapp: uniqueness for `external_user_id` + seeding test user script (#224)

---
+++ proxy/src/services/aggregation-output-plugin/common/url_tree.go
@@ -0,0 +1,71 @@
+package common
+
+import (
+	"lunar/toolkit-core/configuration"
+	"lunar/toolkit-core/urltree"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+const policiesConfigEnvVar = "LUNAR_PROXY_POLICIES_CONFIG"
+
+type (
+	EmptyStruct    = struct{}
+	SimpleURLTree  = urltree.URLTree[EmptyStruct]
+	SimpleURLTreeI = urltree.URLTreeI[EmptyStruct]
+)
+
+func BuildTree(endpoints KnownEndpoints) (*SimpleURLTree, error) {
+	tree := urltree.NewURLTree[EmptyStruct]()
+	emptyStruct := EmptyStruct{}
+	for _, endpoint := range endpoints.Endpoints {
+		err := tree.Insert(endpoint.URL, &emptyStruct)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return tree, nil
+}
+
+func GetPoliciesPath() (string, error) {
+	path, pathErr := configuration.GetPathFromEnvVarOrDefault(
+		policiesConfigEnvVar,
+		"./policies.yaml",
+	)
+	if pathErr != nil {
+		return "", pathErr
+	}
+	return path, nil
+}
+
+func ReadKnownEndpoints() (*KnownEndpoints, error) {
+	path, pathErr := GetPoliciesPath()
+	if pathErr != nil {
+		return nil, pathErr
+	}
+
+	config, readErr := configuration.DecodeYAML[KnownEndpoints](path)
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	log.Debug().Msg("Loaded endpoints tree")
+
+	return config, nil
+}
+
+func GetPoliciesLastModifiedTime() (time.Time, error) {
+	path, pathErr := GetPoliciesPath()
+	if pathErr != nil {
+		return time.Time{}, pathErr
+	}
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return info.ModTime(), nil
+}
