1093bbaba84c3266fa3f5508fbcd95f9a1a3b61b
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-02-29 18:37:08 +0000

    Merge pull request #295 from TheLunarCompany/cloud-111-refactor-socket-store-to-update-proxies-and-interceptors-by
    CLOUD-111: refactor socket store to update proxies and interceptors by

---
+++ interceptors/lunar-ts-interceptor/src/lunarObjects.ts
@@ -0,0 +1,28 @@
+import { type RequestOptions, type request as httpRequest, type get as httpGet, IncomingMessage, ClientRequest } from "http"
+import { type request as httpsRequest, type get as httpsGet } from 'https'
+
+export interface LunarOptions extends RequestOptions {
+    href?: URL | null | undefined
+    pathname?: string | null | undefined
+}
+
+export class LunarClientRequest extends ClientRequest {
+    baseURL?: string | null = null
+    public lunarRetry!: (...args: unknown[]) => ClientRequest;
+    public lunarRetryOnError!: (...args: unknown[]) => ClientRequest;
+    public originalWrite!: NodeJS.WritableStream;
+    public originalEmit?: NodeJS.WritableStream["emit"];
+}
+
+export class LunarIncomingMessage extends IncomingMessage {
+    req!: LunarClientRequest
+}
+export interface OriginalFunctionMap {
+    request: typeof httpRequest | typeof httpsRequest,
+    get: typeof httpGet | typeof httpsGet,
+}
+
+export interface SchemeToFunctionsMap {
+    http: OriginalFunctionMap,
+    https: OriginalFunctionMap
+}
