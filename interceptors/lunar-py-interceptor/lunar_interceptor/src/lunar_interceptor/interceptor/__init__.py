5c35e39e6a6d7324a9b788645595bc337d817f54
eitangol <108608289+eitangol@users.noreply.github.com>
2024-03-13 10:45:13 +0200

    Core 1039 node interceptor dont run on lambda servers (#318)

---
+++ interceptors/lunar-py-interceptor/lunar_interceptor/src/lunar_interceptor/interceptor/__init__.py
@@ -0,0 +1,7 @@
+from lunar_interceptor.interceptor.interceptor import Interceptor
+from lunar_interceptor.interceptor.configuration import (
+    InterceptorConfig,
+    ENV_LUNAR_PROXY_HOST_KEY,
+)
+
+__all__ = ["Interceptor", "InterceptorConfig", "ENV_LUNAR_PROXY_HOST_KEY"]
