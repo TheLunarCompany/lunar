0e8748207df62fdda59fde03e0c7174444965bde
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-03-19 11:15:58 +0000

    Merge pull request #330 from TheLunarCompany/cloud-172-serve-assets-fonts-images-etc-through-cdn
    fix: add asset handling through CDN to UI.

---
+++ proxy/src/services/lunar-engine/services/remedies/account_orchestration_plugin.go
@@ -0,0 +1,97 @@
+package remedies
+
+import (
+	"fmt"
+	"lunar/engine/actions"
+	"lunar/engine/messages"
+	sharedConfig "lunar/shared-model/config"
+	"sync"
+
+	"github.com/rs/zerolog/log"
+)
+
+type AccountOrchestrationPlugin struct {
+	accountID int
+
+	mutex *sync.Mutex
+}
+
+func NewAccountOrchestrationPlugin() *AccountOrchestrationPlugin {
+	return &AccountOrchestrationPlugin{
+		accountID: 0,
+		mutex:     &sync.Mutex{},
+	}
+}
+
+func (plugin *AccountOrchestrationPlugin) OnRequest(
+	onRequest messages.OnRequest,
+	remedyConfig *sharedConfig.AccountOrchestrationConfig,
+	accounts map[sharedConfig.AccountID]sharedConfig.Account,
+) (actions.ReqLunarAction, error) {
+	var lunarAction actions.ReqLunarAction = &actions.NoOpAction{}
+	numAccounts := len(remedyConfig.RoundRobin)
+	if numAccounts == 0 {
+		err := fmt.Errorf("No accounts configured for orchestration")
+		return lunarAction, err
+	}
+
+	plugin.mutex.Lock()
+	currentAccountID := plugin.accountID % numAccounts
+	accountName := remedyConfig.RoundRobin[currentAccountID]
+	plugin.accountID = (plugin.accountID + 1) % numAccounts
+	plugin.mutex.Unlock()
+
+	account, found := accounts[accountName]
+	if !found {
+		err := fmt.Errorf("Account [%v] is not defined in the accounts section",
+			accountName)
+		return lunarAction, err
+	}
+	lunarAction = modifyRequestToUseAccount(onRequest, account, accounts)
+
+	return lunarAction, nil
+}
+
+func (plugin *AccountOrchestrationPlugin) OnResponse(
+	_ messages.OnResponse,
+	_ *sharedConfig.AccountOrchestrationConfig,
+) (actions.RespLunarAction, error) {
+	return &actions.NoOpAction{}, nil
+}
+
+func modifyRequestToUseAccount(
+	onRequestArgs messages.OnRequest,
+	accountToUse sharedConfig.Account,
+	accounts map[sharedConfig.AccountID]sharedConfig.Account,
+) actions.ReqLunarAction {
+	headers := map[string]string{}
+	for _, token := range accountToUse.Tokens {
+		if value, found := onRequestArgs.Headers[token.Header.Name]; found {
+			if value == token.Header.Value {
+				continue // Token already present in request
+			}
+
+			// Check if the token is from an unknown account
+			knownToken := false
+			for _, account := range accounts {
+				for _, token := range account.Tokens {
+					if value == token.Header.Value {
+						knownToken = true
+					}
+				}
+			}
+			if !knownToken {
+				log.Trace().
+					Msg("Unknown account token is already present in request")
+			}
+		}
+
+		headers[token.Header.Name] = token.Header.Value
+	}
+	if len(headers) == 0 {
+		return &actions.NoOpAction{}
+	}
+	return &actions.ModifyRequestAction{
+		HeadersToSet: headers,
+	}
+}
