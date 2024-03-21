0d5201e2079da4ba4c52857ebda79c30f3e15143
eitangol <108608289+eitangol@users.noreply.github.com>
2024-03-21 14:46:29 +0200

    Core 1035 streams filter (#334)
    * Streams Filter init
    
    * Update filter_validation.go
    
    * Now we can filter our incoming APIStream
    
    * Update the go linter
    
    * Update filter_validation.go
    
    * Update streams.types.go
    
    * Added docs to the functions
    
    * CR Fixes
    
    * Update filter_test.go

---
+++ proxy/src/libs/shared-model/discovery/outputs.model.go
@@ -0,0 +1,24 @@
+package discovery
+
+type (
+	InterceptorOutput struct {
+		Type                string `json:"type"`
+		Version             string `json:"version"`
+		LastTransactionDate string `json:"last_transaction_date"`
+	}
+
+	Output struct {
+		CreatedAt    string                    `json:"created_at"`
+		Interceptors []InterceptorOutput       `json:"interceptors"`
+		Endpoints    map[string]EndpointOutput `json:"endpoints"`
+	}
+
+	EndpointOutput struct {
+		MinTime string `json:"min_time"`
+		MaxTime string `json:"max_time"`
+
+		Count           int         `json:"count"`
+		StatusCodes     map[int]int `json:"status_codes"`
+		AverageDuration float32     `json:"average_duration"`
+	}
+)
