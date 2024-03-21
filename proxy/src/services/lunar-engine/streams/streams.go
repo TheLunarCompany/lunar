d04ce7e8cc5cb9718aa600bd72ae48375ba593e0
JonathanLande <108218977+JonathanLande@users.noreply.github.com>
2024-03-21 15:09:54 +0200

    Use api key value to label the metrics (#340)

---
+++ proxy/src/services/lunar-engine/streams/streams.go
@@ -0,0 +1,25 @@
+package streams
+
+import (
+	streamconfig "lunar/engine/streams/config"
+	"lunar/engine/streams/stream"
+	streamtypes "lunar/engine/streams/types"
+)
+
+type Stream struct {
+	APIStreams *stream.Stream
+}
+
+func NewStream() *Stream {
+	return &Stream{&stream.Stream{}}
+}
+
+func (s *Stream) CreateFlows(_ *streamconfig.Flow) error {
+	return nil
+}
+
+func (s *Stream) ExecuteFlow(
+	APIStream *streamtypes.APIStream,
+) error {
+	return s.APIStreams.ExecuteFlow(APIStream)
+}
