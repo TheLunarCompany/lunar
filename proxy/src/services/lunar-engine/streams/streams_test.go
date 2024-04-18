52d02d80dec1b2e336e43862c658ce2d7edc0149
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-04-18 11:54:58 +0100

    Merge pull request #411 from TheLunarCompany/cloud-144-add-policy-suggestions-form-to-policy-page
    CLOUD-144: add policy suggestions form to policy page

---
+++ proxy/src/services/lunar-engine/streams/streams_test.go
@@ -0,0 +1,29 @@
+package streams
+
+import (
+	"testing"
+)
+
+func TestNewStream(t *testing.T) {
+	_ = NewStream()
+	t.Skip("skipping test")
+}
+
+func TestParseYaml(t *testing.T) {
+	_ = NewStream()
+	t.Skip("skipping test")
+
+	// TODO: Write test cases to cover the parseYaml method
+}
+
+func TestBuildFlows(t *testing.T) {
+	_ = NewStream()
+	t.Skip("skipping test")
+	// TODO: Write test cases to cover the buildFlows method
+}
+
+func TestExecuteFlows(t *testing.T) {
+	_ = NewStream()
+	t.Skip("skipping test")
+	// TODO: Write test cases to cover the ExecuteFlows method
+}
