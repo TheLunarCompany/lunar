27abc09f55b899dddf22f7452c0c8208b098c28a
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-05-23 17:08:00 +0100

    Merge pull request #473 from TheLunarCompany/cloud-251-evolve-initial-loading-state-into-ui-skeleton
    CLOUD-251: evolve initial loading state into UI skeleton

---
+++ proxy/src/services/lunar-engine/streams/types/stream.type.go
@@ -0,0 +1,78 @@
+package streamtypes
+
+import "fmt"
+
+type APIStream struct {
+	Name     string
+	Type     StreamType
+	Request  *OnRequest
+	Response *OnResponse
+
+	Context ContextData
+}
+
+type ContextData struct {
+	EarlyResponse *EarlyResponseData
+}
+
+func (req *APIStream) SetEarlyResponseData(status int, body string, headers map[string]string) {
+	req.Context.EarlyResponse = &EarlyResponseData{
+		Status:  status,
+		Body:    body,
+		Headers: headers,
+	}
+}
+
+type StreamType int
+
+const (
+	GlobalStream = "globalStream"
+	StreamStart  = "start"
+	StreamEnd    = "end"
+
+	StreamTypeMirror StreamType = iota
+	StreamTypeResponse
+	StreamTypeRequest
+	StreamTypeAny
+)
+
+var streamTypeToString = map[StreamType]string{
+	StreamTypeMirror:   "StreamTypeMirror",
+	StreamTypeResponse: "StreamTypeResponse",
+	StreamTypeRequest:  "StreamTypeRequest",
+	StreamTypeAny:      "StreamTypeAny",
+}
+
+var stringToStreamType = map[string]StreamType{
+	"StreamTypeMirror":   StreamTypeMirror,
+	"StreamTypeResponse": StreamTypeResponse,
+	"StreamTypeRequest":  StreamTypeRequest,
+	"StreamTypeAny":      StreamTypeAny,
+}
+
+func (s StreamType) String() string {
+	if str, ok := streamTypeToString[s]; ok {
+		return str
+	}
+	return fmt.Sprintf("StreamType(%d)", s)
+}
+
+func (s *StreamType) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var str string
+	if err := unmarshal(&str); err != nil {
+		return err
+	}
+	if streamType, ok := stringToStreamType[str]; ok {
+		*s = streamType
+		return nil
+	}
+	return fmt.Errorf("invalid StreamType: %s", str)
+}
+
+func (s StreamType) IsRequestType() bool {
+	return s == StreamTypeRequest
+}
+
+func (s StreamType) IsResponseType() bool {
+	return s == StreamTypeResponse
+}
