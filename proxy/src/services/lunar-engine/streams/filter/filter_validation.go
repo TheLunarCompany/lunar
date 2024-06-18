477b571d9633182bad7eec6cae25182fa0466b78
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2024-06-18 13:38:52 +0200

    Fix CD action (#513)

---
+++ proxy/src/services/lunar-engine/streams/filter/filter_validation.go
@@ -0,0 +1,77 @@
+package streamfilter
+
+import (
+	streamtypes "lunar/engine/streams/types"
+
+	"github.com/rs/zerolog/log"
+)
+
+func (node *FilterNode) isHeadersQualified(APIStream *streamtypes.APIStream) bool {
+	/* Check if stream headers are qualified based on the filter */
+	for _, header := range node.filter.Headers {
+		if APIStream.Type.IsRequestType() || APIStream.Type.IsAnyType() {
+			if !APIStream.Request.DoesHeaderValueMatch(header.Key, header.Value) {
+				return false
+			}
+		} else if !APIStream.Response.DoesHeaderValueMatch(header.Key, header.Value) {
+			return false
+		}
+	}
+	return true
+}
+
+func (node *FilterNode) isStatusCodeQualified(APIStream *streamtypes.APIStream) bool {
+	/* Check if stream status code is qualified based on the filter */
+	if len(node.filter.StatusCode) == 0 {
+		log.Trace().Msgf("Status code not specified")
+		return true
+	}
+	for _, statusCode := range node.filter.StatusCode {
+		if statusCode == APIStream.Response.Status {
+			log.Trace().Msg("Status code is qualified")
+			return true
+		}
+	}
+	log.Trace().Msg("Status code not qualified")
+	return false
+}
+
+func (node *FilterNode) isMethodQualified(APIStream *streamtypes.APIStream) bool {
+	/* Check if stream method is qualified based on the filter */
+	if len(node.filter.Method) == 0 {
+		log.Trace().Msgf("Method not specified")
+		return true
+	}
+	for _, method := range node.filter.Method {
+		if method == APIStream.GetMethod() {
+			log.Trace().Msgf("Method qualified")
+			return true
+		}
+	}
+	log.Trace().Msgf("Method not qualified")
+	return false
+}
+
+func (node *FilterNode) isQueryParamsQualified(APIStream *streamtypes.APIStream) bool {
+	/* Check if stream query params are qualified based on the filter */
+	if len(node.filter.QueryParams) == 0 {
+		log.Trace().Msgf("Query params not specified")
+		return true
+	}
+	for _, queryParam := range node.filter.QueryParams {
+		if exists, err := APIStream.Request.DoesQueryParamExist(queryParam.Key); !exists {
+			log.Trace().Err(err).Msgf("Query param %s not found", queryParam.Key)
+			return false
+		}
+		if queryMatch, _ := APIStream.Request.DoesQueryParamValueMatch(
+			queryParam.Key,
+			queryParam.Value,
+		); !queryMatch {
+			log.Trace().Msgf("Query param %s value not matched", queryParam.Key)
+			return false
+		}
+	}
+
+	log.Trace().Msgf("Query params qualified")
+	return true
+}
---
+++ proxy/src/services/lunar-engine/streams/types/request.util.go
@@ -0,0 +1,94 @@
+package streamtypes
+
+import (
+	"fmt"
+	"lunar/engine/messages"
+	"net/url"
+	"strconv"
+)
+
+func (req *OnRequest) init() error {
+	if req.parsedURL != nil {
+		return nil
+	}
+
+	if sizeStr, ok := req.Headers["Content-Length"]; ok {
+		size, _ := strconv.Atoi(sizeStr)
+		req.size = size
+	} else {
+		req.size = len(req.Body)
+	}
+
+	urlWithQueryString := fmt.Sprintf(
+		"%s://%s?%s",
+		req.Scheme,
+		req.URL,
+		req.Query,
+	)
+	parsedURL, err := url.Parse(urlWithQueryString)
+	if err != nil {
+		return err
+	}
+	req.parsedURL = parsedURL
+	req.parsedQuery = parsedURL.Query()
+
+	return nil
+}
+
+func (req *OnRequest) DoesHeaderExist(headerName string) bool {
+	_, found := req.Headers[headerName]
+	return found
+}
+
+func (req *OnRequest) DoesHeaderValueMatch(headerName, headerValue string) bool {
+	if !req.DoesHeaderExist(headerName) {
+		return false
+	}
+	return req.Headers[headerName] == headerValue
+}
+
+func (req *OnRequest) DoesQueryParamExist(paramName string) (bool, error) {
+	if err := req.init(); err != nil {
+		return false, err
+	}
+	_, found := req.parsedURL.Query()[paramName]
+	return found, nil
+}
+
+func (req *OnRequest) DoesQueryParamValueMatch(paramName, paramValue string) (bool, error) {
+	queryExists, err := req.DoesQueryParamExist(paramName)
+	if !queryExists {
+		return queryExists, err
+	}
+
+	return req.parsedURL.Query().Get(paramName) == paramValue, nil
+}
+
+func (req *OnRequest) Size() (int, error) {
+	if err := req.init(); err != nil {
+		return -1, err
+	}
+	return req.size, nil
+}
+
+// NewRequestAPIStream creates a new APIStream with the given OnRequest
+func NewRequestAPIStream(onRequest messages.OnRequest) *APIStream {
+	name := fmt.Sprintf("RequestAPIStream-%s", onRequest.ID)
+	apiStream := &APIStream{
+		Name: name,
+		Type: StreamTypeRequest,
+		Request: &OnRequest{
+			ID:         onRequest.ID,
+			SequenceID: onRequest.SequenceID,
+			Method:     onRequest.Method,
+			Scheme:     onRequest.Scheme,
+			URL:        onRequest.URL,
+			Path:       onRequest.Path,
+			Query:      onRequest.Query,
+			Headers:    onRequest.Headers,
+			Body:       onRequest.Body,
+			Time:       onRequest.Time,
+		},
+	}
+	return apiStream
+}
