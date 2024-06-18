b312053d47712079cd6d23fd280dae4abd41a44e
JonathanLande <108218977+JonathanLande@users.noreply.github.com>
2024-06-18 17:03:41 +0300

    Update auth0 env vars (#515)

---
+++ proxy/src/libs/toolkit-core/urltree/url_tree_insert_test.go
@@ -0,0 +1,303 @@
+package urltree_test
+
+import (
+	"lunar/toolkit-core/urltree"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGivenConstantURLInsertIsSuccessful(t *testing.T) {
+	t.Parallel()
+	wantValue := &TestStruct{Data: 99999}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/1234", wantValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/1234")
+
+	assert.Equal(t, wantValue, lookupResult.Value)
+}
+
+func TestGivenWildcardURLInsertIsSuccessful(t *testing.T) {
+	t.Parallel()
+	wantValue := &TestStruct{Data: 1}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/*", wantValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/blabla/hello")
+
+	assert.Equal(t, wantValue, lookupResult.Value)
+}
+
+func TestGivenParametricURLInsertIsSuccessful(t *testing.T) {
+	t.Parallel()
+	wantValue := &TestStruct{Data: 1}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/{userID}", wantValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/999")
+
+	wantParams := map[string]string{"userID": "999"}
+	assert.Equal(t, wantValue, lookupResult.Value)
+	assert.Equal(t, wantParams, lookupResult.PathParams)
+}
+
+func TestGivenConstantURLIsInTreeParametricURLInsertIsSuccessful(t *testing.T) {
+	t.Parallel()
+	wantExactMatchValue := &TestStruct{Data: 1}
+	wantParametricPathValue := &TestStruct{Data: 2}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/1234", wantExactMatchValue)
+	assert.Nil(t, err)
+
+	err = urlTree.Insert("twitter.com/user/{userID}", wantParametricPathValue)
+	assert.Nil(t, err)
+
+	lookupExactMatchResult := urlTree.Lookup("twitter.com/user/1234")
+	lookupPathParameterResult := urlTree.Lookup("twitter.com/user/999")
+
+	wantParams := map[string]string{"userID": "999"}
+	assert.Equal(t, wantExactMatchValue, lookupExactMatchResult.Value)
+	assert.Nil(t, lookupExactMatchResult.PathParams)
+	assert.Equal(t, wantParametricPathValue, lookupPathParameterResult.Value)
+	assert.Equal(t, wantParams, lookupPathParameterResult.PathParams)
+}
+
+func TestGivenParametricURLIsInTreePathParameterInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	wantValue1 := &TestStruct{Data: 1}
+	wantValue2 := &TestStruct{Data: 2}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/{userID}", wantValue1)
+	assert.Nil(t, err)
+
+	lookupResult1 := urlTree.Lookup("twitter.com/user/999")
+
+	err = urlTree.Insert("twitter.com/user/{userID}", wantValue2)
+	assert.Nil(t, err)
+
+	lookupResult2 := urlTree.Lookup("twitter.com/user/999")
+
+	wantParams := map[string]string{"userID": "999"}
+	assert.Equal(t, wantValue1, lookupResult1.Value)
+	assert.Equal(t, wantParams, lookupResult2.PathParams)
+	assert.Equal(t, wantValue2, lookupResult2.Value)
+	assert.Equal(t, wantParams, lookupResult2.PathParams)
+}
+
+func TestGivenParametricURLIsInTreeConstantChildInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	wantParametricPathValue := &TestStruct{Data: 1}
+	wantExactMatchValue := &TestStruct{Data: 2}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/{userID}", wantParametricPathValue)
+	assert.Nil(t, err)
+
+	lookupResult1 := urlTree.Lookup("twitter.com/user/1234")
+
+	err = urlTree.Insert("twitter.com/user/1234", wantExactMatchValue)
+	assert.Nil(t, err)
+
+	lookupResult2 := urlTree.Lookup("twitter.com/user/1234")
+
+	wantParams := map[string]string{"userID": "1234"}
+	assert.Equal(t, wantParametricPathValue, lookupResult1.Value)
+	assert.Equal(t, wantParams, lookupResult1.PathParams)
+	assert.Equal(t, wantExactMatchValue, lookupResult2.Value)
+	assert.Nil(t, lookupResult2.PathParams)
+}
+
+func TestGivenParametricURLIsInTreeParametricURLWithDifferentParamNameInsertReturnsError(
+	t *testing.T,
+) {
+	t.Parallel()
+	wantValue1 := &TestStruct{Data: 1}
+	wantValue2 := &TestStruct{Data: 2}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/{userID}", wantValue1)
+	assert.Nil(t, err)
+
+	lookupResult1 := urlTree.Lookup("twitter.com/user/1234")
+
+	err = urlTree.Insert("twitter.com/user/{uid}", wantValue2)
+	assert.NotNil(t, err)
+
+	lookupResult2 := urlTree.Lookup("twitter.com/user/1234")
+
+	wantParams := map[string]string{"userID": "1234"}
+	assert.Equal(t, wantValue1, lookupResult1.Value)
+	assert.Equal(t, wantParams, lookupResult1.PathParams)
+	assert.Equal(t, wantValue1, lookupResult2.Value)
+	assert.Equal(t, wantParams, lookupResult1.PathParams)
+}
+
+func TestGivenMixedURLsInsertIsSuccessful(t *testing.T) {
+	t.Parallel()
+	wantExactMatchValue := &TestStruct{Data: 1}
+	wantWildcardMatchValue := &TestStruct{Data: 2}
+	wantPathParamMatchValue := &TestStruct{Data: 3}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert(
+		"twitter.com/user/1234", wantExactMatchValue)
+	assert.Nil(t, err)
+	lookupResult1 := urlTree.Lookup("twitter.com/user/1234")
+	assert.Equal(t, wantExactMatchValue, lookupResult1.Value)
+
+	lookupResult2 := urlTree.Lookup("twitter.com/user/999")
+	assert.Nil(t, lookupResult2.Value)
+
+	err = urlTree.Insert(
+		"twitter.com/user/*", wantWildcardMatchValue)
+	assert.Nil(t, err)
+	lookupResult3 := urlTree.Lookup("twitter.com/user/1234")
+	assert.Equal(t, wantExactMatchValue, lookupResult3.Value)
+
+	lookupResult4 := urlTree.Lookup("twitter.com/user/1234/messages")
+	assert.Equal(t, wantWildcardMatchValue, lookupResult4.Value)
+
+	lookupResult5 := urlTree.Lookup("twitter.com/user/999")
+	assert.Equal(t, wantWildcardMatchValue, lookupResult5.Value)
+
+	lookupResult6 := urlTree.Lookup("twitter.com/post/1234")
+	assert.Nil(t, lookupResult6.Value)
+
+	err = urlTree.Insert(
+		"twitter.com/user/{userID}", wantPathParamMatchValue)
+	assert.Nil(t, err)
+
+	lookupResult7 := urlTree.Lookup("twitter.com/user/1234")
+	assert.Equal(t, wantExactMatchValue, lookupResult7.Value)
+
+	lookupResult10 := urlTree.Lookup("twitter.com/user/1234/messages")
+	assert.Equal(t, wantWildcardMatchValue, lookupResult10.Value)
+
+	lookupResult11 := urlTree.Lookup("twitter.com/user/999")
+	wantParams := map[string]string{"userID": "999"}
+	assert.Equal(t, wantPathParamMatchValue, lookupResult11.Value)
+	assert.Equal(
+		t,
+		wantParams,
+		lookupResult11.PathParams,
+	)
+
+	lookupResult12 := urlTree.Lookup("twitter.com/post/1234")
+	assert.Nil(t, lookupResult12.Value)
+}
+
+func TestGivenWildcardAndPathParameterURLPathParameterIsMatched(
+	t *testing.T,
+) {
+	t.Parallel()
+	wantWildcardValue := &TestStruct{Data: 1}
+	wantParametricPathValue := &TestStruct{Data: 2}
+	urlTree := urltree.NewURLTree[TestStruct]()
+
+	err := urlTree.Insert("twitter.com/user/*", wantWildcardValue)
+	assert.Nil(t, err)
+	err = urlTree.Insert("twitter.com/user/{userID}", wantParametricPathValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/999")
+
+	wantParams := map[string]string{"userID": "999"}
+	assert.Equal(t, wantParametricPathValue, lookupResult.Value)
+	assert.Equal(t, wantParams, lookupResult.PathParams)
+}
+
+func TestGivenDuplicateURLsInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	testValue := &TestStruct{Data: 99999}
+	urlTree := urltree.NewURLTree[TestStruct]()
+	err := urlTree.Insert("twitter.com/user/1234", testValue)
+	assert.Nil(t, err)
+	err = urlTree.Insert("twitter.com/user/1234", testValue)
+	assert.Nil(t, err)
+}
+
+func TestGivenOverlappingURLsInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	testValue := &TestStruct{Data: 99999}
+	urlTree := urltree.NewURLTree[TestStruct]()
+	err := urlTree.Insert("twitter.com/user/1234", testValue)
+	assert.Nil(t, err)
+	err = urlTree.Insert(
+		"twitter.com/user/{userID}", testValue)
+	assert.Nil(t, err)
+	err = urlTree.Insert("twitter.com/user/*", testValue)
+	assert.Nil(t, err)
+	err = urlTree.Insert("twitter.com/*", testValue)
+	assert.Nil(t, err)
+}
+
+func TestGivenPathParamInHostInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	testValue := &TestStruct{Data: 456}
+	urlTree := urltree.NewURLTree[TestStruct]()
+	err := urlTree.Insert("{domain}.com/user/1234", testValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/1234")
+	assert.Equal(t, testValue, lookupResult.Value)
+	assert.Equal(t, "{domain}.com/user/1234", lookupResult.NormalizedURL)
+}
+
+func TestGivenParametricPathInHostAndWildcardInPathInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	testValue := &TestStruct{Data: 789}
+	urlTree := urltree.NewURLTree[TestStruct]()
+	err := urlTree.Insert("{otherDomain}.com/user/*", testValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/1234")
+	assert.Equal(t, testValue, lookupResult.Value)
+	assert.Equal(t, "{otherDomain}.com/user/*", lookupResult.NormalizedURL)
+}
+
+func TestGivenParametricsPathInHostAndInPathInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	testValue := &TestStruct{Data: 101112}
+	urlTree := urltree.NewURLTree[TestStruct]()
+	err := urlTree.Insert("{domain}.com/user/{userID}", testValue)
+	assert.Nil(t, err)
+
+	lookupResult := urlTree.Lookup("twitter.com/user/1234")
+	assert.Equal(t, testValue, lookupResult.Value)
+	assert.Equal(t, "{domain}.com/user/{userID}", lookupResult.NormalizedURL)
+}
+
+func TestGivenWildcardInHostInsertIsSuccessful(
+	t *testing.T,
+) {
+	t.Parallel()
+	testValue := &TestStruct{Data: 131415}
+	urlTree := urltree.NewURLTree[TestStruct]()
+	err := urlTree.Insert("myip.*", testValue)
+	assert.Nil(t, err)
+	lookupResult := urlTree.Lookup("myip.wtf/json")
+	assert.Equal(t, testValue, lookupResult.Value)
+}
---
+++ proxy/src/services/lunar-engine/streams/filter/filter_node.go
@@ -0,0 +1,34 @@
+package streamfilter
+
+import (
+	streamconfig "lunar/engine/streams/config"
+	internal_types "lunar/engine/streams/internal-types"
+	streamtypes "lunar/engine/streams/types"
+)
+
+type FilterNode struct {
+	filter *streamconfig.Filter
+	flow   internal_types.FlowGraphI
+}
+
+func (node *FilterNode) getFlow(apiStream *streamtypes.APIStream) internal_types.FlowGraphI {
+	/* Get flow based on the API stream,
+	   the function will validate the stream based on the filter */
+	if !node.isHeadersQualified(apiStream) {
+		return nil
+	}
+
+	if apiStream.Response != nil && !node.isStatusCodeQualified(apiStream) {
+		return nil
+	}
+
+	if !node.isMethodQualified(apiStream) {
+		return nil
+	}
+
+	if apiStream.Request != nil && !node.isQueryParamsQualified(apiStream) {
+		return nil
+	}
+
+	return node.flow
+}
