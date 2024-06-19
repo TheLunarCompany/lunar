f9e08d5131aacc5c4fa16d2e3fefc9d8a3bd8020
JonathanLande <108218977+JonathanLande@users.noreply.github.com>
2024-06-19 18:09:37 +0300

    Update env vars (#520)

---
+++ proxy/src/services/lunar-engine/streams/types/context_manager.go
@@ -0,0 +1,47 @@
+package streamtypes
+
+var globalContext ContextI
+
+func init() {
+	globalContext = NewContext()
+}
+
+type ContextManager struct {
+	globalContext ContextI
+	adminContext  LunarAdminContextI
+}
+
+// NewContextManager creates a new ContextManager
+func NewContextManager() *ContextManager {
+	return &ContextManager{
+		globalContext: globalContext,
+		adminContext:  NewLunarContext(globalContext),
+	}
+}
+
+// WithFlowContext sets the flow context
+func (c *ContextManager) WithFlowContext() *ContextManager {
+	c.adminContext.SetFlowContext(NewContext())
+	return c
+}
+
+// WithTransactionalContext sets the transactional context
+func (c *ContextManager) WithTransactionalContext() *ContextManager {
+	c.adminContext.InitiateTransactionalContext()
+	return c
+}
+
+// DestroyTransactionalContext destroys the lunar context
+func (c *ContextManager) DestroyTransactionalContext() {
+	c.adminContext.DestroyTransactionalContext()
+}
+
+// GetLunarContext returns the lunar context
+func (c *ContextManager) GetLunarContext() LunarAdminContextI {
+	return c.adminContext
+}
+
+// GetGlobalContext returns the global context
+func (c *ContextManager) GetGlobalContext() ContextI {
+	return c.globalContext
+}
