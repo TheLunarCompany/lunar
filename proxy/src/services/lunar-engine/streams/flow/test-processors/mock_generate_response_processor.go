876aeae370a52463d5b5b5a45183d4e84c79173c
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-08-13 18:48:12 +0100

    Merge pull request #591 from TheLunarCompany/cloud-384-restrict-auto-enabled-connection-on-org-creation
    fix: specify connections we want to enable by default instead of getting all.

---
+++ proxy/src/services/lunar-engine/streams/flow/test-processors/mock_generate_response_processor.go
@@ -0,0 +1,48 @@
+package testprocessors
+
+import (
+	"lunar/engine/actions"
+	publictypes "lunar/engine/streams/public-types"
+	streamtypes "lunar/engine/streams/types"
+	"net/http"
+)
+
+type mockGenerateResponseProcessor struct {
+	name       string
+	statusCode int
+	body       string
+	header     map[string]string
+	metaData   *streamtypes.ProcessorMetaData
+}
+
+func NewMockGenerateResponseProcessor(metadata *streamtypes.ProcessorMetaData) (streamtypes.Processor, error) { //nolint:lll
+	return &mockGenerateResponseProcessor{name: metadata.Name, metaData: metadata, statusCode: http.StatusOK}, nil //nolint:lll
+}
+
+func NewMockGenerateResponse403Processor(metadata *streamtypes.ProcessorMetaData) (streamtypes.Processor, error) { //nolint:lll
+	return &mockGenerateResponseProcessor{name: metadata.Name, metaData: metadata, statusCode: http.StatusForbidden}, nil //nolint:lll
+}
+
+func (p *mockGenerateResponseProcessor) GetName() string {
+	return p.name
+}
+
+func (p *mockGenerateResponseProcessor) Execute(
+	apiStream publictypes.APIStreamI,
+) (streamtypes.ProcessorIO, error) {
+	signInExecution(apiStream, p.name)
+	var action actions.ReqLunarAction = &actions.NoOpAction{}
+	if apiStream.GetType() == publictypes.StreamTypeRequest {
+		action = &actions.EarlyResponseAction{
+			Status:  p.statusCode,
+			Body:    p.body,
+			Headers: p.header,
+		}
+	}
+
+	return streamtypes.ProcessorIO{
+		Type:      publictypes.StreamTypeResponse,
+		ReqAction: action,
+		Name:      "",
+	}, nil
+}
---
+++ proxy/src/services/lunar-engine/streams/internal-types/filter.type.go
@@ -0,0 +1,8 @@
+package internaltypes
+
+import publictypes "lunar/engine/streams/public-types"
+
+type FilterTreeI interface {
+	AddFlow(FlowI) error
+	GetFlow(APIStream publictypes.APIStreamI) FlowI
+}
