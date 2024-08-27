3abdea4c283055a5068d00746c24e49615889d3a
eitangol <108608289+eitangol@users.noreply.github.com>
2024-08-27 20:19:31 +0300

    Now support the use of vanilla mode in case of an panic in LunarEngine (#611)
    * Now support the use of vanilla mode in case of an panic in LunarEngine
    
    * Added the engineFailsafeEnabled to the k8s deployment
    
    * Update logging.go
    
    * Added a Failsafe section in the proxy docs
    
    * Update .gitignore
    
    * Remove YAMLS
    
    * Update .gitignore

---
+++ proxy/src/services/lunar-engine/streams/resources/resource_management.go
@@ -0,0 +1,245 @@
+package resources
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	internaltypes "lunar/engine/streams/internal-types"
+	publictypes "lunar/engine/streams/public-types"
+	quotaresource "lunar/engine/streams/resources/quota"
+	resourceutils "lunar/engine/streams/resources/utils"
+	streamtypes "lunar/engine/streams/types"
+	"lunar/engine/utils/environment"
+	"lunar/toolkit-core/clock"
+	"lunar/toolkit-core/configuration"
+	"lunar/toolkit-core/network"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+type ResourceManagement struct {
+	clock        clock.Clock
+	quotas       *Resource[quotaresource.QuotaAdmI]
+	reqIDToQuota publictypes.ContextI
+	flowData     map[publictypes.ComparableFilter]*resourceutils.SystemFlowRepresentation
+	loadedConfig []network.ConfigurationPayload
+}
+
+func NewResourceManagement(
+	clock clock.Clock,
+) (*ResourceManagement, error) {
+	management := &ResourceManagement{
+		clock:        clock,
+		loadedConfig: []network.ConfigurationPayload{},
+		quotas:       NewResource[quotaresource.QuotaAdmI](),
+		reqIDToQuota: streamtypes.NewContext(),
+		flowData:     make(map[publictypes.ComparableFilter]*resourceutils.SystemFlowRepresentation),
+	}
+
+	if err := management.init(); err != nil {
+		return nil, err
+	}
+	return management, nil
+}
+
+func (rm *ResourceManagement) WithQuotaData(
+	quotaData []*quotaresource.QuotaResourceData,
+) (*ResourceManagement, error) {
+	if err := rm.loadQuotaResources(quotaData); err != nil {
+		return nil, err
+	}
+	return rm, nil
+}
+
+func (rm *ResourceManagement) OnRequestDrop(APIStream publictypes.APIStreamI) {
+	quotaObj, err := rm.reqIDToQuota.Pop(APIStream.GetID())
+	if err != nil {
+		log.Debug().Msgf("Could not locate quota resource with ID %s", APIStream.GetID())
+		return
+	}
+	quota := quotaObj.(publictypes.QuotaResourceI)
+	if err := quota.Dec(APIStream); err != nil {
+		log.Warn().Err(err).Msgf("Failed to decrement quota for request %s", APIStream.GetID())
+	}
+}
+
+func (rm *ResourceManagement) OnRequestFinish(APIStream publictypes.APIStreamI) {
+	_, _ = rm.reqIDToQuota.Pop(APIStream.GetID())
+}
+
+func (rm *ResourceManagement) GetQuota(
+	quotaID string,
+	reqID string,
+) (publictypes.QuotaResourceI, error) {
+	quotaResource, found := rm.quotas.Get(quotaID)
+	if !found {
+		return nil, fmt.Errorf("quota resource with ID %s not found", quotaID)
+	}
+	quotaObj, err := quotaResource.GetQuota(quotaID)
+	if err != nil {
+		return nil, err
+	}
+
+	if reqID != "" {
+		if _, err := rm.reqIDToQuota.Get(reqID); err != nil {
+			if err := rm.reqIDToQuota.Set(reqID, quotaObj); err != nil {
+				log.Debug().Err(err).
+					Msgf("Failed to set quota resource with ID %s for request %s", quotaID, reqID)
+			}
+		}
+	}
+
+	return quotaObj, nil
+}
+
+func (rm *ResourceManagement) UpdateQuota(
+	quotaID string,
+	metaData *quotaresource.QuotaResourceData,
+) error {
+	// TODO: When updating quota, we should also update the system flow data
+	// 			 and update LunarHub with the new configuration
+	quotaResource, found := rm.quotas.Get(quotaID)
+	if !found {
+		log.Trace().Msgf("Could not locate quota resource with ID %s", quotaID)
+		return nil
+	}
+
+	return quotaResource.Update(metaData)
+}
+
+func (rm *ResourceManagement) GetFlowData(
+	filter publictypes.ComparableFilter,
+) (*resourceutils.SystemFlowRepresentation, error) {
+	log.Trace().Msgf("Looking for system flow with Key: %v", filter)
+	for key := range rm.flowData {
+		log.Trace().Msgf("Key: %v", key)
+	}
+	flowRepresentation, found := rm.flowData[filter]
+	if !found {
+		return nil, fmt.Errorf("system flow data with filter %v not found", filter)
+	}
+	return flowRepresentation, nil
+}
+
+func (rm *ResourceManagement) GetLoadedConfig() []network.ConfigurationPayload {
+	return rm.loadedConfig
+}
+
+func (rm *ResourceManagement) GetUnReferencedFlowData() []*resourceutils.SystemFlowRepresentation {
+	log.Trace().Msg("Retrieving unreferenced system flow data")
+	var flowRepresentation []*resourceutils.SystemFlowRepresentation
+	for _, systemFlow := range rm.flowData {
+		if systemFlow.IsReferencedByUsedFlow() {
+			continue
+		}
+		flowRepresentation = append(flowRepresentation, systemFlow)
+	}
+
+	return flowRepresentation
+}
+
+func (
+	rm *ResourceManagement,
+) GetFlowsData() map[publictypes.ComparableFilter]*resourceutils.SystemFlowRepresentation {
+	return rm.flowData
+}
+
+func (rm *ResourceManagement) init() error {
+	var err error
+	var quotaData []*quotaresource.QuotaResourceData
+
+	quotaData, err = rm.loadAndParseQuotaFiles()
+	if err != nil {
+		return err
+	}
+	for _, data := range quotaData {
+		err = data.Validate()
+		if err != nil {
+			err = errors.Join(err)
+		}
+	}
+	if err != nil {
+		return err
+	}
+	return rm.loadQuotaResources(quotaData)
+}
+
+func (rm *ResourceManagement) loadAndParseQuotaFiles() (
+	[]*quotaresource.QuotaResourceData,
+	error,
+) {
+	resources := environment.GetResourcesDirectory()
+	quotaResourceFiles, err := findQuotaResources(resources)
+	var quotaData []*quotaresource.QuotaResourceData
+	if err != nil {
+		return nil, err
+	}
+	for _, path := range quotaResourceFiles {
+		config, readErr := configuration.DecodeYAML[quotaresource.QuotaResourceData](path)
+		if readErr != nil {
+			return nil, readErr
+		}
+		rm.loadedConfig = append(rm.loadedConfig, network.ConfigurationPayload{
+			Type:     "quota-resource",
+			FileName: path,
+			Content:  config.Content,
+		})
+		quotaData = append(quotaData, config.UnmarshaledData)
+	}
+	return quotaData, nil
+}
+
+func (rm *ResourceManagement) loadQuotaResources(
+	quotaData []*quotaresource.QuotaResourceData,
+) error {
+	for _, metaData := range quotaData {
+		quotaResource, err := quotaresource.NewQuota(rm.clock, metaData)
+		if err != nil {
+			return err
+		}
+
+		log.Trace().Msgf("Adding quota resource with: ID %s, Filter: %v",
+			metaData.Quota.ID,
+			metaData.Quota.Filter,
+		)
+
+		for _, id := range quotaResource.GetIDs() {
+			rm.quotas.Set(id, quotaResource)
+		}
+
+		for comparableFilter, systemFlow := range quotaResource.GetSystemFlow() {
+			log.Trace().Msgf("Adding system flow with Key: %v", comparableFilter)
+			log.Trace().Msgf("SystemFlowID: %v", systemFlow.GetID())
+			if _, found := rm.flowData[comparableFilter]; !found {
+				rm.flowData[comparableFilter] = systemFlow // resourceutils.NewSystemFlowRepresentation()
+			} else {
+				if err := rm.flowData[comparableFilter].AddSystemRepresentation(systemFlow); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func findQuotaResources(dir string) ([]string, error) {
+	var files []string
+	err := filepath.WalkDir(dir, func(path string, directory fs.DirEntry, err error) error {
+		if err != nil {
+			if os.IsNotExist(err) { // ignore if directory does not exist
+				return nil
+			}
+			return err
+		}
+
+		if !directory.IsDir() && strings.HasSuffix(path, internaltypes.QuotaResourceExtension) {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
---
+++ proxy/src/services/lunar-engine/streams/streams.go
@@ -0,0 +1,164 @@
+package streams
+
+import (
+	"fmt"
+	"lunar/engine/communication"
+	streamconfig "lunar/engine/streams/config"
+	streamfilter "lunar/engine/streams/filter"
+	streamflow "lunar/engine/streams/flow"
+	internal_types "lunar/engine/streams/internal-types"
+	"lunar/engine/streams/processors"
+	publictypes "lunar/engine/streams/public-types"
+	"lunar/engine/streams/resources"
+	"lunar/engine/streams/stream"
+	"lunar/engine/utils"
+	"lunar/toolkit-core/clock"
+	"lunar/toolkit-core/network"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Stream struct {
+	apiStreams        *stream.Stream
+	filterTree        internal_types.FilterTreeI
+	processorsManager *processors.ProcessorManager
+	resources         *resources.ResourceManagement
+	supportedFilters  map[publictypes.ComparableFilter][]streamconfig.Filter
+	loadedConfig      network.ConfigurationData
+	lunarHub          *communication.HubCommunication
+}
+
+func NewStream(clock clock.Clock) *Stream {
+	resources, err := resources.NewResourceManagement(clock)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create resources")
+	}
+	return &Stream{
+		loadedConfig:      network.ConfigurationData{},
+		apiStreams:        stream.NewStream(),
+		filterTree:        streamfilter.NewFilterTree(),
+		processorsManager: processors.NewProcessorManager(resources),
+		resources:         resources,
+	}
+}
+
+func (s *Stream) WithHub(hub *communication.HubCommunication) *Stream {
+	s.lunarHub = hub
+	return s
+}
+
+// Initialize initializes the stream engine by creating flows from the stream config.
+func (s *Stream) Initialize() error {
+	log.Info().Msg("Initializing stream engine")
+
+	flowsDefinition, err := streamconfig.GetFlows()
+	if err != nil {
+		return fmt.Errorf("failed to parse streams config: %w", err)
+	}
+
+	// Get all supported filters
+	s.supportedFilters = map[publictypes.ComparableFilter][]streamconfig.Filter{}
+	for key, resource := range s.resources.GetFlowsData() {
+		s.supportedFilters[key] = append(s.supportedFilters[key], *resource.GetFilter())
+	}
+
+	if s.lunarHub != nil {
+		for _, flow := range flowsDefinition {
+			s.loadedConfig.Data = append(s.loadedConfig.Data, flow.Data)
+		}
+	}
+
+	for _, flow := range flowsDefinition {
+		s.supportedFilters[flow.Filters.ToComparable()] = append(
+			s.supportedFilters[flow.Filters.ToComparable()],
+			flow.Filters,
+		)
+	}
+
+	log.Trace().Msgf("Supported filters: %v", s.supportedFilters)
+
+	log.Trace().Msg("Creating processors")
+	if err = s.processorsManager.Init(); err != nil {
+		return fmt.Errorf("failed to initialize processors: %w", err)
+	}
+
+	err = s.createFlows(flowsDefinition)
+	if err != nil {
+		return fmt.Errorf("failed to create flows: %w", err)
+	}
+	s.notifyHub() // Here we notify the hub about the loaded config of the stream engine
+	return nil
+}
+
+func (s *Stream) ExecuteFlow(
+	apiStream publictypes.APIStreamI,
+	actions *streamconfig.StreamActions,
+) (err error) {
+	log.Trace().Msgf("Executing flow for APIStream %v", apiStream.GetName())
+	// resetting apiStream instance before flow execution
+	s.apiStreams = stream.NewStream()
+
+	flow := s.filterTree.GetFlow(apiStream)
+	if utils.IsInterfaceNil(flow) {
+		log.Trace().Msgf("No flow found for %v", apiStream.GetURL())
+		return nil
+	}
+
+	apiStream.SetContext(flow.GetExecutionContext())
+	defer flow.CleanExecution()
+
+	log.Trace().Msgf("Flow %v found for %v", flow.GetName(), apiStream.GetURL())
+	var flowDir internal_types.FlowDirectionI
+	if apiStream.GetType().IsRequestType() {
+		flowDir = flow.GetRequestDirection()
+	} else if apiStream.GetType().IsResponseType() {
+		flowDir = flow.GetResponseDirection()
+	} else {
+		return fmt.Errorf("unknown stream type")
+	}
+
+	if !flowDir.IsDefined() {
+		return nil
+	}
+
+	start, err := flowDir.GetRoot()
+	if err != nil {
+		return err
+	}
+	executeRes := s.apiStreams.ExecuteFlow(flow, apiStream, start.GetNode(), actions)
+	if apiStream.GetType().IsRequestType() {
+		s.resources.OnRequestFinish(apiStream)
+	}
+	return executeRes
+}
+
+func (s *Stream) GetAPIStreams() *stream.Stream {
+	return s.apiStreams
+}
+
+func (s *Stream) createFlows(flowReps []*streamconfig.FlowRepresentation) error {
+	return streamflow.BuildFlows(s.filterTree, flowReps, s.processorsManager, s.resources)
+}
+
+func (s *Stream) GetSupportedFilters() map[publictypes.ComparableFilter][]streamconfig.Filter {
+	return s.supportedFilters
+}
+
+func (s *Stream) notifyHub() {
+	if s.lunarHub == nil {
+		return
+	}
+	log.Debug().Msg("Notifying hub about loaded config")
+	s.loadedConfig.Data = append(s.loadedConfig.Data, s.resources.GetLoadedConfig()...)
+	s.loadedConfig.Data = append(s.loadedConfig.Data, s.processorsManager.GetLoadedConfig()...)
+
+	if s.loadedConfig.Data == nil {
+		log.Debug().Msg("No configuration loaded, skipping notification to hub")
+		return
+	}
+
+	s.lunarHub.SendDataToHub(&network.ConfigurationMessage{
+		Event: network.WebSocketEventConfigurationLoad,
+		Data:  s.loadedConfig,
+	})
+}
