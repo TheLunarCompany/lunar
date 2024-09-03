41059e2dec43da5ae54bf8b5de5d9ff6863651b2
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2024-09-03 13:25:38 +0100

    fix breaking changes from discord.js (#616)

---
+++ interceptors/lunar-ts-interceptor/src/constants.ts
@@ -0,0 +1,43 @@
+export const TENANT_ID_KEY = "LUNAR_TENANT_ID"
+export const PROXY_HOST_KEY = "LUNAR_PROXY_HOST"
+export const HANDSHAKE_PORT_KEY = "LUNAR_HANDSHAKE_PORT"
+export const SUPPORT_TLS_KEY = "LUNAR_PROXY_SUPPORT_TLS"
+
+export const INTERCEPTOR_VERSION = "2.1.4"
+export const PROXY_DEFAULT_HANDSHAKE_PORT = 8081
+export const INTERCEPTOR_ID = `lunar-ts-interceptor/${INTERCEPTOR_VERSION}`
+
+export const GET = "get"
+export const MS_IN_SECOND = 1000
+export const REQUEST = "request"
+export const HTTP_TYPE = "http:"
+export const HTTPS_TYPE = "https:"
+export const LUNAR_REQ_ID_HEADER_KEY = "x-lunar-req-id"
+export const LUNAR_SEQ_ID_HEADER_KEY = "x-lunar-sequence-id"
+export const LUNAR_RETRY_AFTER_HEADER_KEY = "x-lunar-retry-after"
+
+export const LUNAR_HOST_HEADER = "x-lunar-host"
+export const LUNAR_SCHEME_HEADER = "x-lunar-scheme"
+export const LUNAR_TENANT_ID_HEADER = "x-lunar-tenant-id"
+export const LUNAR_INTERCEPTOR_HEADER = "x-lunar-interceptor"
+
+export const EVENT_CLOSE = "close"
+export const EVENT_ERROR = "error"
+export const EVENT_SOCKET = "socket"
+export const EVENT_CONNECT = "connect"
+export const EVENT_RESPONSE = "response"
+export const SOCKET_OPEN_STATE = "open"
+
+export const LUNAR_EVENT_ERROR = "LunarError"
+export const LUNAR_EVENT_CLOSE = "LunarClose"
+export const LUNAR_EVENT_SOCKET = "LunarSocket"
+export const LUNAR_EVENT_CONNECT = "LunarConnect"
+export const LUNAR_EVENT_RESPONSE = "LunarResponse"
+
+export const LUNAR_PROXY_ERROR_TRANSLATOR: Record<string, string> = {
+  "1": "Wrong request, Lunar Proxy could not find header `x-lunar-host` and Proxy was not set to use query params.",
+  "2": "The endpoint cannot be reached",
+  "3": "Gateway timeout",
+  "4": "Lunar Proxy could not find the endpoint",
+  "5": "Lunar Proxy could not resolve host"
+}
