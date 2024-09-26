3ead9553123cb08e5b4e32688c7f0ec7d36c3aaa
JonathanLande <108218977+JonathanLande@users.noreply.github.com>
2024-09-26 15:34:14 +0300

    Update interceptor version (#645)

---
+++ proxy/src/libs/toolkit-core/context-manager/context_manager.go
@@ -0,0 +1,76 @@
+package contextmanager
+
+import (
+	"context"
+	"lunar/toolkit-core/clock"
+	"sync"
+
+	"github.com/rs/zerolog/log"
+)
+
+// Manager is the singleton that holds the context and clock
+type ContextManager struct {
+	ctx   context.Context
+	clock clock.Clock
+}
+
+var (
+	instance *ContextManager
+	once     sync.Once
+)
+
+// Get returns the singleton instance of ContextManager
+func Get() *ContextManager {
+	if instance == nil {
+		initContextManager(context.Background(), clock.NewRealClock())
+	}
+	return instance
+}
+
+// WithContext returns a new instance of ContextManager with the provided context
+func (m *ContextManager) WithContext(ctx context.Context) *ContextManager {
+	m.ctx = ctx
+	return instance
+}
+
+// WithClock returns a new instance of ContextManager with the provided clock
+func (m *ContextManager) SetRealClock() *ContextManager {
+	m.clock = clock.NewRealClock()
+	return instance
+}
+
+// SetMockClock returns a new instance of ContextManager with a mock clock
+func (m *ContextManager) SetMockClock() *ContextManager {
+	m.clock = clock.NewMockClock()
+	return instance
+}
+
+// GetContext returns the context held by the Manager
+func (m *ContextManager) GetContext() context.Context {
+	return m.ctx
+}
+
+// GetClock returns the current time held by the Manager
+func (m *ContextManager) GetClock() clock.Clock {
+	return m.clock
+}
+
+// GetMockClock returns the mock time held by the Manager
+func (m *ContextManager) GetMockClock() *clock.MockClock {
+	mock, ok := m.clock.(*clock.MockClock)
+	if !ok {
+		log.Error().Msg("Clock is not a mock clock")
+		return nil
+	}
+	return mock
+}
+
+// initContextManager initializes the singleton instance of ContextManager
+func initContextManager(ctx context.Context, clk clock.Clock) {
+	once.Do(func() {
+		instance = &ContextManager{
+			ctx:   ctx,
+			clock: clk,
+		}
+	})
+}
