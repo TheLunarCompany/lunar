41db5d93cfd3f7744512482764f7794ab5f708fc
Dmitry Gorohovsky <35920455+d-goro@users.noreply.github.com>
2024-09-26 12:48:33 +0300

    files for flows (#643)
    * files for flows
    
    * fix test
    
    * add logs
    
    ---------
    
    Co-authored-by: d-goro <dmitryg@lunar.dev>

---
+++ proxy/src/services/lunar-engine/metrics/metrics_data_providers.go
@@ -0,0 +1,22 @@
+package metrics
+
+import publictypes "lunar/engine/streams/public-types"
+
+type APICallMetricsProviderI interface {
+	GetID() string
+	GetType() publictypes.StreamType
+	GetURL() string
+	GetBody() string
+	GetStrStatus() (string, error)
+	GetMethod() string
+	GetHeaders() map[string]string
+	GetSize() int
+}
+
+type FlowMetricsProviderI interface {
+	GetActiveFlows() int
+	GetFlowInvocations() int
+	GetRequestsThroughFlows() int
+	GetAvgFlowExecutionTime() float64
+	GetAvgProcessorExecutionTime() float64
+}
