4703761cfcc357dcd1e11a86a45c6b1ef84ec199
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2024-10-01 16:47:17 +0100

    revert changes from commit ad70d0f (#653)

---
+++ proxy/src/services/lunar-engine/streams/resources/quota/quota.utils.go
@@ -0,0 +1,32 @@
+package quotaresource
+
+import (
+	streamconfig "lunar/engine/streams/config"
+	"time"
+)
+
+func (q *QuotaMetaData) GetID() string {
+	return q.ID
+}
+
+func (q *QuotaMetaData) GetFilter() *streamconfig.Filter {
+	return q.Filter
+}
+
+func (q *QuotaMetaData) GetStrategy() *StrategyConfig {
+	return q.Strategy
+}
+
+func (mrd *MonthlyRenewalData) getMonthlyResetIn() (time.Time, error) {
+	loc, err := time.LoadLocation(mrd.Timezone)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	now := time.Now().In(loc)
+	nextReset := now.AddDate(0, 1, mrd.Day-1).
+		Add(time.Duration(mrd.Hour) * time.Hour).
+		Add(time.Duration(mrd.Minute) * time.Minute)
+
+	return nextReset, nil
+}
---
+++ proxy/src/services/lunar-engine/streams/stream/stream.go
@@ -0,0 +1,87 @@
+package stream
+
+import (
+	"fmt"
+	streamconfig "lunar/engine/streams/config"
+	internaltypes "lunar/engine/streams/internal-types"
+	publictypes "lunar/engine/streams/public-types"
+
+	"github.com/rs/zerolog/log"
+)
+
+type Stream struct {
+	Request  *streamconfig.RequestStream
+	Response *streamconfig.ResponseStream
+}
+
+func NewStream() *Stream {
+	return &Stream{
+		Request:  &streamconfig.RequestStream{},
+		Response: &streamconfig.ResponseStream{},
+	}
+}
+
+func (s *Stream) GetRequestStream() *streamconfig.RequestStream {
+	return s.Request
+}
+
+func (s *Stream) GetResponseStream() *streamconfig.ResponseStream {
+	return s.Response
+}
+
+func (s *Stream) ExecuteFlow(
+	flow internaltypes.FlowI,
+	apiStream publictypes.APIStreamI,
+	node internaltypes.FlowGraphNodeI,
+	actions *streamconfig.StreamActions,
+) error {
+	procIO, err := node.GetProcessor().Execute(apiStream)
+	if err != nil {
+		return fmt.Errorf("failed to execute processor %s: %w", node.GetProcessorKey(), err)
+	}
+
+	log.Debug().Msgf("Executed processor %s. ProcIO: %+v", node.GetProcessorKey(), procIO)
+
+	if apiStream.GetType().IsRequestType() {
+		if procIO.IsRequestActionAvailable() {
+			if procIO.ReqAction.IsEarlyReturnType() {
+				// If the request is early response, we should drop the request slot from the quota
+				// to allow other requests to be processed
+				flow.GetResourceManagement().OnRequestDrop(apiStream)
+			}
+			actions.Request.Actions = append(actions.Request.Actions, procIO.ReqAction)
+		}
+	} else if apiStream.GetType().IsResponseType() {
+		if procIO.IsResponseActionAvailable() {
+			actions.Response.Actions = append(actions.Response.Actions, procIO.RespAction)
+		}
+	} else {
+		return fmt.Errorf("unknown stream type: %v", apiStream.GetType())
+	}
+
+	if procIO.Type.IsResponseType() && apiStream.GetType().IsRequestType() {
+		// Case of early response. We should perform walk on response flow.
+		// Walk on response flow should be started from node with key equal to the key of current node
+		node, err = flow.GetResponseDirection().GetNode(node.GetProcessorKey())
+		if err != nil {
+			return fmt.Errorf("failed to get response node: %w", err)
+		}
+	}
+
+	for _, edge := range node.GetEdges() {
+		if !edge.IsNodeAvailable() {
+			// if no node is available, it means node connects to stream, meaning 'end of walk'
+			continue
+		}
+		// Check if the condition is met. procIO.Name - is name of condition.
+		// Condition can be met either if it's defined, of if it's just empty string -
+		// meaning there is no condition defined (procIO.Name is empty).
+		if edge.GetCondition() == procIO.Name {
+			targetNode := edge.GetTargetNode()
+			if err := s.ExecuteFlow(flow, apiStream, targetNode, actions); err != nil {
+				return fmt.Errorf("failed to execute flow: %w", err)
+			}
+		}
+	}
+	return nil
+}
