3289d98054db5a7dde2ca0fb4d8d785194078eea
Dmitry Gorohovsky <35920455+d-goro@users.noreply.github.com>
2024-10-01 13:28:45 +0300

    fix status error log in flow metrics (#656)
    Co-authored-by: d-goro <dmitryg@lunar.dev>

---
+++ proxy/src/services/lunar-engine/streams/filter/filter_node.go
@@ -0,0 +1,33 @@
+package streamfilter
+
+import (
+	internaltypes "lunar/engine/streams/internal-types"
+	publictypes "lunar/engine/streams/public-types"
+)
+
+type FilterNode struct {
+	filter publictypes.FilterI
+	flow   internaltypes.FlowI
+}
+
+func (node *FilterNode) getFlow(apiStream publictypes.APIStreamI) internaltypes.FlowI {
+	/* Get flow based on the API stream,
+	   the function will validate the stream based on the filter */
+	if !node.isHeadersQualified(apiStream) {
+		return nil
+	}
+
+	if apiStream.GetResponse() != nil && !node.isStatusCodeQualified(apiStream) {
+		return nil
+	}
+
+	if !node.isMethodQualified(apiStream) {
+		return nil
+	}
+
+	if apiStream.GetRequest() != nil && !node.isQueryParamsQualified(apiStream) {
+		return nil
+	}
+
+	return node.flow
+}
