6555519c5fed74b1189fc5152242f58a343b9275
eitangol <108608289+eitangol@users.noreply.github.com>
2024-10-09 20:13:04 +0300

    Core 1240 flows support same level flows different methods headers etc (#665)
    * Added the required validation to support same level flows
    
    * Small validation fixes
    
    * Small fix to the gitignore
    
    * CR FIX

---
+++ proxy/src/services/lunar-engine/runner/diagnosis_worker.go
@@ -0,0 +1,178 @@
+package runner
+
+import (
+	"lunar/engine/config"
+	"lunar/engine/messages"
+	"lunar/engine/services"
+	"lunar/engine/utils"
+	sharedConfig "lunar/shared-model/config"
+	contextmanager "lunar/toolkit-core/context-manager"
+	"runtime"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+type DiagnosisTask struct {
+	Request  messages.OnRequest
+	Response messages.OnResponse
+}
+
+type DiagnosisWorker struct {
+	diagnosisCache utils.Cache[string, DiagnosisTask]
+	diagnosisData  chan string
+}
+
+const (
+	// cacheTTL: The time that the data will be stored in the cache
+	// before being deleted. The default will be 2 minutes.
+	cacheTTL float64 = 60 * 2
+	// channelBufferSize: The size of the channel buffer,
+	// the number of messages that can be stored without blocking the flow.
+	channelBufferSize int = 128
+)
+
+func NewDiagnosisWorker() *DiagnosisWorker {
+	return &DiagnosisWorker{
+		diagnosisCache: utils.NewMemoryCache[string, DiagnosisTask](contextmanager.Get().GetClock()),
+		diagnosisData:  make(chan string, channelBufferSize),
+	}
+}
+
+func (worker *DiagnosisWorker) AddRequestToTask(onRequest messages.OnRequest) {
+	var emptyResponse messages.OnResponse
+
+	cacheKey := strings.Clone(onRequest.ID)
+	log.Trace().Msgf("Adding request data to the cache with key: %v",
+		cacheKey)
+
+	err := worker.diagnosisCache.Set(
+		cacheKey,
+		DiagnosisTask{Request: onRequest.DeepCopy(), Response: emptyResponse},
+		cacheTTL)
+	if err != nil {
+		log.Warn().
+			Msgf("Failed to cache key: %v, cache: %+v. %+v",
+				cacheKey, worker.diagnosisCache, err)
+		return
+	}
+	log.Debug().Msgf("Cache after adding request: %+v", worker.diagnosisCache)
+}
+
+func (worker *DiagnosisWorker) AddResponseToTask(
+	onResponse messages.OnResponse,
+) {
+	cacheKey := strings.Clone(onResponse.ID)
+	task, found := worker.diagnosisCache.Get(cacheKey)
+
+	if !found {
+		log.Warn().
+			Msgf("Failed to find transaction for key: %v, cache: %+v",
+				cacheKey, worker.diagnosisCache)
+		return
+	}
+
+	task.Response = onResponse.DeepCopy()
+	log.Trace().Msgf(
+		"Adding response data to the cache with key: %v, value: %+v",
+		cacheKey,
+		task.Response,
+	)
+
+	err := worker.diagnosisCache.Set(cacheKey, task, cacheTTL)
+	if err != nil {
+		log.Warn().
+			Msgf("Failed to cache key: %v, cache: %+v. %+v",
+				cacheKey, worker.diagnosisCache, err)
+		return
+	}
+
+	log.Trace().Msgf("Cache after adding response: %+v", worker.diagnosisCache)
+}
+
+func (worker *DiagnosisWorker) NotifyTaskReady(transactionID string) {
+	// This is executed in a separate goroutine
+	// to avoid blocking the flow if the channel is full.
+	log.Trace().Msgf(
+		"Scheduling goroutine to send %v to diagnosis worker", transactionID)
+	copyOfTransactionID := strings.Clone(transactionID)
+	go func(transactionID string) {
+		log.Trace().Msgf("Sending %v to diagnosis worker", transactionID)
+		worker.diagnosisData <- transactionID
+	}(copyOfTransactionID)
+}
+
+func (worker *DiagnosisWorker) Run(
+	policiesAccessor config.PoliciesAccessor,
+	plugins *services.DiagnosisPlugins,
+	exporters *services.Exporters,
+) {
+	go worker.diagnosisWorker(
+		worker.diagnosisData,
+		policiesAccessor,
+		plugins,
+		exporters,
+	)
+}
+
+func (worker *DiagnosisWorker) Stop() {
+	log.Info().Msg("Stopping the Diagnosis Worker...")
+	close(worker.diagnosisData)
+}
+
+func (worker *DiagnosisWorker) diagnosisWorker(
+	diagnosisTasks <-chan string,
+	policiesAccessor config.PoliciesAccessor,
+	plugins *services.DiagnosisPlugins,
+	exporters *services.Exporters,
+) {
+	sublogger := log.With().
+		Str("component", "diagnosis-worker").
+		Logger() // TODO: How to share the logger down the call stack?
+
+	for taskKey := range diagnosisTasks {
+		task, found := worker.diagnosisCache.Get(taskKey)
+		if !found {
+			sublogger.Error().Msgf(
+				"Failed to find transaction for key: %v, cache keys: %+v",
+				taskKey,
+				worker.diagnosisCache,
+			)
+		}
+
+		policiesData := policiesAccessor.GetTxnPoliciesData(
+			config.TxnID(taskKey),
+		)
+
+		RunTask(
+			task,
+			&policiesData.EndpointPolicyTree,
+			policiesData.Config.Global.Diagnosis,
+			plugins,
+			exporters,
+		)
+
+		// Set the function as low priority to give more runtime to the remedy types.
+		runtime.Gosched()
+	}
+}
+
+func RunTask(
+	task DiagnosisTask,
+	policyTree *config.EndpointPolicyTree,
+	globalDiagnoses []sharedConfig.Diagnosis,
+	plugins *services.DiagnosisPlugins,
+	exporters *services.Exporters,
+) {
+	diagnoses := getDiagnoses(
+		task.Request.Method, task.Request.URL, policyTree, globalDiagnoses)
+
+	runOnTransaction(
+		task.Request,
+		task.Response,
+		diagnoses,
+		plugins,
+		exporters,
+		policyTree,
+	)
+}
---
+++ proxy/src/services/lunar-engine/streams/config/streams.model.go
@@ -0,0 +1,59 @@
+package streamconfig
+
+import (
+	publictypes "lunar/engine/streams/public-types"
+	"lunar/toolkit-core/network"
+)
+
+type FlowRepresentation struct {
+	Name       string                `yaml:"name"`
+	Filter     *Filter               `yaml:"filter"`
+	Processors map[string]*Processor `yaml:"processors"` // key (processor key)
+	Flow       Flow                  `yaml:"flow"`
+	Data       network.ConfigurationPayload
+}
+type Flow struct {
+	Request  []*FlowConnection `yaml:"request"`
+	Response []*FlowConnection `yaml:"response"`
+}
+
+type FlowConnection struct {
+	From *Connection `yaml:"from"`
+	To   *Connection `yaml:"to"`
+}
+
+type Connection struct {
+	Stream    *StreamRef    `yaml:"stream,omitempty"`
+	Flow      *FlowRef      `yaml:"flow,omitempty"`
+	Processor *ProcessorRef `yaml:"processor,omitempty"`
+}
+type FlowRef struct {
+	Name string `yaml:"name"` // name of the flow to connect from|into
+	At   string `yaml:"at"`   // (start | end)
+}
+
+type StreamRef struct {
+	Name string `yaml:"name"`
+	At   string `yaml:"at"` // (start | end)
+}
+type ProcessorRef struct {
+	Name      string `yaml:"name"`                // processor key
+	Condition string `yaml:"condition,omitempty"` // Linked Processor Output
+}
+
+type Filter struct {
+	Name        string                 `yaml:"name"`
+	URL         string                 `yaml:"url"`
+	QueryParams []publictypes.KeyValue `yaml:"query_params,omitempty"`
+	Method      []string               `yaml:"method,omitempty"`
+	Headers     []publictypes.KeyValue `yaml:"headers,omitempty"`
+	StatusCode  []int                  `yaml:"status_code,omitempty"`
+}
+
+// This will assist in comparing the filters, we drop the name as it is not relevant for comparison.
+type Processor struct {
+	Processor  string                  `yaml:"processor"`
+	Parameters []*publictypes.KeyValue `yaml:"parameters,omitempty"`
+	// Key will be set by the engine as a unique key for the processor
+	Key string
+}
