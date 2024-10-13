dc04a3adcc96a7a437f99ee8e1a6a594ab653891
tal-lunar <122523439+tal-lunar@users.noreply.github.com>
2024-10-13 17:11:00 +0300

    updated docs and diagrams (#683)
    * updated docs and diagrams
    
    * CR Fixes
    
    * Uploaded Diagrams
    
    * CR Fixes 2
    
    * CR Fixes 3

---
+++ proxy/src/services/lunar-engine/metrics/metrics_model.go
@@ -0,0 +1,17 @@
+package metrics
+
+type MetricValue struct {
+	Name     Metric     `yaml:"name"`
+	Type     MetricType `yaml:"type"`
+	JSONPath string     `yaml:"json_path,omitempty"`
+}
+
+type GeneralMetrics struct {
+	LabelValue  []MetricLabel `yaml:"label_value"`
+	MetricValue []MetricValue `yaml:"metric_value"`
+}
+
+type Config struct {
+	GeneralMetrics GeneralMetrics `yaml:"general_metrics"`
+	SystemMetrics  []MetricValue  `yaml:"system_metrics"`
+}
