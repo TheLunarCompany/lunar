0f62c7aff05b7d31d966761fea411aad767079d2
Dmitry Gorohovsky <35920455+d-goro@users.noreply.github.com>
2024-10-16 11:38:50 +0300

    fix header case sensitivity (#695)
    * fix header case sensitivity
    
    * add comments
    
    ---------
    
    Co-authored-by: d-goro <dmitryg@lunar.dev>

---
+++ proxy/src/services/lunar-engine/streams/processors/mock/mock_processor.go
@@ -0,0 +1,44 @@
+package processormock
+
+import (
+	publictypes "lunar/engine/streams/public-types"
+	streamtypes "lunar/engine/streams/types"
+
+	"github.com/rs/zerolog/log"
+)
+
+type mockProcessor struct {
+	name     string
+	arg1     int
+	arg2     string
+	metaData *streamtypes.ProcessorMetaData
+}
+
+func NewProcessor(metaData *streamtypes.ProcessorMetaData) (streamtypes.Processor, error) {
+	mockProc := &mockProcessor{
+		name:     metaData.Name,
+		metaData: metaData,
+	}
+
+	if val, ok := metaData.Parameters["arg1"]; ok {
+		mockProc.arg1 = val.Value.GetInt()
+	}
+
+	if val, ok := metaData.Parameters["arg2"]; ok {
+		mockProc.arg2 = val.Value.GetString()
+	}
+
+	return mockProc, nil
+}
+
+func (p *mockProcessor) Execute(_ publictypes.APIStreamI) (streamtypes.ProcessorIO, error) {
+	log.Info().Int("arg1", p.arg1).Str("arg2", p.arg2).Msgf("Executing mock processor %s", p.name)
+	return streamtypes.ProcessorIO{
+		Type: publictypes.StreamTypeAny,
+		Name: "",
+	}, nil
+}
+
+func (p *mockProcessor) GetName() string {
+	return p.name
+}
---
+++ proxy/src/services/lunar-engine/streams/types/response.util.go
@@ -0,0 +1,115 @@
+package streamtypes
+
+import (
+	"fmt"
+	"lunar/engine/messages"
+	publictypes "lunar/engine/streams/public-types"
+	"lunar/engine/utils"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func NewResponse(onResponse messages.OnResponse) publictypes.TransactionI {
+	return &OnResponse{
+		id:         onResponse.ID,
+		sequenceID: onResponse.SequenceID,
+		method:     onResponse.Method,
+		url:        onResponse.URL,
+		status:     onResponse.Status,
+		headers:    onResponse.Headers,
+		body:       onResponse.Body,
+		time:       onResponse.Time,
+	}
+}
+
+func (res *OnResponse) IsNewSequence() bool {
+	return res.id == res.sequenceID
+}
+
+func (res *OnResponse) DoesHeaderExist(headerName string) bool {
+	_, found := res.GetHeader(headerName)
+	return found
+}
+
+func (res *OnResponse) DoesHeaderValueMatch(headerName, headerValue string) bool {
+	if existingHeaderValue, found := res.GetHeader(headerName); found {
+		return strings.EqualFold(existingHeaderValue, headerValue)
+	}
+	return false
+}
+
+func (res *OnResponse) Size() int {
+	if res.size > 0 {
+		return res.size
+	}
+
+	if sizeStr, ok := res.headers["Content-Length"]; ok {
+		size, _ := strconv.Atoi(sizeStr)
+		res.size = size
+		return res.size
+	}
+
+	if res.body != "" {
+		res.size = len(res.body)
+		return len(res.body)
+	}
+	return 0
+}
+
+func (res *OnResponse) DoesQueryParamExist(string) bool {
+	return false
+}
+
+func (res *OnResponse) DoesQueryParamValueMatch(string, string) bool {
+	return false
+}
+
+func (res *OnResponse) GetID() string {
+	return res.id
+}
+
+func (res *OnResponse) GetSequenceID() string {
+	return res.sequenceID
+}
+
+func (res *OnResponse) GetMethod() string {
+	return res.method
+}
+
+func (res *OnResponse) GetURL() string {
+	return res.url
+}
+
+func (res *OnResponse) GetStatus() int {
+	return res.status
+}
+
+func (res *OnResponse) GetHeader(key string) (string, bool) {
+	// TODO: can we make this more efficient by storing the headers in lowercase?
+	value, found := utils.MakeHeadersLowercase(res.headers)[strings.ToLower(key)]
+	if !found {
+		return "", false
+	}
+	return value, true
+}
+
+func (res *OnResponse) GetHeaders() map[string]string {
+	return res.headers
+}
+
+func (res *OnResponse) GetBody() string {
+	return res.body
+}
+
+func (res *OnResponse) GetTime() time.Time {
+	return res.time
+}
+
+// NewResponseAPIStream creates a new APIStream with the given OnResponse
+func NewResponseAPIStream(onResponse messages.OnResponse) publictypes.APIStreamI {
+	name := fmt.Sprintf("ResponseAPIStream-%s", onResponse.ID)
+	apiStream := NewAPIStream(name, publictypes.StreamTypeResponse)
+	apiStream.SetResponse(NewResponse(onResponse))
+	return apiStream
+}
