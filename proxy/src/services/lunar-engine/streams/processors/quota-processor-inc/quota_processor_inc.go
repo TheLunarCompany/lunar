21ceb56508c49392eed6887693cd973c7a0690bd
eitangol <108608289+eitangol@users.noreply.github.com>
2024-10-21 08:55:43 +0300

    Update lunar_hub.go (#703)

---
+++ proxy/src/services/lunar-engine/streams/processors/quota-processor-inc/quota_processor_inc.go
@@ -0,0 +1,56 @@
+package quotaprocessorinc
+
+import (
+	"lunar/engine/streams/processors/utils"
+	publictypes "lunar/engine/streams/public-types"
+	streamtypes "lunar/engine/streams/types"
+
+	"github.com/rs/zerolog/log"
+)
+
+const quotaParam = "quota_id"
+
+type quotaProcessorInc struct {
+	name     string
+	quotaID  string
+	metaData *streamtypes.ProcessorMetaData
+}
+
+func NewProcessor(
+	metaData *streamtypes.ProcessorMetaData,
+) (streamtypes.Processor, error) {
+	proc := &quotaProcessorInc{
+		name:     metaData.Name,
+		metaData: metaData,
+	}
+	log.Trace().Msgf("quotaProcessorInc Params: %v", metaData.Parameters)
+	if err := utils.ExtractStrParam(metaData.Parameters,
+		quotaParam,
+		&proc.quotaID); err != nil {
+		log.Trace().Msgf("quotaID not defined for %v", metaData.Name)
+	}
+
+	return proc, nil
+}
+
+func (p *quotaProcessorInc) GetName() string {
+	return p.name
+}
+
+func (p *quotaProcessorInc) Execute(
+	apiStream publictypes.APIStreamI,
+) (streamtypes.ProcessorIO, error) {
+	quota, err := p.metaData.Resources.GetQuota(p.quotaID, apiStream.GetID())
+	if err != nil {
+		return streamtypes.ProcessorIO{}, err
+	}
+
+	if err := quota.Inc(apiStream); err != nil {
+		return streamtypes.ProcessorIO{}, err
+	}
+
+	return streamtypes.ProcessorIO{
+		Type: apiStream.GetType(),
+		Name: "",
+	}, nil
+}
