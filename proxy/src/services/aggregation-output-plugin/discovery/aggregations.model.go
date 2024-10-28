68f65fb2425c7a589be0b30a3fd466a34cfdcc2d
eitangol <108608289+eitangol@users.noreply.github.com>
2024-10-28 13:27:49 +0200

    Set a default value to the interceptor version, so we by default set … (#714)
    * Set a default value to the interceptor version, so we by default set it to be "direct"
    
    * Update discovered_endpoint_metrics.feature

---
+++ proxy/src/services/aggregation-output-plugin/discovery/aggregations.model.go
@@ -0,0 +1,37 @@
+package discovery
+
+import (
+	"lunar/aggregation-plugin/common"
+	sharedDiscovery "lunar/shared-model/discovery"
+)
+
+type AccessLog common.AccessLog
+
+type (
+	Count int
+
+	EndpointMapping map[sharedDiscovery.Endpoint]EndpointAgg
+
+	Agg struct {
+		Interceptors map[common.Interceptor]InterceptorAgg
+		Endpoints    map[sharedDiscovery.Endpoint]EndpointAgg
+		Consumers    map[string]EndpointMapping
+	}
+
+	InterceptorAgg struct {
+		Timestamp int64
+	}
+
+	EndpointAgg struct {
+		MinTime int64
+		MaxTime int64
+
+		Count           Count
+		StatusCodes     map[int]Count
+		AverageDuration float32
+	}
+)
+
+func (agg EndpointAgg) totalDuration() float32 {
+	return agg.AverageDuration * float32(agg.Count)
+}
