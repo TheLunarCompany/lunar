4de46d6e1f7f6364abc695da07c816eb36e03e03
Dmitry Gorohovsky <35920455+d-goro@users.noreply.github.com>
2024-10-29 15:28:04 +0200

    Core 1297 count api call count metric so it will include every api (#716)
    * aggregate plugin
    
    * update metric
    
    * merge
    
    * lint
    
    * lint
    
    * fix test
    
    * support labels
    
    * refactor
    
    * refactor
    
    * refactor
    
    * package
    
    * lint
    
    ---------
    
    Co-authored-by: d-goro <dmitryg@lunar.dev>

---
+++ proxy/src/services/aggregation-output-plugin/discovery/decoding_test.go
@@ -0,0 +1,205 @@
+package discovery
+
+import (
+	"lunar/aggregation-plugin/common"
+	"testing"
+
+	sharedActions "lunar/shared-model/actions"
+	sharedConfig "lunar/shared-model/config"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// These tests are currently testing the private function `decodeRecord`.
+// If we can find a way to make up the input to the public `DecodeRecords`,
+// It would be preferable to test all these through that function.
+
+func TestDecodeRecordSucceedsWhenKeysAreStringAndValuesAreCompatibleBytes(
+	t *testing.T,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+		"message": []byte(
+			`{` +
+				`"timestamp":1675697113071,"duration":190,"method":"GET",` +
+				`"host":"httpbin.org","url":"httpbin.org/status/{code}",` +
+				`"status_code":402,` +
+				`"request_active_remedies":{"fixed_response":["no_op"]},` +
+				`"response_active_remedies":{}` +
+				`}`,
+		),
+	}
+
+	res, err := decodeRecord(record)
+	assert.Nil(t, err)
+	want := common.AccessLog{
+		Timestamp:  1675697113071,
+		Duration:   190,
+		Method:     "GET",
+		Host:       "httpbin.org",
+		URL:        "httpbin.org/status/{code}",
+		StatusCode: 402,
+		RequestActiveRemedies: common.RequestActiveRemedies{
+			sharedConfig.RemedyFixedResponse: []sharedActions.RemedyReqRunResult{
+				sharedActions.ReqNoOp,
+			},
+		},
+		ResponseActiveRemedies: common.ResponseActiveRemedies{},
+	}
+
+	assert.True(t, res.decoded)
+	assert.Equal(t, *res.accessLog, want)
+}
+
+func TestDecodeRecordFailsWhenKeysAreNotString(
+	t *testing.T,
+) {
+	record := map[any]any{
+		1: []byte("Feb  6 15:25:20"),
+		2: []byte("haproxy[229]"),
+		3: []byte(
+			`{"timestamp":1675697113071,"duration":190,"method":"GET",` +
+				`"url":"httpbin.org/status/{code}","status_code":402` +
+				`"host":"httpbin.org"` +
+				`"request_active_remedies":{},` +
+				`"response_active_remedies":{}` +
+				`}`,
+		),
+	}
+
+	_, err := decodeRecord(record)
+	assert.NotNil(t, err)
+}
+
+func TestDecodeRecordWithAllRemedyTypes(
+	t *testing.T,
+) {
+	const numberOfRemedyTypes = 1000
+	for i := 0; i < numberOfRemedyTypes; i++ {
+		remedyType := sharedConfig.RemedyType(i)
+		switch remedyType {
+		case sharedConfig.RemedyAccountOrchestration:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyStrategyBasedQueue:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyStrategyBasedThrottling:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyResponseBasedThrottling:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyConcurrencyBasedThrottling:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyCaching:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyFixedResponse:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyRetry:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyAuth:
+			testDecodeRecordWithRemedyType(t, remedyType)
+		case sharedConfig.RemedyUndefined:
+			continue
+		}
+	}
+}
+
+func testDecodeRecordWithRemedyType(
+	t *testing.T,
+	remedyType sharedConfig.RemedyType,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+		"message": []byte(
+			`{` +
+				`"timestamp":1675697113071,"duration":190,"method":"GET",` +
+				`"host":"httpbin.org","url":"httpbin.org/status/{code}",` +
+				`"status_code":402,` +
+				`"request_active_remedies":{"` + remedyType.String() +
+				`":["modified_request"]},` +
+				`"response_active_remedies":{}` +
+				`}`,
+		),
+	}
+
+	_, err := decodeRecord(record)
+	assert.Nil(t, err)
+}
+
+func TestDecodeRecordFailsWhenMessageKeyIsNotFound(
+	t *testing.T,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+	}
+
+	_, err := decodeRecord(record)
+	assert.NotNil(t, err)
+}
+
+func TestDecodeRecordFailsWhenMessageValueIsNotBytes(
+	t *testing.T,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+		"message": `{"timestamp":1675697113071,"duration":190,"method":"GET",` +
+			`"url":"httpbin.org/status/{code}","status_code":402` +
+			`"host":"httpbin.org"` +
+			`"request_active_remedies":{},` +
+			`"response_active_remedies":{}` +
+			`}`,
+	}
+
+	_, err := decodeRecord(record)
+	assert.NotNil(t, err)
+}
+
+func TestDecodeRecordReturnsZeroWhenMessageValueBytesAreIncompatible(
+	t *testing.T,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+		"message": []byte("not-a-json"),
+	}
+
+	accessLogResponse, err := decodeRecord(record)
+	assert.Nil(t, err)
+	assert.False(t, accessLogResponse.decoded)
+}
+
+func TestDecodeRecordFailsWhenURLIsADash(
+	t *testing.T,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+		"message": []byte(
+			`{"timestamp":1675697113071,"duration":190,"method":"GET",` +
+				`"url":"-","status_code":402` +
+				`"host":"httpbin.org"` +
+				`"request_active_remedies":{},` +
+				`"response_active_remedies":{}` +
+				`}`,
+		),
+	}
+
+	_, err := decodeRecord(record)
+	assert.NotNil(t, err)
+}
+
+func TestDecodedFalseForMissingMessageFields(
+	t *testing.T,
+) {
+	record := map[any]any{
+		"time":    []byte("Feb  6 15:25:20"),
+		"service": []byte("haproxy[229]"),
+		"message": []byte(`{}`),
+	}
+
+	res, err := decodeRecord(record)
+	assert.Nil(t, err)
+	assert.False(t, res.decoded)
+}
---
+++ proxy/src/services/lunar-engine/streams/processors/processors_test.go
@@ -0,0 +1,192 @@
+package processors
+
+import (
+	filterprocessor "lunar/engine/streams/processors/filter-processor"
+	publictypes "lunar/engine/streams/public-types"
+	streamtypes "lunar/engine/streams/types"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewFilterProcessor(t *testing.T) {
+	metaData := &streamtypes.ProcessorMetaData{
+		Name:       "testProcessor",
+		Parameters: createFilterProcessorParams("http://example.com", "GET", "body", ""),
+	}
+	processor, err := filterprocessor.NewProcessor(metaData)
+	require.NoError(t, err)
+	require.NotNil(t, processor)
+	require.Equal(t, "testProcessor", processor.GetName())
+}
+
+func TestFilterProcessorInit(t *testing.T) {
+	metaData := &streamtypes.ProcessorMetaData{
+		Name:       "testProcessor",
+		Parameters: createFilterProcessorParams("http://example.com", "GET", "body", "X-Group=production"),
+	}
+
+	processor, err := filterprocessor.NewProcessor(metaData)
+	require.NoError(t, err)
+
+	// Use reflection to access private fields for testing purposes
+	processorValue := reflect.ValueOf(processor).Elem()
+	url := processorValue.FieldByName("url").String()
+	method := processorValue.FieldByName("method").String()
+	body := processorValue.FieldByName("body").String()
+	headerKey := processorValue.FieldByName("headerKey").String()
+	headerValue := processorValue.FieldByName("headerValue").String()
+
+	require.Equal(t, "http://example.com", url)
+	require.Equal(t, "GET", method)
+	require.Equal(t, "body", body)
+	require.Equal(t, "X-Group", headerKey)
+	require.Equal(t, "production", headerValue)
+}
+
+func TestFilterProcessorExecute(t *testing.T) {
+	testCases := []struct {
+		name              string
+		filterURL         string
+		method            string
+		body              string
+		headers           map[string]string
+		expectedCondition string
+		apiStream         *mockAPIStream
+	}{
+		{
+			name:              "Match all conditions",
+			filterURL:         "http://example.com",
+			method:            "GET",
+			body:              "body",
+			headers:           map[string]string{"X-Domain-Access": "production"},
+			expectedCondition: filterprocessor.HitConditionName,
+			apiStream: &mockAPIStream{
+				url:     "http://example.com",
+				method:  "GET",
+				body:    "body",
+				headers: map[string]string{"X-Domain-Access": "production"},
+			},
+		},
+		{
+			name:              "Header mismatch",
+			filterURL:         "http://example.com",
+			method:            "GET",
+			body:              "body",
+			headers:           map[string]string{"X-Domain-Access": "production"},
+			expectedCondition: filterprocessor.MissConditionName,
+			apiStream: &mockAPIStream{
+				url:     "http://example.com",
+				method:  "GET",
+				body:    "body",
+				headers: map[string]string{"X-Domain-Access": "development"},
+			},
+		},
+		{
+			name:              "Domain match",
+			filterURL:         "example.com",
+			expectedCondition: filterprocessor.HitConditionName,
+			apiStream: &mockAPIStream{
+				url: "http://example.com/test",
+			},
+		},
+		{
+			name:              "Domain wildcard match",
+			filterURL:         "example.com/*",
+			expectedCondition: filterprocessor.HitConditionName,
+			apiStream: &mockAPIStream{
+				url: "http://example.com/test",
+			},
+		},
+		{
+			name:              "Domain with path wildcard match",
+			filterURL:         "example.com/test/*",
+			expectedCondition: filterprocessor.HitConditionName,
+			apiStream: &mockAPIStream{
+				url: "http://example.com/test/something",
+			},
+		},
+		{
+			name:              "Domain with path wildcard mismatch",
+			filterURL:         "example.com/test/headers/*/something",
+			expectedCondition: filterprocessor.MissConditionName,
+			apiStream: &mockAPIStream{
+				url: "http://example.com/test/headers/not/expected",
+			},
+		},
+		{
+			name:              "Regex URL match",
+			filterURL:         "http://example.com/*",
+			method:            "GET",
+			body:              "body",
+			headers:           map[string]string{"X-Domain-Access": "production"},
+			expectedCondition: filterprocessor.HitConditionName,
+			apiStream: &mockAPIStream{
+				url:     "http://example.com/test",
+				method:  "GET",
+				body:    "body",
+				headers: map[string]string{"X-Domain-Access": "production"},
+			},
+		},
+		{
+			name:              "Regex URL with protocol match",
+			filterURL:         "http*://example.com/headers/*/something",
+			method:            "GET",
+			headers:           map[string]string{"X-Domain-Access": "production"},
+			expectedCondition: filterprocessor.HitConditionName,
+			apiStream: &mockAPIStream{
+				url:     "https://example.com/headers/test/something",
+				method:  "GET",
+				body:    "body",
+				headers: map[string]string{"X-Domain-Access": "production"},
+			},
+		},
+		{
+			name:              "Method mismatch",
+			filterURL:         "https://example.com/headers/test/something",
+			method:            "POST",
+			headers:           map[string]string{"X-Domain-Access": "production"},
+			expectedCondition: filterprocessor.MissConditionName,
+			apiStream: &mockAPIStream{
+				url:     "https://example.com/headers/test/something",
+				method:  "GET",
+				body:    "body",
+				headers: map[string]string{"X-Domain-Access": "production"},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			metaData := &streamtypes.ProcessorMetaData{
+				Name:       "testProcessor",
+				Parameters: createFilterProcessorParams(tc.filterURL, tc.method, tc.body, "X-Domain-Access="+tc.headers["X-Domain-Access"]),
+			}
+
+			processor, err := filterprocessor.NewProcessor(metaData)
+			require.NoError(t, err)
+
+			output, err := processor.Execute("", tc.apiStream)
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedCondition, output.Name)
+		})
+	}
+}
+
+func createFilterProcessorParams(url, method, body, header string) map[string]streamtypes.ProcessorParam {
+	paramMap := make(map[string]streamtypes.ProcessorParam)
+	if url != "" {
+		paramMap[filterprocessor.URLParam] = streamtypes.ProcessorParam{Name: filterprocessor.URLParam, Value: publictypes.NewParamValue(url)}
+	}
+	if method != "" {
+		paramMap[filterprocessor.MethodParam] = streamtypes.ProcessorParam{Name: filterprocessor.MethodParam, Value: publictypes.NewParamValue(method)}
+	}
+	if body != "" {
+		paramMap[filterprocessor.BodyParam] = streamtypes.ProcessorParam{Name: filterprocessor.BodyParam, Value: publictypes.NewParamValue(body)}
+	}
+	if header != "" {
+		paramMap[filterprocessor.HeaderParam] = streamtypes.ProcessorParam{Name: filterprocessor.HeaderParam, Value: publictypes.NewParamValue(header)}
+	}
+	return paramMap
+}
