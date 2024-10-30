7d497ca4d507aeda10414f4f8b05260019ad7e2a
tal-lunar <122523439+tal-lunar@users.noreply.github.com>
2024-10-30 20:25:09 +0200

    Core-1307 - flows on/off (#725)
    * Core-1307 - flows on/off
    
    * Update interval_unit reference

---
+++ proxy/src/services/aggregation-output-plugin/discovery/aggregation_converge_test.go
@@ -0,0 +1,145 @@
+package discovery_test
+
+import (
+	"lunar/aggregation-plugin/common"
+	"lunar/aggregation-plugin/discovery"
+	sharedDiscovery "lunar/shared-model/discovery"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfNoNewURLsAddedAggRemainsTheSame(t *testing.T) {
+	t.Parallel()
+	endpointA := sharedDiscovery.Endpoint{
+		Method: "GET",
+		URL:    "api.com/user/1",
+	}
+
+	endpointB := sharedDiscovery.Endpoint{
+		Method: "GET",
+		URL:    "api.com/user/2",
+	}
+
+	interceptorA := common.Interceptor{
+		Type:    "lunar-aiohttp-interceptor",
+		Version: "2.0.2",
+	}
+
+	initial := discovery.Agg{
+		Endpoints: map[sharedDiscovery.Endpoint]discovery.EndpointAgg{
+			endpointA: endpointAggA(),
+			endpointB: endpointAggB(),
+		},
+		Interceptors: map[common.Interceptor]discovery.InterceptorAgg{
+			interceptorA: interceptorAggA(),
+		},
+		Consumers: map[string]discovery.EndpointMapping{
+			"consumerA": {
+				endpointA: endpointAggA(),
+			},
+			"consumerB": {
+				endpointB: endpointAggB(),
+			},
+		},
+	}
+
+	maxSplitThreshold := 2
+	tree, err := common.BuildTree(
+		sharedDiscovery.KnownEndpoints{
+			Endpoints: []sharedDiscovery.Endpoint{endpointA, endpointB},
+		},
+		maxSplitThreshold,
+	)
+	assert.Nil(t, err)
+
+	updatedAgg, err := discovery.ConvergeAggregation(
+		initial,
+		[]discovery.AccessLog{},
+		tree,
+	)
+	assert.Nil(t, err)
+	assert.Equal(t, initial, updatedAgg)
+}
+
+func TestIfNewURLsAddedAggEndpointsConverged_RegardlessOfMethod(t *testing.T) {
+	t.Parallel()
+	endpointA := sharedDiscovery.Endpoint{
+		Method: "GET",
+		URL:    "api.com/user/1",
+	}
+
+	endpointB := sharedDiscovery.Endpoint{
+		Method: "GET",
+		URL:    "api.com/user/2",
+	}
+
+	endpointC := sharedDiscovery.Endpoint{
+		Method: "POST",
+		URL:    "api.com/unrelated/1",
+	}
+
+	interceptorA := common.Interceptor{
+		Type:    "lunar-aiohttp-interceptor",
+		Version: "2.0.2",
+	}
+
+	initial := discovery.Agg{
+		Endpoints: map[sharedDiscovery.Endpoint]discovery.EndpointAgg{
+			endpointA: endpointAggA(),
+			endpointB: endpointAggB(),
+			endpointC: endpointAggC(),
+		},
+		Interceptors: map[common.Interceptor]discovery.InterceptorAgg{
+			interceptorA: interceptorAggA(),
+		},
+		Consumers: map[string]discovery.EndpointMapping{
+			"consumerA": {
+				endpointA: endpointAggA(),
+			},
+			"consumerB": {
+				endpointB: endpointAggB(),
+			},
+			"consumerC": {
+				endpointB: endpointAggC(),
+			},
+		},
+	}
+
+	maxSplitThreshold := 2
+	tree, err := common.BuildTree(
+		sharedDiscovery.KnownEndpoints{
+			Endpoints: []sharedDiscovery.Endpoint{endpointA, endpointB, endpointC},
+		},
+		maxSplitThreshold,
+	)
+	assert.Nil(t, err)
+
+	updatedAgg, err := discovery.ConvergeAggregation(
+		initial,
+		[]discovery.AccessLog{
+			{Method: "POST", URL: "api.com/user/3", ConsumerTag: "consumerC"},
+		},
+		tree,
+	)
+	assert.Nil(t, err)
+	assert.NotEqual(t, initial, updatedAgg)
+	assert.Len(t, updatedAgg.Endpoints, 2)
+	assert.Contains(t, updatedAgg.Endpoints, endpointC)
+	assert.Contains(t, updatedAgg.Consumers, "consumerC")
+
+	convergedEndpoint := sharedDiscovery.Endpoint{
+		Method: "GET",
+		URL:    "api.com/user/{_param_1}",
+	}
+	assert.Contains(t, updatedAgg.Endpoints, convergedEndpoint)
+	convergedEndpointAgg, exists := updatedAgg.Endpoints[convergedEndpoint]
+
+	assert.True(t, exists)
+
+	combinedEndpointAgg := endpointAggA().Combine(endpointAggB())
+	assert.Equal(t, combinedEndpointAgg, convergedEndpointAgg)
+
+	// Interceptors are not affected
+	assert.Equal(t, initial.Interceptors, updatedAgg.Interceptors)
+}
