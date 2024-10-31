6b12348a0aa4a00344c8c05cb96b4b03d64c3cf4
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-10-31 10:31:30 +0000

    Merge pull request #723 from TheLunarCompany/cloud-432-build-the-flow-builder
    feat: continued development of Flow Builder UI (WIP)

---
+++ proxy/src/services/aggregation-output-plugin/discovery/persistence_utils.go
@@ -0,0 +1,154 @@
+package discovery
+
+import (
+	"lunar/aggregation-plugin/common"
+	sharedActions "lunar/shared-model/actions"
+	sharedDiscovery "lunar/shared-model/discovery"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	endpointDelimiter = ":::"
+)
+
+func ConvertToPersisted(aggregations Agg) sharedDiscovery.Output {
+	output := sharedDiscovery.Output{
+		Interceptors: []sharedDiscovery.InterceptorOutput{},
+		Endpoints:    map[string]sharedDiscovery.EndpointOutput{},
+		Consumers:    map[string]map[string]sharedDiscovery.EndpointOutput{},
+	}
+
+	for endpoint, agg := range aggregations.Endpoints {
+		key := dumpEndpoint(endpoint)
+		output.Endpoints[key] = convertEndpointToPersisted(agg)
+	}
+
+	for consumer, endpoints := range aggregations.Consumers {
+		output.Consumers[consumer] = map[string]sharedDiscovery.EndpointOutput{}
+		for endpoint, agg := range endpoints {
+			key := dumpEndpoint(endpoint)
+			output.Consumers[consumer][key] = convertEndpointToPersisted(agg)
+		}
+	}
+
+	for interceptor, agg := range aggregations.Interceptors {
+		output.Interceptors = append(output.Interceptors, sharedDiscovery.InterceptorOutput{
+			Type:    interceptor.Type,
+			Version: interceptor.Version,
+			LastTransactionDate: sharedActions.TimestampToStringFromInt64(
+				agg.Timestamp),
+		})
+	}
+
+	return output
+}
+
+func ConvertFromPersisted(output sharedDiscovery.Output) *Agg {
+	aggregations := Agg{
+		Interceptors: map[common.Interceptor]InterceptorAgg{},
+		Endpoints:    map[sharedDiscovery.Endpoint]EndpointAgg{},
+		Consumers:    map[string]EndpointMapping{},
+	}
+
+	for key, endpoint := range output.Endpoints {
+		parts := strings.Split(key, endpointDelimiter)
+		minTime, err := sharedActions.TimestampFromStringToInt64(endpoint.MinTime)
+		if err != nil {
+			log.Error().Msgf("Error converting timestamp: %v", err)
+			minTime = 0
+		}
+		maxTime, err := sharedActions.TimestampFromStringToInt64(endpoint.MaxTime)
+		if err != nil {
+			log.Error().Msgf("Error converting timestamp: %v", err)
+			maxTime = 0
+		}
+		aggregations.Endpoints[sharedDiscovery.Endpoint{
+			Method: parts[0],
+			URL:    parts[1],
+		}] = convertEndpointFromPersisted(minTime, maxTime, endpoint)
+	}
+
+	for consumer, endpoints := range output.Consumers {
+		aggregations.Consumers[consumer] = map[sharedDiscovery.Endpoint]EndpointAgg{}
+		for key, endpoint := range endpoints {
+			parts := strings.Split(key, endpointDelimiter)
+			if len(parts) < 2 {
+				log.Error().Msgf("Invalid endpoint key: %v", key)
+				continue
+			}
+			minTime, err := sharedActions.TimestampFromStringToInt64(endpoint.MinTime)
+			if err != nil {
+				log.Error().Msgf("Error converting timestamp: %v", err)
+			}
+			maxTime, err := sharedActions.TimestampFromStringToInt64(endpoint.MaxTime)
+			if err != nil {
+				log.Error().Msgf("Error converting timestamp: %v", err)
+			}
+			aggregations.Consumers[consumer][sharedDiscovery.Endpoint{
+				Method: parts[0],
+				URL:    parts[1],
+			}] = convertEndpointFromPersisted(minTime, maxTime, endpoint)
+		}
+	}
+
+	for _, interceptor := range output.Interceptors {
+		timestamp, err := sharedActions.TimestampFromStringToInt64(interceptor.LastTransactionDate)
+		if err != nil {
+			log.Error().Msgf("Error converting timestamp: %v", err)
+			timestamp = 0
+		}
+		aggregations.Interceptors[common.Interceptor{
+			Type:    interceptor.Type,
+			Version: interceptor.Version,
+		}] = InterceptorAgg{
+			Timestamp: timestamp,
+		}
+	}
+
+	return &aggregations
+}
+
+func dumpEndpoint(endpoint sharedDiscovery.Endpoint) string {
+	return strings.Join([]string{endpoint.Method, endpoint.URL}, endpointDelimiter)
+}
+
+func convertMapOfCountToInt(counts map[int]Count) map[int]int {
+	result := make(map[int]int)
+	for key, value := range counts {
+		result[key] = int(value)
+	}
+	return result
+}
+
+func convertMapOfIntToCount(ints map[int]int) map[int]Count {
+	result := make(map[int]Count)
+	for key, value := range ints {
+		result[key] = Count(value)
+	}
+	return result
+}
+
+func convertEndpointFromPersisted(
+	minTime, maxTime int64,
+	endpoint sharedDiscovery.EndpointOutput,
+) EndpointAgg {
+	return EndpointAgg{
+		MinTime:         minTime,
+		MaxTime:         maxTime,
+		Count:           Count(endpoint.Count),
+		StatusCodes:     convertMapOfIntToCount(endpoint.StatusCodes),
+		AverageDuration: endpoint.AverageDuration,
+	}
+}
+
+func convertEndpointToPersisted(agg EndpointAgg) sharedDiscovery.EndpointOutput {
+	return sharedDiscovery.EndpointOutput{
+		MinTime:         sharedActions.TimestampToStringFromInt64(agg.MinTime),
+		MaxTime:         sharedActions.TimestampToStringFromInt64(agg.MaxTime),
+		Count:           int(agg.Count),
+		StatusCodes:     convertMapOfCountToInt(agg.StatusCodes),
+		AverageDuration: agg.AverageDuration,
+	}
+}
