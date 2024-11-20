5614ae8f2db3bf8892dbcf6cee0be60ce27262ae
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-11-20 08:59:03 +0000

    Merge pull request #789 from TheLunarCompany/cloud-482-add-an-apply-action-to-filtering-to-better-control-when-the
    feat: add Apply action to filtering and show loading skeletons.

---
+++ proxy/src/services/lunar-engine/services/remedies/fixed_response_plugin.go
@@ -0,0 +1,52 @@
+package remedies
+
+import (
+	"lunar/engine/actions"
+	"lunar/engine/messages"
+	sharedConfig "lunar/shared-model/config"
+	"lunar/toolkit-core/clock"
+
+	"github.com/rs/zerolog/log"
+)
+
+type FixedResponsePlugin struct {
+	counter int
+	clock   clock.Clock
+}
+
+func NewFixedResponsePlugin(clock clock.Clock) *FixedResponsePlugin {
+	return &FixedResponsePlugin{
+		counter: 0,
+		clock:   clock,
+	}
+}
+
+func (plugin *FixedResponsePlugin) OnRequest(
+	onRequest messages.OnRequest,
+	remedyConfig *sharedConfig.FixedResponseConfig,
+) (actions.ReqLunarAction, error) {
+	var lunarAction actions.ReqLunarAction = &actions.NoOpAction{}
+	plugin.counter++
+	log.Trace().Msgf("Counter: %v", plugin.counter)
+
+	if onRequest.Headers["Early-Response"] == "true" {
+		body := "{\"message\": \"GO Lunar\"}"
+		headers := map[string]string{"Powered-By": "Lunar Interventions Inc."}
+		lunarAction = &actions.EarlyResponseAction{
+			Status:  remedyConfig.StatusCode,
+			Body:    body,
+			Headers: headers,
+		}
+
+	}
+
+	return lunarAction, nil
+}
+
+func (plugin *FixedResponsePlugin) OnResponse(
+	onResponse messages.OnResponse,
+	_ *sharedConfig.FixedResponseConfig,
+) (actions.RespLunarAction, error) {
+	log.Trace().Msgf("OnResponse: %+v", onResponse)
+	return &actions.NoOpAction{}, nil
+}
