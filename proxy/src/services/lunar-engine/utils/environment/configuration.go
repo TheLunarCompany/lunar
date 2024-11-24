c22d8b0844bba7b3594c75aa6052c9fa58e7a4eb
Dmitry Gorohovsky <35920455+d-goro@users.noreply.github.com>
2024-11-24 15:05:53 +0200

    Core-1366-make-policies-metrics-work-without-diagnosis-plugin (#796)
    * move types to shared library
    
    * implement metrics
    
    * use const
    
    * logs
    
    * test
    
    * refactor
    
    ---------
    
    Co-authored-by: d-goro <dmitryg@lunar.dev>

---
+++ proxy/src/services/lunar-engine/utils/environment/configuration.go
@@ -0,0 +1,317 @@
+package environment
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	proxyVersionEnvVar                              string = "LUNAR_VERSION"
+	tenantNameEnvVar                                string = "TENANT_NAME"
+	haproxyManageEndpointsPortEnvVar                string = "HAPROXY_MANAGE_ENDPOINTS_PORT"
+	haproxyHealthcheckPortEnvVar                    string = "LUNAR_HEALTHCHECK_PORT"
+	redisURLEnvVar                                  string = "REDIS_URL"
+	redisUseCluster                                 string = "REDIS_USE_CLUSTER"
+	redisPrefix                                     string = "REDIS_PREFIX"
+	redisMaxRetryAttempts                           string = "REDIS_MAX_RETRY_ATTEMPTS"
+	redisRetryBackoffMillis                         string = "REDIS_RETRY_BACKOFF_MILLIS"
+	redisMaxOLRetryAttempts                         string = "REDIS_MAX_OPTIMISTIC_LOCKING_RETRY_ATTEMPTS" //nolint: lll
+	lunarAPIKeyEnvVar                               string = "LUNAR_API_KEY"
+	lunarHubURLEnvVar                               string = "LUNAR_HUB_URL"
+	lunarHubReportIntervalEnvVar                    string = "HUB_REPORT_INTERVAL"
+	discoveryStateLocationEnvVar                    string = "DISCOVERY_STATE_LOCATION"
+	apiCallsMetricsStateLocationEnvVar              string = "API_CALLS_METRICS_STATE_LOCATION"
+	remedyStatsStateLocationEnvVar                  string = "REMEDY_STATE_LOCATION"
+	streamsFeatureFlagEnvVar                        string = "LUNAR_STREAMS_ENABLED"
+	streamsFlowsDirectoryEnvVar                     string = "LUNAR_PROXY_FLOW_DIRECTORY"
+	QuotasDirectoryEnvVar                           string = "LUNAR_PROXY_QUOTAS_DIRECTORY"
+	PathParamsDirectoryEnvVar                       string = "LUNAR_FLOWS_PATH_PARAM_DIR"
+	processorsDirectoryEnvVar                       string = "LUNAR_PROXY_PROCESSORS_DIRECTORY"
+	userProcessorsDirectoryEnvVar                   string = "LUNAR_PROXY_USER_PROCESSORS_DIRECTORY"
+	proxyConfigPath                                 string = "LUNAR_PROXY_CONFIG"
+	lunarEngineFailsafeEnableEnvVar                 string = "LUNAR_ENGINE_FAILSAFE_ENABLED"
+	lunarProxyBindPortEnvVar                        string = "BIND_PORT"
+	logLevelEnvVar                                  string = "LOG_LEVEL"
+	lunarGatewayInstanceIDEnvVar                    string = "GATEWAY_INSTANCE_ID"
+	diagnosisFailsafeMinSecBetweenCallsEnvVar       string = "DIAGNOSIS_FAILSAFE_MIN_SEC_BETWEEN_CALLS"
+	diagnosisFailsafeConsecutiveNEnvVar             string = "DIAGNOSIS_FAILSAFE_CONSECUTIVE_N"
+	diagnosisFailsafeMinStableSecEnvVar             string = "DIAGNOSIS_FAILSAFE_MIN_STABLE_SEC"
+	diagnosisFailsafeCooldownSecEnvVar              string = "DIAGNOSIS_FAILSAFE_COOLDOWN_SEC"
+	diagnosisFailsafeHealthySessionRateEnvVar       string = "DIAGNOSIS_FAILSAFE_HEALTHY_SESSION_RATE"
+	diagnosisFailsafeHealthyMaxLastSessionSecEnvVar string = "DIAGNOSIS_FAILSAFE_HEALTHY_MAX_LAST_SESSION_SEC" //nolint: lll
+
+	lunarHubDefaultValue string = "hub.lunar.dev"
+)
+
+func GetGatewayInstanceID() string {
+	return os.Getenv(lunarGatewayInstanceIDEnvVar)
+}
+
+func GetBindPort() string {
+	return os.Getenv(lunarProxyBindPortEnvVar)
+}
+
+func GetLogLevel() string {
+	return os.Getenv(logLevelEnvVar)
+}
+
+func GetTenantName() string {
+	return os.Getenv(tenantNameEnvVar)
+}
+
+func IsEngineFailsafeEnabled() bool {
+	return parseBooleanEnvVar(lunarEngineFailsafeEnableEnvVar)
+}
+
+func GetDiscoveryStateLocation() string {
+	return os.Getenv(discoveryStateLocationEnvVar)
+}
+
+func SetDiscoveryStateLocation(val string) string {
+	prev := GetDiscoveryStateLocation()
+	os.Setenv(discoveryStateLocationEnvVar, val)
+	return prev
+}
+
+func GetAPICallsMetricsStateLocation() string {
+	return os.Getenv(apiCallsMetricsStateLocationEnvVar)
+}
+
+func GetProxyVersion() string {
+	return os.Getenv(proxyVersionEnvVar)
+}
+
+func GetRemedyStateLocation() string {
+	return os.Getenv(remedyStatsStateLocationEnvVar)
+}
+
+func GetManageEndpointsPort() string {
+	return os.Getenv(haproxyManageEndpointsPortEnvVar)
+}
+
+func GetHAProxyHealthcheckPort() string {
+	return os.Getenv(haproxyHealthcheckPortEnvVar)
+}
+
+func GetRedisURL() string {
+	return os.Getenv(redisURLEnvVar)
+}
+
+func SetRedisURL(val string) string {
+	prev := GetRedisURL()
+	os.Setenv(redisURLEnvVar, val)
+	return prev
+}
+
+func GetRedisUseCluster() (bool, error) {
+	raw := os.Getenv(redisUseCluster)
+	if raw == "true" {
+		return true, nil
+	}
+	if raw == "false" {
+		return false, nil
+	}
+	return false, fmt.Errorf(
+		"%s must be either `true` or `false`",
+		redisUseCluster,
+	)
+}
+
+func SetRedisUseCluster(val bool) bool {
+	prev, _ := GetRedisUseCluster()
+	os.Setenv(redisUseCluster, strconv.FormatBool(val))
+	return prev
+}
+
+func GetRedisPrefix() string {
+	return os.Getenv(redisPrefix)
+}
+
+func GetRedisMaxRetryAttempts() (int, error) {
+	return strconv.Atoi(os.Getenv(redisMaxRetryAttempts))
+}
+
+func SetRedisMaxRetryAttempts(val int) int {
+	prev, _ := GetRedisMaxRetryAttempts()
+	os.Setenv(redisMaxRetryAttempts, strconv.Itoa(val))
+	return prev
+}
+
+func GetRedisRetryBackoffTime() (time.Duration, error) {
+	millis, err := strconv.Atoi(os.Getenv(redisRetryBackoffMillis))
+	if err != nil {
+		return 0, err
+	}
+	return time.Millisecond * time.Duration(millis), nil
+}
+
+func SetRedisRetryBackoffTime(val time.Duration) time.Duration {
+	prev, _ := GetRedisRetryBackoffTime()
+	os.Setenv(redisRetryBackoffMillis, strconv.Itoa(int(val.Milliseconds())))
+	return prev
+}
+
+func GetRedisMaxOLRetryAttempts() (int, error) {
+	return strconv.Atoi(os.Getenv(redisMaxOLRetryAttempts))
+}
+
+func SetRedisMaxOLRetryAttempts(val int) int {
+	prev, _ := GetRedisMaxOLRetryAttempts()
+	os.Setenv(redisMaxOLRetryAttempts, strconv.Itoa(val))
+	return prev
+}
+
+func GetRedisUseClientCertificate() bool {
+	return parseBooleanEnvVar("REDIS_USE_CLIENT_CERT")
+}
+
+func GetRedisClientCertificatePath() string {
+	return os.Getenv("REDIS_CLIENT_CERT_PATH")
+}
+
+func GetRedisClientKeyPath() string {
+	return os.Getenv("REDIS_CLIENT_KEY_PATH")
+}
+
+func GetRedisUseCACertificate() bool {
+	return parseBooleanEnvVar("REDIS_USE_CA_CERT")
+}
+
+func GetRedisCACertificatePath() string {
+	return os.Getenv("REDIS_CA_CERT_PATH")
+}
+
+func GetHubURL() string {
+	lunarHubURL := os.Getenv(lunarHubURLEnvVar)
+	if lunarHubURL == "" {
+		log.Warn().
+			Msgf("Could not find Lunar Hub URL from ENV, using default: %s", lunarHubDefaultValue)
+		lunarHubURL = lunarHubDefaultValue
+	}
+	return lunarHubURL
+}
+
+func GetAPIKey() string {
+	return os.Getenv(lunarAPIKeyEnvVar)
+}
+
+func GetHubReportInterval() (int, error) {
+	return strconv.Atoi(os.Getenv(lunarHubReportIntervalEnvVar))
+}
+
+func IsLogLevelDebug() bool {
+	return log.Logger.GetLevel() == zerolog.DebugLevel
+}
+
+func IsStreamsEnabled() bool {
+	return os.Getenv(streamsFeatureFlagEnvVar) == "true"
+}
+
+func GetStreamsFlowsDirectory() string {
+	return os.Getenv(streamsFlowsDirectoryEnvVar)
+}
+
+func SetStreamsFlowsDirectory(dir string) string {
+	prevVal := GetStreamsFlowsDirectory()
+	os.Setenv(streamsFlowsDirectoryEnvVar, dir)
+	return prevVal
+}
+
+func GetUserProcessorsDirectory() string {
+	return os.Getenv(userProcessorsDirectoryEnvVar)
+}
+
+func GetProcessorsDirectory() string {
+	return os.Getenv(processorsDirectoryEnvVar)
+}
+
+func SetQuotasDirectory(dir string) string {
+	prevVal := GetQuotasDirectory()
+	os.Setenv(QuotasDirectoryEnvVar, dir)
+	return prevVal
+}
+
+func GetQuotasDirectory() string {
+	return os.Getenv(QuotasDirectoryEnvVar)
+}
+
+func GetPathParamsDirectory() string {
+	return os.Getenv(PathParamsDirectoryEnvVar)
+}
+
+func SetPathParamsDirectory(dir string) string {
+	prevVal := GetPathParamsDirectory()
+	os.Setenv(PathParamsDirectoryEnvVar, dir)
+	return prevVal
+}
+
+func SetProcessorsDirectory(dir string) string {
+	prevVal := GetProcessorsDirectory()
+	os.Setenv(processorsDirectoryEnvVar, dir)
+	return prevVal
+}
+
+func UnsetProcessorsDirectory() {
+	os.Unsetenv(processorsDirectoryEnvVar)
+}
+
+func GetGatewayConfigPath() string {
+	return os.Getenv(proxyConfigPath)
+}
+
+func GetDiagnosisFailsafeMinTimeBetweenCalls() (time.Duration, error) {
+	raw, err := strconv.Atoi(os.Getenv(diagnosisFailsafeMinSecBetweenCallsEnvVar))
+	if err != nil {
+		return 0, err
+	}
+	return time.Second * time.Duration(raw), nil
+}
+
+func GetDiagnosisFailsafeConsecutiveN() (int, error) {
+	return strconv.Atoi(os.Getenv(diagnosisFailsafeConsecutiveNEnvVar))
+}
+
+func GetDiagnosisFailsafeMinStablePeriod() (time.Duration, error) {
+	raw, err := strconv.Atoi(os.Getenv(diagnosisFailsafeMinStableSecEnvVar))
+	if err != nil {
+		return 0, err
+	}
+	return time.Second * time.Duration(raw), nil
+}
+
+func GetDiagnosisFailsafeCooldownPeriod() (time.Duration, error) {
+	raw, err := strconv.Atoi(os.Getenv(diagnosisFailsafeCooldownSecEnvVar))
+	if err != nil {
+		return 0, err
+	}
+	return time.Second * time.Duration(raw), nil
+}
+
+func GetDiagnosisFailsafeHealthySessionRate() (int, error) {
+	return strconv.Atoi(os.Getenv(diagnosisFailsafeHealthySessionRateEnvVar))
+}
+
+func GetDiagnosisFailsafeHealthyMaxLastSession() (time.Duration, error) {
+	raw, err := strconv.Atoi(os.Getenv(diagnosisFailsafeHealthyMaxLastSessionSecEnvVar))
+	if err != nil {
+		return 0, err
+	}
+	return time.Second * time.Duration(raw), nil
+}
+
+func parseBooleanEnvVar(envVar string) bool {
+	raw := os.Getenv(envVar)
+	if raw == "true" {
+		return true
+	}
+	if raw == "false" {
+		return false
+	}
+	log.Warn().Msgf("%s must be either `true` or `false`", envVar)
+	return false
+}
