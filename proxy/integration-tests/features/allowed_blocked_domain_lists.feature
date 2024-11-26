2fa4c5ca21e6a009e18193f336c9c434c23f5569
tal-lunar <122523439+tal-lunar@users.noreply.github.com>
2024-11-26 10:18:13 +0200

    Add dedicated scenarios to Lunar Studio - Part 1 (#798)

---
+++ proxy/integration-tests/features/allowed_blocked_domain_lists.feature
@@ -0,0 +1,49 @@
+@legacy
+Feature: Domain lists
+    Scenario: When allowed domain specified only it is accessible
+        Given   Lunar Proxy is up
+        And     API Provider is up
+        When    policies.yaml file is updated
+        And     policies.yaml included allowed_domains list with jsonplaceholder.typicode.com
+        And     policies.yaml file is saved
+        And     apply_policies command is run
+        When    Request to https:// jsonplaceholder.typicode.com /posts is made through Lunar Proxy
+        Then    Response has status 200
+        When    Request to https:// example.com / is made through Lunar Proxy
+        Then    Response has status 403
+    
+    Scenario: Allowed domain isn't specified - all domains are accessible
+        Given   Lunar Proxy is up
+        And     API Provider is up
+        When    policies.yaml file is updated
+        And     policies.yaml file is saved
+        And     apply_policies command is run
+        When    Request to https:// jsonplaceholder.typicode.com /posts is made through Lunar Proxy
+        Then    Response has status 200
+        When    Request to https:// example.com / is made through Lunar Proxy
+        Then    Response has status 200
+
+    Scenario: Allowed domain specified as any - all domains are accessible
+        Given   Lunar Proxy is up
+        And     API Provider is up
+        When    policies.yaml file is updated
+        And     policies.yaml included allowed_domains list with .*
+        And     policies.yaml file is saved
+        And     apply_policies command is run
+        When    Request to https:// jsonplaceholder.typicode.com /posts is made through Lunar Proxy
+        Then    Response has status 200
+        When    Request to https:// example.com / is made through Lunar Proxy
+        Then    Response has status 200
+
+    Scenario: When blocked domain specified it is inaccessible, other domains can be accessed
+        Given   Lunar Proxy is up
+        And     API Provider is up
+        When    policies.yaml file is updated
+        And     policies.yaml included blocked_domains list with example.com
+        And     policies.yaml file is saved
+        And     apply_policies command is run
+        When    Request to https:// example.com / is made through Lunar Proxy
+        Then    Response has status 403
+        When    Request to https:// jsonplaceholder.typicode.com /posts is made through Lunar Proxy
+        Then    Response has status 200
+        
---
+++ proxy/src/services/lunar-engine/streams/resources/resource_management_test.go
@@ -0,0 +1,127 @@
+package resources
+
+import (
+	streamconfig "lunar/engine/streams/config"
+	publictypes "lunar/engine/streams/public-types"
+	quotaresource "lunar/engine/streams/resources/quota"
+	resourceutils "lunar/engine/streams/resources/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadQuotaResources(t *testing.T) {
+	quotaData := generateQuotaRepresentation()
+
+	resourceManagement := &ResourceManagement{
+		quotas:   resourceutils.NewResource[quotaresource.QuotaAdmI](),
+		flowData: make(map[publictypes.ComparableFilter]*resourceutils.SystemFlowRepresentation),
+	}
+	quotaLoader, err := quotaresource.NewLoader()
+	require.NoError(t, err)
+	resourceManagement.quotaLoader = quotaLoader
+
+	resourceManagement, err = resourceManagement.WithQuotaData(quotaData)
+	require.NoError(t, err)
+
+	for _, quota := range quotaData {
+		loadedQuota, found := resourceManagement.quotas.Get(quota.Quota.ID)
+		assert.True(t, found)
+		assert.Equal(t, loadedQuota.GetMetaData().Quota.Filter, quota.Quota.Filter)
+		assert.Equal(t, loadedQuota.GetMetaData().Quota.ID, quota.Quota.ID)
+		assert.Equal(t, loadedQuota.GetMetaData().Quota.Strategy, quota.Quota.Strategy)
+	}
+}
+
+func TestSystemFlowAvailability(t *testing.T) {
+	quotaData := generateQuotaRepresentation()
+
+	resourceManagement := &ResourceManagement{
+		quotas:   resourceutils.NewResource[quotaresource.QuotaAdmI](),
+		flowData: make(map[publictypes.ComparableFilter]*resourceutils.SystemFlowRepresentation),
+	}
+	quotaLoader, err := quotaresource.NewLoader()
+	require.NoError(t, err)
+	resourceManagement.quotaLoader = quotaLoader
+	resourceManagement, err = resourceManagement.WithQuotaData(quotaData)
+	require.NoError(t, err)
+
+	for _, quota := range quotaData {
+		generatedSystemFlow, _ := resourceManagement.GetFlowData(quota.Quota.Filter.ToComparable())
+		assert.NotNil(t, generatedSystemFlow)
+	}
+}
+
+func generateQuotaRepresentation() []*quotaresource.QuotaResourceData {
+	return []*quotaresource.QuotaResourceData{
+		{
+			Quota: &quotaresource.QuotaConfig{
+				ID:       "quota1",
+				Filter:   generateFilter(0),
+				Strategy: generateQuotaStrategy(0),
+			},
+		},
+		{
+			Quota: &quotaresource.QuotaConfig{
+				ID:       "quota2",
+				Filter:   generateFilter(1),
+				Strategy: generateQuotaStrategy(1),
+			},
+		},
+		{
+			Quota: &quotaresource.QuotaConfig{
+				ID:       "quota3",
+				Filter:   generateFilter(2),
+				Strategy: generateQuotaStrategy(2),
+			},
+		},
+	}
+}
+
+func generateFilter(useCase int) *streamconfig.Filter {
+	filter := &streamconfig.Filter{
+		URL: "api.example.com",
+	}
+	switch useCase {
+	case 0:
+		filter.Method = []string{"GET"}
+	case 1:
+		filter.Method = []string{"POST"}
+	case 2:
+		filter.Method = []string{"GET", "POST"}
+	}
+	return filter
+}
+
+func generateQuotaStrategy(_ int) *quotaresource.StrategyConfig {
+	useCase := 0 // Remove when we support other strategies
+	switch useCase {
+	case 0:
+		return &quotaresource.StrategyConfig{
+			FixedWindow: &quotaresource.FixedWindowConfig{
+				QuotaLimit: quotaresource.QuotaLimit{
+					Max:          1,
+					Interval:     10,
+					IntervalUnit: "hour",
+				},
+			},
+		}
+	case 1:
+		return &quotaresource.StrategyConfig{
+			Concurrent: &quotaresource.ConcurrentConfig{
+				MaxRequestCount: 1,
+			},
+		}
+	case 2:
+		return &quotaresource.StrategyConfig{
+			HeaderBased: &quotaresource.HeaderBasedConfig{
+				QuotaHeader:      "X-RateLimit-Limit",
+				ResetHeader:      "X-RateLimit-Reset",
+				RetryAfterHeader: "Retry-After",
+			},
+		}
+	default:
+		return nil
+	}
+}
