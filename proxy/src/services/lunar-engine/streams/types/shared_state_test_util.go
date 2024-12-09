b0b3bde8fd2410dc1aa5b3e393c460a5213c64fa
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-12-09 17:30:40 +0000

    Merge pull request #843 from TheLunarCompany/cloud-505-update-pre-made-flow-graphics-on-flow-library-screen
    feat: add premade flow graphics to Flow Library.

---
+++ proxy/src/services/lunar-engine/streams/types/shared_state_test_util.go
@@ -0,0 +1,110 @@
+//go:build pro
+
+package streamtypes
+
+import (
+	publictypes "lunar/engine/streams/public-types"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestContextForPrimitive(
+	t *testing.T,
+	contextA publictypes.SharedStateI[string],
+	contextB publictypes.SharedStateI[string],
+) {
+	err := contextA.Set("key1", "value1")
+	require.NoError(t, err)
+
+	err = contextA.Set("key2", "value2")
+	require.NoError(t, err)
+
+	actualValStr, err := contextB.Get("key1")
+	require.NoError(t, err)
+	require.Equal(t, "value1", actualValStr)
+
+	actualValStr, err = contextB.Get("key2")
+	require.NoError(t, err)
+	require.Equal(t, "value2", actualValStr)
+
+	// test IsExists
+	require.True(t, contextA.Exists("key2"))
+	require.True(t, contextB.Exists("key2"))
+
+	// test update value
+	err = contextA.Set("key2", "new_value")
+	require.NoError(t, err)
+
+	actualValStr, err = contextB.Get("key2")
+	require.NoError(t, err)
+	require.Equal(t, "new_value", actualValStr)
+
+	// test pop
+	actualValStr, err = contextB.Pop("key1")
+	require.NoError(t, err)
+	require.Equal(t, "value1", actualValStr)
+
+	_, err = contextA.Get("key1")
+	require.Error(t, err)
+}
+
+func TestContextForSlice(
+	t *testing.T,
+	contextSliceA publictypes.SharedStateI[[]float64],
+	contextSliceB publictypes.SharedStateI[[]float64],
+) {
+	err := contextSliceA.Set("key4", []float64{1.1, 2.2, 3.3})
+	require.NoError(t, err)
+
+	err = contextSliceA.Set("key5", []float64{4.4, 5.5, 6.6})
+	require.NoError(t, err)
+
+	actualSlice, err := contextSliceB.Get("key4")
+	require.NoError(t, err)
+	require.Equal(t, []float64{1.1, 2.2, 3.3}, actualSlice)
+
+	actualSlice, err = contextSliceB.Get("key5")
+	require.NoError(t, err)
+	require.Equal(t, []float64{4.4, 5.5, 6.6}, actualSlice)
+}
+
+func TestContextWithScore(
+	t *testing.T,
+	contextKey string,
+	contextA publictypes.SharedStateI[string],
+	contextB publictypes.SharedStateI[string],
+) {
+	err := contextA.SetWithScore(contextKey, 2.0, "value1")
+	require.NoError(t, err)
+
+	err = contextA.SetWithScore(contextKey, 1.0, "value2")
+	require.NoError(t, err)
+
+	err = contextA.SetWithScore(contextKey, 3.0, "value3")
+	require.NoError(t, err)
+
+	actualVal, err := contextB.Get(contextKey)
+	require.NoError(t, err)
+	require.Equal(t, "value2", actualVal)
+
+	expectedSlice, err := contextB.GetMany(contextKey, 3)
+	require.NoError(t, err)
+	require.Equal(t, []string{"value2", "value1", "value3"}, expectedSlice)
+
+	// testing pop by score
+	actualVal, err = contextA.Pop(contextKey)
+	require.NoError(t, err)
+	require.Equal(t, "value2", actualVal)
+
+	actualVal, err = contextA.Pop(contextKey)
+	require.NoError(t, err)
+	require.Equal(t, "value1", actualVal)
+
+	actualVal, err = contextA.Pop(contextKey)
+	require.NoError(t, err)
+	require.Equal(t, "value3", actualVal)
+
+	// testing isExists
+	require.False(t, contextB.Exists(contextKey))
+}
---
+++ proxy/src/services/lunar-engine/streams/validation/validator.go
@@ -0,0 +1,60 @@
+package validation
+
+import (
+	"fmt"
+	"lunar/engine/streams"
+	"lunar/engine/utils/environment"
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v2"
+)
+
+type Validator struct {
+	dryRunStream *streams.Stream
+}
+
+func NewValidator() *Validator {
+	return &Validator{}
+}
+
+func (v *Validator) Validate() error {
+	stream, err := streams.NewStream()
+	if err != nil {
+		return err
+	}
+	v.dryRunStream = stream.WithStrictMode()
+
+	log.Trace().Msg("Starting flows validation")
+	if err := v.dryRunStream.Initialize(); err != nil {
+		return err
+	}
+
+	if err := v.ValidateGatewayConfig(); err != nil {
+		return err
+	}
+
+	log.Trace().Msg("Flows validation completed")
+	return nil
+}
+
+func (v *Validator) ValidateGatewayConfig() error {
+	log.Trace().Msg("Starting gateway config validation")
+	configPath := environment.GetGatewayConfigPath()
+
+	log.Trace().Msgf("Gateway config file path: %s", configPath)
+	file, _ := os.Open(configPath)
+	if file == nil {
+		log.Trace().Msg("Gateway config file not found")
+		return nil
+	}
+	defer file.Close()
+
+	var configData map[string]interface{}
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&configData); err != nil {
+		return fmt.Errorf("failed to validate gateway config: %w", err)
+	}
+
+	return nil
+}
