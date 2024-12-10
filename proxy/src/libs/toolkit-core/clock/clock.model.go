de23aa149f5f8ceb89b9c5895431af270920e974
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-12-10 09:00:47 +0000

    Merge pull request #844 from TheLunarCompany/cloud-501-update-flow-diagrams-in-flow-builder
    feat: update all premade flow diagrams and interactivity.

---
+++ proxy/src/libs/toolkit-core/clock/clock.model.go
@@ -0,0 +1,12 @@
+package clock
+
+import "time"
+
+type Clock interface {
+	Now() time.Time
+	Sleep(d time.Duration)
+	After(d time.Duration) <-chan time.Time
+	Since(sinceTime time.Time) time.Duration
+}
+
+type RealClock struct{}
