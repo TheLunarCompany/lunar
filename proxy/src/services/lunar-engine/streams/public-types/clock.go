403c6976e6076fb085b911a876c9475de04b78db
Ryan Taylor <ryanhavoctaylor@gmail.com>
2024-12-12 21:17:11 +0000

    Merge pull request #861 from TheLunarCompany/cloud-516-label-alignment-on-flow-diagrams-misaligned-on-resize
    Cloud 516 label alignment on flow diagrams misaligned on resize

---
+++ proxy/src/services/lunar-engine/streams/public-types/clock.go
@@ -0,0 +1,10 @@
+package publictypes
+
+import "time"
+
+type ClockI interface {
+	Now() time.Time
+	Sleep(d time.Duration)
+	After(d time.Duration) <-chan time.Time
+	Since(t time.Time) time.Duration
+}
