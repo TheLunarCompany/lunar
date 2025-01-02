9f7867efa12f3a824710cda79f952f6afc683eb6
eitangol <108608289+eitangol@users.noreply.github.com>
2025-01-02 13:03:39 +0200

    This PR fix a bug in the quota counters logic (#916)
    * This PR fix a bug in the quota counters logic
    
    * Added A new parameter to limit the shared queue size
    
    * Update flows-components.md
    
    * Update queue_processor.go
    
    * Update queue_processor.go
    
    * Change the param value to boolean
    
    * Change the param name of the shared queue
    
    * Update flows-components.md
    
    * Update queue_processor.go

---
+++ proxy/src/services/lunar-engine/streams/public-types/transaction.go
@@ -0,0 +1,47 @@
+package publictypes
+
+import "time"
+
+type TransactionI interface {
+	IsNewSequence() bool
+	DoesHeaderExist(string) bool
+	DoesHeaderValueMatch(string, string) bool
+	DoesQueryParamExist(string) bool
+	DoesQueryParamValueMatch(string, string) bool
+	Size() int
+	GetID() string
+	GetSequenceID() string
+	GetMethod() string
+	GetURL() string
+	GetHost() string
+	GetStatus() int
+	GetHeader(key string) (string, bool)
+	GetHeaders() map[string]string
+	GetBody() string
+	GetTime() time.Time
+}
+
+type APIStreamI interface {
+	WithLunarContext(context LunarContextI) APIStreamI
+	GetID() string
+	GetType() StreamType
+	GetActionsType() StreamType
+	GetName() string
+	GetURL() string
+	GetHost() string
+	GetBody() string
+	GetStrStatus() string
+	GetMethod() string
+	GetSize() int
+	GetHeader(key string) (string, bool)
+	GetHeaders() map[string]string
+	DoesHeaderValueMatch(headerName, headerValue string) bool
+	GetRequest() TransactionI
+	GetResponse() TransactionI
+	GetContext() LunarContextI
+	SetRequest(request TransactionI)
+	SetResponse(response TransactionI)
+	SetContext(context LunarContextI)
+	SetType(streamType StreamType)
+	SetActionsType(streamType StreamType)
+}
