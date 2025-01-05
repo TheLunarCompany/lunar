c953e97b02ca9024ed4df0c2799b587519e92da9
RotemY-Lunar <rotemy@lunar.dev>
2025-01-05 13:34:54 +0200

    Testing scenarios for version v0.10.19-alpha-3a49a33 (#919)
    * testing limiter scenario 01
    
    * rename scenario #2 and fix it's parameters
    
    * limiter scenario-06 - update

---
+++ proxy/src/services/lunar-engine/streams/public-types/context.go
@@ -0,0 +1,60 @@
+package publictypes
+
+import (
+	"lunar/toolkit-core/clock"
+	"time"
+
+	"golang.org/x/exp/constraints"
+)
+
+type ContextI interface {
+	Set(string, interface{}) error
+	Get(string) (interface{}, error)
+	Pop(string) (interface{}, error)
+
+	Exists(string) bool
+}
+
+type LunarContextI interface {
+	GetGlobalContext() ContextI
+	GetFlowContext() ContextI
+	GetTransactionalContext() ContextI
+}
+
+type (
+	SharedIsReqIDRelevant func(string) bool
+	SharedQueueI          interface {
+		Enqueue(string, float64) error
+		DequeueIfValueMatch(SharedIsReqIDRelevant) string
+		Remove(string)
+		Size() int64
+	}
+)
+
+type SharedStateI[T PersistentType] interface {
+	WithClock(clock.Clock) SharedStateI[T]
+	Set(string, T) error
+	SetWithScore(string, float64, T) error
+	NewQueue(string, time.Duration) SharedQueueI
+	Get(string) (T, error)
+	GetMany(string, int64) ([]T, error)
+
+	Pop(string) (T, error)
+	AtomicWindowReset(string, time.Duration) error
+	AtomicIncWindow(string, time.Duration, int64) (int64, error)
+	AtomicWindowResetIn(string, time.Duration) (time.Duration, error)
+	Exists(string) bool
+}
+
+// Constraint for types acceptable for persistent storage (strings, numbers, slices of these types)
+type PersistentType interface {
+	constraints.Integer |
+		constraints.Float |
+		~string |
+		bool |
+		[]string |
+		[]bool |
+		[]int64 |
+		[]int |
+		[]float64
+}
