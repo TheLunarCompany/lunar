966859f76578b1c438d98ff122dd24d343ba33c0
roygab <108218987+roygab@users.noreply.github.com>
2025-01-19 18:13:14 +0200

    Add security and user settings to Grafana configuration (#969)

---
+++ proxy/src/services/flows-validator/validator_test.go
@@ -0,0 +1,290 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"lunar/engine/utils/environment"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	procPath := path.Join(path.Dir(wd), "lunar-engine/streams/processors")
+	os.Setenv("LUNAR_PROXY_PROCESSORS_DIRECTORY", procPath)
+	os.Setenv("LUNAR_SPOE_PROCESSING_TIMEOUT_SEC", "61")
+
+	code := m.Run()
+	os.RemoveAll(path.Join(".", DefaultRoot))
+	os.Exit(code)
+}
+
+func TestGetValidatorPort(t *testing.T) {
+	t.Run("Default Port", func(t *testing.T) {
+		os.Unsetenv(ValidatorPortEnvVar)
+		port := GetValidatorPort()
+		require.Equal(t, DefaultPort, port)
+	})
+
+	t.Run("Custom Port", func(t *testing.T) {
+		os.Setenv(ValidatorPortEnvVar, "9090")
+		defer os.Unsetenv(ValidatorPortEnvVar)
+
+		port := GetValidatorPort()
+		require.Equal(t, "9090", port)
+	})
+}
+
+func TestWriteBase64File(t *testing.T) {
+	content := "test content"
+	encoded := base64.StdEncoding.EncodeToString([]byte(content))
+	tempFile := filepath.Join(os.TempDir(), "testfile.yaml")
+	defer os.Remove(tempFile)
+
+	err := writeBase64File(encoded, tempFile)
+	require.NoError(t, err)
+
+	data, err := os.ReadFile(tempFile)
+	require.NoError(t, err)
+	require.Equal(t, content, string(data))
+}
+
+func TestValidation(t *testing.T) {
+	testCases := []struct {
+		name          string
+		testFolder    string
+		useTestFolder bool // if true, the test folder will be used as the root folder
+		validationFn  func(t *testing.T, input *ValidationInput, result *ValidationResult)
+	}{
+		{
+			name:       "Valid",
+			testFolder: "valid",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 2)
+				require.Len(t, input.PathParams, 1)
+				require.Len(t, input.Quotas, 2)
+				require.NotEmpty(t, input.GatewayConfig)
+
+				require.True(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:          "Valid with ready folder",
+			testFolder:    "valid",
+			useTestFolder: true,
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 0)
+				require.Len(t, input.PathParams, 0)
+				require.Len(t, input.Quotas, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.NotEmpty(t, input.FolderPath)
+
+				require.True(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Invalid queue processor configuration - queue ttl more than LUNAR_SPOE_PROCESSING_TIMEOUT_SEC",
+			testFolder: "invalid-queue-processor-config",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 2)
+				require.Len(t, input.Quotas, 2)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+				require.Contains(t, result.Message, "processing timeout (1m1s) is less than queue TTL (2m0s)")
+			},
+		},
+		{
+			name:       "Invalid gateway configuration",
+			testFolder: "invalid-gateway-config",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 0)
+				require.Len(t, input.Quotas, 0)
+				require.Len(t, input.PathParams, 0)
+				require.NotEmpty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+				require.Contains(t, result.Message, "failed to validate gateway config")
+			},
+		},
+		{
+			name:       "Invalid Quota",
+			testFolder: "invalid-quota",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 1)
+				require.Len(t, input.Quotas, 1)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Quota With Same Provider Across Multiple Files",
+			testFolder: "quota-with-same-provider-across-multiple-files",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 0)
+				require.Len(t, input.Quotas, 2)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Quota With Same Provider Across Multiple Files - Internal Filter",
+			testFolder: "quota-with-same-provider-across-multiple-files-internal-filter",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 0)
+				require.Len(t, input.Quotas, 2)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Invalid Flow - no request",
+			testFolder: "no-request-section",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 1)
+				require.Len(t, input.Quotas, 0)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Invalid Flow - no response",
+			testFolder: "no-response-section",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 1)
+				require.Len(t, input.Quotas, 0)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Invalid flow yaml",
+			testFolder: "invalid-yaml-flow",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 1)
+				require.Len(t, input.Quotas, 0)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Invalid quota with no filter",
+			testFolder: "quota-with-no-filter",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 0)
+				require.Len(t, input.Quotas, 1)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+			},
+		},
+		{
+			name:       "Invalid flow - invalid processor",
+			testFolder: "flow-with-invalid-processor",
+			validationFn: func(t *testing.T, input *ValidationInput, result *ValidationResult) {
+				require.Len(t, input.Flows, 2)
+				require.Len(t, input.Quotas, 2)
+				require.Len(t, input.PathParams, 0)
+				require.Empty(t, input.GatewayConfig)
+
+				require.False(t, result.Success, result.Message)
+				require.Contains(t, result.Message, "failed to create processor")
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testFolder := filepath.Join("test-cases", tc.testFolder)
+
+			var input *ValidationInput
+			if tc.useTestFolder {
+				input = &ValidationInput{
+					FolderPath: testFolder,
+				}
+			} else {
+				input = loadTestCase(t, testFolder)
+			}
+
+			body, err := json.Marshal(input)
+			require.NoError(t, err)
+
+			req := httptest.NewRequest(http.MethodPost, "/validate-flows", bytes.NewReader(body))
+			req.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+
+			validateFlowsHandler(w, req)
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			require.Equal(t, http.StatusOK, resp.StatusCode)
+
+			var result ValidationResult
+			err = json.NewDecoder(resp.Body).Decode(&result)
+			require.NoError(t, err)
+
+			tc.validationFn(t, input, &result)
+		})
+	}
+}
+
+func loadTestCase(t *testing.T, testFolder string) *ValidationInput {
+	valInput := &ValidationInput{}
+	valInput.Flows = loadFiles(t, testFolder, environment.FlowsFolder)
+	valInput.PathParams = loadFiles(t, testFolder, environment.PathParamsFolder)
+	valInput.Quotas = loadFiles(t, testFolder, environment.QuotasFolder)
+
+	gatewayConfig, _ := os.ReadFile(environment.GetCustomGatewayConfigPath(testFolder))
+	if len(gatewayConfig) != 0 {
+		valInput.GatewayConfig = base64.StdEncoding.EncodeToString(gatewayConfig)
+	}
+
+	return valInput
+}
+
+func loadFiles(t *testing.T, root, subFolder string) []string {
+	dirEntries, err := os.ReadDir(filepath.Join(root, subFolder))
+	if err != nil {
+		return nil
+	}
+	var files []string
+	for _, entry := range dirEntries {
+		if entry.IsDir() || !strings.HasSuffix(entry.Name(), ".yaml") {
+			continue
+		}
+
+		content, err := os.ReadFile(filepath.Join(root, subFolder, entry.Name()))
+		require.NoError(t, err)
+
+		// Convert the content to Base64
+		encoded := base64.StdEncoding.EncodeToString(content)
+		files = append(files, encoded)
+	}
+	return files
+}
---
+++ proxy/src/services/lunar-engine/streams/processors/retry/retry_processor.go
@@ -0,0 +1,250 @@
+package processorretry
+
+import (
+	"context"
+	"fmt"
+	"lunar/engine/actions"
+	lunar_metrics "lunar/engine/metrics"
+	"lunar/engine/streams/processors/utils"
+	public_types "lunar/engine/streams/public-types"
+	stream_types "lunar/engine/streams/types"
+	"lunar/engine/utils/environment"
+	"lunar/toolkit-core/otel"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"go.opentelemetry.io/otel/metric"
+)
+
+const (
+	attemptsKey           = "attempts"
+	cooldownKey           = "cooldown_between_attempts_seconds"
+	cooldownMultiplierKey = "cooldown_multiplier"
+
+	retryCountMetric       = "lunar_retry_processor_retry_count"
+	failedRetryCountMetric = "lunar_retry_processor_failed_retry_count"
+)
+
+type retryProcessor struct {
+	name               string
+	attempts           int
+	cooldown           time.Duration
+	cooldownMultiplier float64
+	metaData           *stream_types.ProcessorMetaData
+	logger             zerolog.Logger
+	labelManager       *lunar_metrics.LabelManager
+	metricObjects      map[string]metric.Float64Counter
+}
+
+func NewProcessor(metaData *stream_types.ProcessorMetaData) (stream_types.Processor, error) {
+	retryProc := &retryProcessor{
+		name:          metaData.Name,
+		metaData:      metaData,
+		metricObjects: make(map[string]metric.Float64Counter),
+		labelManager:  lunar_metrics.NewLabelManager(metaData.GetMetricLabels()),
+	}
+
+	if err := retryProc.init(); err != nil {
+		return nil, err
+	}
+
+	if err := retryProc.initializeMetrics(); err != nil {
+		log.Error().Err(err).Msgf("failed to initialize metrics for %s", metaData.Name)
+		retryProc.metaData.Metrics.Enabled = false
+	}
+
+	return retryProc, nil
+}
+
+func (p *retryProcessor) Execute(
+	flowName string,
+	APIStream public_types.APIStreamI,
+) (stream_types.ProcessorIO, error) {
+	retryCounterKey := p.getCounterKey(APIStream.GetSequenceID())
+	currentRetryCount := p.incrementRetryCount(retryCounterKey, APIStream)
+
+	if currentRetryCount > p.attempts {
+		p.removeCount(retryCounterKey, APIStream)
+		p.logger.Trace().Msg("Max retry attempts reached, will not retry")
+		p.updateMetrics(failedRetryCountMetric, flowName, APIStream)
+		return stream_types.ProcessorIO{
+			Type: public_types.StreamTypeRequest,
+			Name: "failed",
+		}, nil
+	}
+
+	cooldownDuration := p.getCooldownDuration(currentRetryCount)
+
+	p.logger.Trace().Int("currentRetryCount", currentRetryCount).
+		Dur("cooldown", cooldownDuration).Msg("waiting before retry")
+
+	<-p.metaData.Clock.After(cooldownDuration)
+
+	p.updateMetrics(retryCountMetric, flowName, APIStream)
+	return stream_types.ProcessorIO{
+		RespAction: &actions.RetryRequestAction{},
+		Type:       public_types.StreamTypeRequest,
+		Name:       "retry",
+	}, nil
+}
+
+func (p *retryProcessor) GetName() string {
+	return p.name
+}
+
+func (p *retryProcessor) GetRequirement() *stream_types.ProcessorRequirement {
+	return &stream_types.ProcessorRequirement{
+		IsReqCaptureRequired: true,
+	}
+}
+
+func (p *retryProcessor) incrementRetryCount(
+	counterKey string,
+	APIStream public_types.APIStreamI,
+) int {
+	var currentRetryCount int
+	flowContext := APIStream.GetContext().GetFlowContext()
+	currentRetryCountRaw, err := flowContext.Get(counterKey)
+
+	if err != nil {
+		log.Trace().Err(err).Msg("Failed to get retry counter")
+		currentRetryCount = 0
+	} else {
+		currentRetryCount = currentRetryCountRaw.(int)
+	}
+
+	updatedRetryCount := currentRetryCount + 1
+
+	err = flowContext.Set(counterKey, updatedRetryCount)
+	if err != nil {
+		p.logger.Debug().Err(err).Msg("Failed to set retry counter")
+	}
+
+	return updatedRetryCount
+}
+
+func (p *retryProcessor) removeCount(
+	counterKey string,
+	APIStream public_types.APIStreamI,
+) {
+	flowContext := APIStream.GetContext().GetFlowContext()
+	_, err := flowContext.Pop(counterKey)
+	if err != nil {
+		p.logger.Debug().Err(err).Msg("Failed to remove retry counter")
+	}
+}
+
+func (p *retryProcessor) getCounterKey(reqID string) string {
+	return fmt.Sprintf("%s::%s::%s", p.name, "retry_counter", reqID)
+}
+
+func (p *retryProcessor) getCooldownDuration(currentRetryCount int) time.Duration {
+	cooldown := p.cooldown.Seconds() + (float64(currentRetryCount) * p.cooldownMultiplier)
+	return time.Duration(cooldown) * time.Second
+}
+
+func (p *retryProcessor) init() error {
+	p.logger = log.Logger.With().
+		Str("processor", "retryProcessor").
+		Str("processorKey", p.name).Logger()
+
+	if err := utils.ExtractIntParam(p.metaData.Parameters,
+		attemptsKey, &p.attempts); err != nil {
+		return err
+	}
+
+	if p.attempts < 1 {
+		return fmt.Errorf("attempts should be greater than 0")
+	}
+
+	if err := utils.ExtractDurationInSecParam(p.metaData.Parameters,
+		cooldownKey, &p.cooldown); err != nil {
+		return err
+	}
+
+	if p.cooldown < 0 {
+		return fmt.Errorf("cooldown should be greater than or equal to 0")
+	}
+
+	if err := utils.ExtractFloat64Param(p.metaData.Parameters,
+		cooldownMultiplierKey, &p.cooldownMultiplier); err != nil {
+		return err
+	}
+
+	if p.cooldownMultiplier < 0 {
+		return fmt.Errorf("cooldownMultiplier should be greater than or equal to 0")
+	}
+
+	configuredTimeout, err := environment.GetLuaRetryRequestTimeout()
+	if err != nil {
+		return err
+	}
+
+	// Verify that the cooldown duration for each attempt is less than the configured timeout
+	// Take in consideration that each retry can wait for the duration of the following attempts
+	var cooldown time.Duration
+	for currentAttempt := 1; currentAttempt <= p.attempts; currentAttempt++ {
+		cooldown = cooldown + p.getCooldownDuration(currentAttempt)
+		log.Trace().
+			Dur("cooldown", cooldown).
+			Dur("configuredTimeout", configuredTimeout).
+			Msgf("Cooldown duration for attempt %d", currentAttempt)
+		if cooldown > configuredTimeout {
+			return fmt.Errorf(
+				"cooldown duration for attempt %d is greater than configured timeout, modify the value of %s",
+				currentAttempt, environment.LuaRetryRequestTimeoutSecEnvVar,
+			)
+		}
+	}
+
+	return nil
+}
+
+func (p *retryProcessor) initializeMetrics() error {
+	log.Info().Msgf("Initializing metrics for %s", p.name)
+	if !p.metaData.IsMetricsEnabled() {
+		log.Info().Msgf("Metrics are disabled for %s", p.name)
+		return nil
+	}
+
+	meter := otel.GetMeter()
+	meterObj, err := meter.Float64Counter(retryCountMetric,
+		metric.WithDescription(fmt.Sprintf("Retry attempts count for %s", p.name)))
+	if err != nil {
+		return fmt.Errorf("failed to initialize attempts count metric: %w", err)
+	}
+	p.metricObjects[retryCountMetric] = meterObj
+
+	meterObj, err = meter.Float64Counter(failedRetryCountMetric,
+		metric.WithDescription(fmt.Sprintf("Retry failed retry count for %s", p.name)))
+	if err != nil {
+		return fmt.Errorf("failed to initialize failed retry count metric: %w", err)
+	}
+	p.metricObjects[failedRetryCountMetric] = meterObj
+
+	log.Info().Msgf("Metrics initialized for %s", p.name)
+	return nil
+}
+
+func (p *retryProcessor) updateMetrics(
+	condition, flowName string,
+	provider lunar_metrics.APICallMetricsProviderI,
+) {
+	if !p.metaData.IsMetricsEnabled() {
+		return
+	}
+
+	attributes := p.labelManager.GetProcessorMetricsAttributes(provider, flowName, p.name)
+
+	updateMetricFunc := func(metricName string) {
+		if metricObj, ok := p.metricObjects[metricName]; ok {
+			metricObj.Add(context.Background(), 1, metric.WithAttributes(attributes...))
+		}
+	}
+
+	updateMetricFunc(condition)
+
+	log.Trace().Msgf("Metrics updated for %s", p.name)
+}
