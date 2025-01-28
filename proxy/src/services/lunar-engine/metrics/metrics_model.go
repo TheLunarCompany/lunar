4590473a72bbff6510fae2d36bd5e68a62517654
shania92 <167332449+shania92@users.noreply.github.com>
2025-01-28 18:05:08 +0200

    update bilt scenario (#1006)
    * update bilt scenario
    
    * revert overwriting of bilt-endpoints and creates new bilt scenario

---
+++ proxy/src/services/lunar-engine/metrics/metrics_model.go
@@ -0,0 +1,17 @@
+package metrics
+
+type MetricValue struct {
+	Name        Metric `yaml:"name"`
+	Description string `yaml:"description"`
+	JSONPath    string `yaml:"json_path,omitempty"`
+}
+
+type GeneralMetrics struct {
+	LabelValue  []string      `yaml:"label_value"`
+	MetricValue []MetricValue `yaml:"metric_value"`
+}
+
+type Config struct {
+	GeneralMetrics GeneralMetrics `yaml:"general_metrics"`
+	SystemMetrics  []MetricValue  `yaml:"system_metrics"`
+}
