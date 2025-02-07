30287e8e8dd43dac6d657a5c1fd611b20cb8d14f
Nick Zolotarov <10888808+nzolot@users.noreply.github.com>
2025-02-07 09:31:59 -0500

    Merge pull request #1021 from TheLunarCompany/DEVOPS-34
    DO NOT MERGE: Allocator implementation

---
+++ interceptors/lunar-ts-interceptor/jest.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
