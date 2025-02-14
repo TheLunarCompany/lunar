90ab0f428606161c7c5487449b49297692dcab44
levhakobyan <44626278+levhakobyan@users.noreply.github.com>
2025-02-14 11:36:40 +0400

    Levon.cloud 629/feature start quota design (#1105)
    * Fixed linting-issues
    
    * Removed unused imports
    
    * Added apexcharts, QuotaChartCard for test, FeatureFlag QuotaEstimationCharts

---
+++ proxy/src/libs/toolkit-core/context-manager/context_manager.go
@@ -0,0 +1,96 @@
+package contextmanager
+
+import (
+	"context"
+	"lunar/toolkit-core/clock"
+	interfaces "lunar/toolkit-core/interfaces"
+	statusMessage "lunar/toolkit-core/status-message"
+	"sync"
+
+	"github.com/rs/zerolog/log"
+)
+
+// Manager is the singleton that holds the context and clock
+type ContextManager struct {
+	ctx             context.Context
+	clock           clock.Clock
+	statusMessage   *statusMessage.StatusMessage
+	clusterLiveness interfaces.ClusterLivenessI
+}
+
+var (
+	instance *ContextManager
+	once     sync.Once
+)
+
+// Get returns the singleton instance of ContextManager
+func Get() *ContextManager {
+	if instance == nil {
+		initContextManager(context.Background(), clock.NewRealClock())
+	}
+	return instance
+}
+
+// WithContext returns a new instance of ContextManager with the provided context
+func (m *ContextManager) WithContext(ctx context.Context) *ContextManager {
+	m.ctx = ctx
+	return instance
+}
+
+func (m *ContextManager) WithClusterLiveness(
+	clusterLiveness interfaces.ClusterLivenessI,
+) *ContextManager {
+	m.clusterLiveness = clusterLiveness
+	return instance
+}
+
+// WithClock returns a new instance of ContextManager with the provided clock
+func (m *ContextManager) SetRealClock() *ContextManager {
+	m.clock = clock.NewRealClock()
+	return instance
+}
+
+// SetMockClock returns a new instance of ContextManager with a mock clock
+func (m *ContextManager) SetMockClock() *ContextManager {
+	m.clock = clock.NewMockClock()
+	return instance
+}
+
+// GetContext returns the context held by the Manager
+func (m *ContextManager) GetContext() context.Context {
+	return m.ctx
+}
+
+// GetClock returns the current time held by the Manager
+func (m *ContextManager) GetClock() clock.Clock {
+	return m.clock
+}
+
+func (m *ContextManager) GetStatusMessage() *statusMessage.StatusMessage {
+	return m.statusMessage
+}
+
+// GetMockClock returns the mock time held by the Manager
+func (m *ContextManager) GetMockClock() *clock.MockClock {
+	mock, ok := m.clock.(*clock.MockClock)
+	if !ok {
+		log.Error().Msg("Clock is not a mock clock")
+		return nil
+	}
+	return mock
+}
+
+func (m *ContextManager) GetClusterLiveness() (interfaces.ClusterLivenessI, bool) {
+	return m.clusterLiveness, m.clusterLiveness != nil
+}
+
+// initContextManager initializes the singleton instance of ContextManager
+func initContextManager(ctx context.Context, clk clock.Clock) {
+	once.Do(func() {
+		instance = &ContextManager{
+			ctx:           ctx,
+			clock:         clk,
+			statusMessage: statusMessage.NewStatusMessage(),
+		}
+	})
+}
