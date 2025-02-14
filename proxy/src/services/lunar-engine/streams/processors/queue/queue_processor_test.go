5955f6916e53415162e248f652a7d3e1def23c13
MaksymLunar <m.ovsiannikov.opportunities@gmail.com>
2025-02-14 14:15:16 +0100

    [CLOUD-645] making headers section in the Queue subform fully optional and free text (#1110)
    * making headers section in the Queue subform fully optional and free text
    
    * removing validation on the queue -> headers section

---
+++ proxy/src/services/lunar-engine/streams/processors/queue/queue_processor_test.go
@@ -0,0 +1,243 @@
+package processorqueue_test
+
+import (
+	lunar_messages "lunar/engine/messages"
+	stream_config "lunar/engine/streams/config"
+	lunar_context "lunar/engine/streams/lunar-context"
+	queue_processor "lunar/engine/streams/processors/queue"
+	public_types "lunar/engine/streams/public-types"
+	"lunar/engine/streams/resources"
+	quota_resource "lunar/engine/streams/resources/quota"
+	stream_types "lunar/engine/streams/types"
+	context_manager "lunar/toolkit-core/context-manager"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+var sharedState = lunar_context.NewMemoryState[[]byte]()
+
+const (
+	allowedKey = "allowed"
+	blockedKey = "blocked"
+)
+
+func getProcIO(name string) stream_types.ProcessorIO {
+	return stream_types.ProcessorIO{
+		Type: public_types.StreamTypeAny,
+		Name: name,
+	}
+}
+
+func getParamValue(key string, value interface{}) *public_types.ParamValue {
+	keyValue := &public_types.KeyValue{
+		Key:   key,
+		Value: value,
+	}
+	return keyValue.GetParamValue()
+}
+
+func getQuotaData(strategy *quota_resource.StrategyConfig, quotaID string) []*quota_resource.QuotaResourceData {
+	return []*quota_resource.QuotaResourceData{
+		{
+			Quotas: []*quota_resource.QuotaConfig{
+				{
+					ID: quotaID,
+					Filter: &stream_config.Filter{
+						Name: quotaID,
+						URL:  "api.example.com/*",
+					},
+					Strategy: strategy,
+				},
+			},
+		},
+	}
+}
+
+func getRandomString(length int) string {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = charset[random.Intn(len(charset))]
+	}
+	return string(result)
+}
+
+func getAPIStream() public_types.APIStreamI {
+	return stream_types.NewRequestAPIStream(
+		lunar_messages.OnRequest{
+			ID:         getRandomString(10),
+			SequenceID: getRandomString(10),
+			URL:        "api.example.com/" + getRandomString(5),
+		},
+		sharedState,
+	)
+}
+
+func TestQueueProcessor_EnqueueIfSlotAvailable(t *testing.T) {
+	memoryState := lunar_context.NewMemoryState[string]()
+
+	strategy := &quota_resource.StrategyConfig{
+		FixedWindow: &quota_resource.FixedWindowConfig{
+			QuotaLimit: quota_resource.QuotaLimit{
+				Max:          1,
+				Interval:     10,
+				IntervalUnit: "second",
+			},
+		},
+	}
+	quotaID := "test"
+	resources, _ := resources.NewResourceManagement()
+	resources, _ = resources.WithQuotaData(getQuotaData(strategy, quotaID))
+	clk := context_manager.Get().SetRealClock().GetClock()
+	metaData := &stream_types.ProcessorMetaData{
+		Name:                "test",
+		SharedMemory:        memoryState.WithClock(clk),
+		Clock:               clk,
+		ProcessorDefinition: stream_types.ProcessorDefinition{},
+		Parameters: map[string]stream_types.ProcessorParam{
+			"quota_id": {
+				Name:  quotaID,
+				Value: getParamValue("quota_id", quotaID),
+			},
+			"queue_size": {
+				Name:  "queue_size",
+				Value: getParamValue("queue_size", 10),
+			},
+			"redis_queue_size": {
+				Name:  "redis_queue_size",
+				Value: getParamValue("redis_queue_size", -1),
+			},
+			"ttl_seconds": {
+				Name:  "ttl_seconds",
+				Value: getParamValue("ttl_seconds", 10),
+			},
+			"priority_group_by_header": {
+				Name:  "priority_group_by_header",
+				Value: getParamValue("priority_group_by_header", nil),
+			},
+			"priority_groups": {
+				Name:  "priority_groups",
+				Value: getParamValue("priority_groups", nil),
+			},
+		},
+		Resources: resources,
+	}
+	queueProcessor, err := queue_processor.NewProcessor(metaData)
+	require.NoError(t, err)
+	procIO, err := queueProcessor.Execute("", getAPIStream())
+	require.NoError(t, err)
+	require.NotNil(t, procIO)
+	require.Equal(t, getProcIO(allowedKey), procIO)
+}
+
+func TestQueueProcessor_SkipEnqueueIfSlotNotAvailable(t *testing.T) {
+	memoryState := lunar_context.NewMemoryState[string]()
+
+	clk := context_manager.Get().SetRealClock().GetClock()
+	numberOfTestRequests := 3
+	strategy := &quota_resource.StrategyConfig{
+		FixedWindow: &quota_resource.FixedWindowConfig{
+			QuotaLimit: quota_resource.QuotaLimit{
+				Max:          1,
+				Interval:     10,
+				IntervalUnit: "second",
+			},
+		},
+	}
+	quotaID := "test2"
+	resources, _ := resources.NewResourceManagement()
+	resources, _ = resources.WithQuotaData(getQuotaData(strategy, quotaID))
+	metaData := &stream_types.ProcessorMetaData{
+		Name:                quotaID,
+		Clock:               clk,
+		SharedMemory:        memoryState.WithClock(clk),
+		ProcessorDefinition: stream_types.ProcessorDefinition{},
+		Parameters: map[string]stream_types.ProcessorParam{
+			"quota_id": {
+				Name:  "quota_id",
+				Value: getParamValue("quota_id", quotaID),
+			},
+			"queue_size": {
+				Name:  "queue_size",
+				Value: getParamValue("queue_size", 1),
+			},
+			"redis_queue_size": {
+				Name:  "redis_queue_size",
+				Value: getParamValue("redis_queue_size", -1),
+			},
+			"ttl_seconds": {
+				Name:  "ttl_seconds",
+				Value: getParamValue("ttl_seconds", 10),
+			},
+			"priority_group_by_header": {
+				Name:  "priority_group_by_header",
+				Value: getParamValue("priority_group_by_header", nil),
+			},
+			"priority_groups": {
+				Name:  "priority_groups",
+				Value: getParamValue("priority_groups", nil),
+			},
+		},
+		Resources: resources,
+	}
+	queueProcessor, err := queue_processor.NewProcessor(metaData)
+	require.NoError(t, err)
+
+	resultChan := make(chan result, numberOfTestRequests)
+	defer close(resultChan)
+	APIStreams := make([]public_types.APIStreamI, numberOfTestRequests)
+
+	// generate APIStreams for testing
+	for i := 0; i < numberOfTestRequests; i++ {
+		APIStreams[i] = getAPIStream()
+	}
+
+	// Execute the APIStreams
+	for i := 0; i < numberOfTestRequests; i++ {
+		go execute(APIStreams[i], queueProcessor, resultChan)
+	}
+
+	// Wait for all results to be processed
+	results := make([]result, numberOfTestRequests)
+	for i := 0; i < 3; i++ {
+		if i == 2 {
+			// Advance time to allow the second request to be dequeued
+			clk.Sleep(11 * time.Second)
+		}
+		results[i] = <-resultChan
+	}
+
+	resultPrediction := map[string]int{
+		allowedKey: 1, // Expected to be allowed
+		blockedKey: 2, // Expected to be blocked
+	}
+
+	for _, res := range results {
+		require.NotNil(t, res.procIO)
+		resultPrediction[res.procIO.Name]--
+	}
+
+	for predictionType, count := range resultPrediction {
+		require.Equal(t, 0, count, "Unexpected result count for prediction: %s", predictionType)
+	}
+}
+
+type result struct {
+	procIO    stream_types.ProcessorIO
+	err       error
+	RequestID string
+}
+
+func execute(
+	APIStream public_types.APIStreamI,
+	processor stream_types.Processor,
+	resultChan chan result,
+) {
+	procIO, err := processor.Execute("", APIStream)
+	resultChan <- result{procIO, err, APIStream.GetID()}
+}
