64e7a79d232b5c79b5a4c62695eb78a02ced1d76
roygab <108218987+roygab@users.noreply.github.com>
2025-02-19 17:25:21 +0200

    Add concurrent request handling to endless-demo-flow stress test (#1131)

---
+++ proxy/src/services/lunar-engine/streams/processors/processors_config.go
@@ -0,0 +1,34 @@
+package processors
+
+import (
+	processorCountLLMTokens "lunar/engine/streams/processors/count-llm-tokens"
+	processorFilter "lunar/engine/streams/processors/filter-processor"
+	processorGenerateResponse "lunar/engine/streams/processors/generate-response"
+	processorLimiter "lunar/engine/streams/processors/limiter"
+	processorMock "lunar/engine/streams/processors/mock"
+	processorQueue "lunar/engine/streams/processors/queue"
+	processorQuotaDec "lunar/engine/streams/processors/quota-processor-dec"
+	processorQuotaInc "lunar/engine/streams/processors/quota-processor-inc"
+	processorRetry "lunar/engine/streams/processors/retry"
+	processorUserDefinedMetrics "lunar/engine/streams/processors/user-defined-metrics"
+	streamTypes "lunar/engine/streams/types"
+)
+
+type ProcessorFactory func(*streamTypes.ProcessorMetaData) (streamTypes.ProcessorI, error)
+
+var internalProcessorRegistry map[string]ProcessorFactory
+
+func init() {
+	internalProcessorRegistry = map[string]ProcessorFactory{
+		"MockProcessor":      processorMock.NewProcessor,
+		"Retry":              processorRetry.NewProcessor,
+		"Filter":             processorFilter.NewProcessor,
+		"Limiter":            processorLimiter.NewProcessor,
+		"GenerateResponse":   processorGenerateResponse.NewProcessor,
+		"Queue":              processorQueue.NewProcessor,
+		"QuotaProcessorInc":  processorQuotaInc.NewProcessor,
+		"QuotaProcessorDec":  processorQuotaDec.NewProcessor,
+		"UserDefinedMetrics": processorUserDefinedMetrics.NewProcessor,
+		"CountLLMTokens":     processorCountLLMTokens.NewProcessor,
+	}
+}
