ed7872f0ba70edcf6cc42633d5c89cac6e17887c
l-yeg-dev <116944705+l-yeg-dev@users.noreply.github.com>
2025-03-13 16:54:31 +0400

    Levonyeg.cloud 751/caching flow builder (#1295)
    * Add new FF
    
    * Fix FF missing part
    
    * Add Caching flow builder form
    
    * Fix prettier
    
    * Fix prettier
    
    * Connect caching data to yaml
    
    * Connect caching data to yaml
    
    * Add validation for caching rules
    
    * Prettier: fix
    
    * Fix prettier and typecheck
    
    * Remove Generate response block from caching flow. Update the yaml
    
    * Update the tooltip

---
+++ proxy/src/services/lunar-engine/streams/flow/test-processors/mock_processor_using_cache.go
@@ -0,0 +1,53 @@
+package testprocessors
+
+import (
+	publictypes "lunar/engine/streams/public-types"
+	streamtypes "lunar/engine/streams/types"
+)
+
+const (
+	GlobalKeyCacheHit = "cache_hit"
+
+	cacheHitConditionName    = "cacheHit"
+	cacheMissedConditionName = "cacheMissed"
+)
+
+func NewMockProcessorUsingCache(metadata *streamtypes.ProcessorMetaData) (streamtypes.ProcessorI, error) { //nolint:lll
+	return &MockProcessorUsingCache{Name: metadata.Name, Metadata: metadata}, nil
+}
+
+type MockProcessorUsingCache struct {
+	Name     string
+	Metadata *streamtypes.ProcessorMetaData
+}
+
+func (p *MockProcessorUsingCache) Execute(
+	_ string,
+	apiStream publictypes.APIStreamI,
+) (streamtypes.ProcessorIO, error) {
+	err := signInExecution(apiStream, p.Name)
+	if err != nil {
+		return streamtypes.ProcessorIO{}, err
+	}
+	cacheHit, err := apiStream.GetContext().GetGlobalContext().Get(GlobalKeyCacheHit)
+	if err == nil {
+		if cacheHit.(bool) {
+			return streamtypes.ProcessorIO{
+				Type: publictypes.StreamTypeRequest,
+				Name: cacheHitConditionName,
+			}, nil
+		}
+	}
+	return streamtypes.ProcessorIO{
+		Type: publictypes.StreamTypeRequest,
+		Name: cacheMissedConditionName,
+	}, nil
+}
+
+func (p *MockProcessorUsingCache) GetName() string {
+	return p.Name
+}
+
+func (p *MockProcessorUsingCache) GetRequirement() *streamtypes.ProcessorRequirement {
+	return &streamtypes.ProcessorRequirement{}
+}
