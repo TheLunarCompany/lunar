09bf4f8adcfd4fedda84869aff5bb9e02813395a
koverboyLunar <nikitak@lunar.dev>
2025-04-04 17:04:57 +0300

    mikita.cloud-867-flow-builder-limiter-suppert-backend-implementation (#1466)
    * mikita.cloud-867-flow-builder-limiter-suppert-backend-implementation
    
    * mikita.cloud-868-priority-queue-backend-connection
    
    * mikita.cloud-868-priority-queue-backend-connection
    
    ---------
    
    Co-authored-by: Mikita Kauryha <mikita.kauryha@constructor.tech>

---
+++ proxy/src/services/lunar-engine/actions/response_action_transformers.go
@@ -0,0 +1,57 @@
+package actions
+
+import (
+	lunarMessages "lunar/engine/messages"
+	"lunar/engine/utils"
+	sharedActions "lunar/shared-model/actions"
+
+	"github.com/negasus/haproxy-spoe-go/action"
+)
+
+const (
+	ModifyResponseActionName = "modify_response"
+	RetryRequestActionName   = "retry_request"
+	RetryHeadersActionName   = "retry_headers"
+)
+
+// ModifyResponseAction
+func (lunarAction *ModifyResponseAction) RespToSpoeActions() action.Actions {
+	actions := action.Actions{}
+	actions.SetVar(action.ScopeResponse, ModifyResponseActionName, true)
+	actions.SetVar(action.ScopeResponse,
+		ResponseHeadersActionName, utils.DumpHeaders(lunarAction.HeadersToSet))
+	actions.SetVar(action.ScopeResponse, ResponseBodyActionName, lunarAction.Body)
+	actions.SetVar(action.ScopeResponse, StatusCodeActionName, lunarAction.Status)
+	return actions
+}
+
+func (lunarAction *ModifyResponseAction) RespRunResult() sharedActions.RemedyRespRunResult {
+	return sharedActions.RespModifiedResponse
+}
+
+func (lunarAction *ModifyResponseAction) EnsureResponseIsUpdated(
+	onResponse *lunarMessages.OnResponse,
+) {
+	for name, value := range lunarAction.HeadersToSet {
+		onResponse.Headers[name] = value
+	}
+	onResponse.Body = lunarAction.Body
+	onResponse.Status = lunarAction.Status
+}
+
+func (lunarAction *RetryRequestAction) RespToSpoeActions() action.Actions {
+	actions := action.Actions{}
+	actions.SetVar(action.ScopeResponse, RetryRequestActionName, true)
+	actions.SetVar(action.ScopeResponse,
+		RetryHeadersActionName, utils.DumpHeaders(lunarAction.HeadersToSet))
+	return actions
+}
+
+func (lunarAction *RetryRequestAction) RespRunResult() sharedActions.RemedyRespRunResult {
+	return sharedActions.RespRetryRequest
+}
+
+func (lunarAction *RetryRequestAction) EnsureResponseIsUpdated(
+	_ *lunarMessages.OnResponse,
+) {
+}
