a607a162ef47fb08d696ae926cb9708e3c44ed6f
Dmitry Gorohovsky <35920455+d-goro@users.noreply.github.com>
2025-04-16 16:31:04 +0300

    Core 1706 trace processor (#1526)
    * implementation before haproxy
    
    * haproxy
    
    * merge
    
    * tidy
    
    * starting scenario
    
    * go mod
    
    * otel versions
    
    * docker version
    
    * registry
    
    * fixes flow
    
    * traces
    
    * go in studio
    
    * scenario
    
    * scenario
    
    * upgrade go
    
    * go version
    
    * go version
    
    * fix validate error
    
    * validator
    
    * add licenses
    
    * ignore os/signal
    
    * license
    
    * undo

---
+++ proxy/src/services/lunar-engine/streams/internal-types/flow_representation.type.go
@@ -0,0 +1,70 @@
+package internaltypes
+
+import (
+	publictypes "lunar/engine/streams/public-types"
+	"lunar/toolkit-core/network"
+)
+
+type FlowType int
+
+const (
+	UserFlow FlowType = iota
+	SystemFlowStart
+	SystemFlowEnd
+)
+
+type FlowRepI interface {
+	GetName() string
+	GetType() FlowType
+	GetFilter() publictypes.FilterI
+	GetProcessors() map[string]publictypes.ProcessorDataI
+	GetFlow() FlowGraphRepI
+	GetData() network.ConfigurationPayload
+	SetProcessors(map[string]publictypes.ProcessorDataI)
+	SetFilter(publictypes.FilterI)
+	SetType(FlowType)
+	AddProcessor(string, publictypes.ProcessorDataI)
+	SetID(string)
+}
+
+type FlowGraphRepI interface {
+	GetRequest() []FlowConnRepI
+	GetResponse() []FlowConnRepI
+	GetFlowConnections(publictypes.StreamType) []FlowConnRepI
+	SetRequest([]FlowConnRepI)
+	SetResponse([]FlowConnRepI)
+	SetFlowConnections(publictypes.StreamType, []FlowConnRepI) []FlowConnRepI
+}
+
+type FlowConnRepI interface {
+	GetFrom() ConnectionRepI
+	GetTo() ConnectionRepI
+	SetTo(ConnectionRepI)
+	SetFrom(ConnectionRepI)
+}
+
+type ConnectionRepI interface {
+	GetStream() StreamRefI
+	GetFlow() FlowRefI
+	GetProcessor() ProcessorRefI
+	SetStream(StreamRefI)
+	SetFlow(FlowRefI)
+	SetProcessor(ProcessorRefI)
+}
+
+type FlowRefI interface {
+	GetName() string
+	GetAt() string
+}
+
+type StreamRefI interface {
+	GetName() string
+	GetAt() string
+}
+
+type ProcessorRefI interface {
+	GetName() string
+	GetCondition() string
+	GetReferenceName() string
+	GetCreatedByFlow() string
+}
