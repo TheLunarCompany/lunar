c53f823fb1e020e4f83df4c88a5c171d42dd00cc
shania92 <167332449+shania92@users.noreply.github.com>
2025-06-23 18:31:44 +0300

    Mcp 30 publish documentation about our helm chars (#1802)
    * add k8s docs
    
    * add advanced features
    
    * pr fixes
    
    * fix
    
    * split tool groups and update
    
    * typo

---
+++ mcpx/packages/ui/src/components/dashboard/SystemConnectivity/nodes/use-react-flow-data.ts
@@ -0,0 +1,231 @@
+import {
+  Edge,
+  Node,
+  OnEdgesChange,
+  OnNodesChange,
+  useEdgesState,
+  useNodesState,
+} from "@xyflow/react";
+import { CoordinateExtent } from "@xyflow/system";
+import { useEffect } from "react";
+import {
+  AgentData,
+  AgentNode,
+  McpServerData,
+  McpServerNode,
+  McpxNode,
+  NoAgentsNode,
+} from "../types";
+import { NODE_HEIGHT, NODE_WIDTH } from "./constants";
+
+export const useReactFlowData = ({
+  agents,
+  mcpxStatus,
+  mcpServersData,
+  onMcpServerClick,
+  selectedServer,
+  onAgentClick,
+  selectedAgent,
+}: {
+  agents: Array<{
+    id: string;
+    identifier: string;
+    status: "connected" | "disconnected";
+    isSelected?: boolean;
+    last_activity?: Date;
+  }>;
+  mcpxStatus: string;
+  mcpServersData: Array<McpServerData> | null | undefined;
+  onMcpServerClick: (server: McpServerData) => void;
+  selectedServer?: McpServerData;
+  onAgentClick: (agent: AgentData) => void;
+  selectedAgent?: AgentData;
+}): {
+  edges: Edge[];
+  nodes: Node[];
+  onNodesChange: OnNodesChange;
+  onEdgesChange: OnEdgesChange;
+  translateExtent?: CoordinateExtent;
+} => {
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]);
+
+  const mcpServersCount = mcpServersData?.length || 0;
+  const agentsCount = agents.length;
+
+  useEffect(() => {
+    if (!mcpServersData || !Array.isArray(mcpServersData)) {
+      setNodes([]);
+      setEdges([]);
+      return;
+    }
+
+    // Create MCPX node
+    const mcpxNode: McpxNode = {
+      id: "mcpx",
+      position: {
+        x: 0,
+        y: -0.5,
+      },
+      data: {
+        status: mcpxStatus,
+        forceSelected: !selectedServer && !selectedAgent,
+      },
+      selected: !selectedServer && !selectedAgent,
+      type: "mcpx",
+    };
+    // Create MCP servers nodes
+    const serverNodes: McpServerNode[] = mcpServersData.map((server, index) => {
+      const position = {
+        x:
+          mcpServersCount < 5
+            ? NODE_WIDTH
+            : NODE_WIDTH * 2 +
+              10 * (mcpServersCount - Math.abs(index - mcpServersCount / 2)),
+        y:
+          mcpServersCount === 1 ||
+          (mcpServersCount > 3 &&
+            mcpServersCount % 2 === 0 &&
+            index > mcpServersCount / 2 &&
+            index - 1 < mcpServersCount / 2)
+            ? 16
+            : NODE_HEIGHT / 2 +
+              16 +
+              Math.abs(index - mcpServersCount / 2) *
+                (index > mcpServersCount / 2 ? NODE_HEIGHT : -NODE_HEIGHT),
+      };
+
+      return {
+        id: server.id,
+        position,
+        data: {
+          ...server,
+          label: server.name,
+          onClick: () => onMcpServerClick(server),
+        },
+        selected: selectedServer?.id === server.id,
+        type: "mcpServer",
+      };
+    });
+    // Create Agent nodes
+    const agentNodes: AgentNode[] = agents.map((agent, index) => ({
+      id: agent.id,
+      position: {
+        x:
+          agentsCount < 5
+            ? -NODE_WIDTH
+            : -(
+                NODE_WIDTH * 2 +
+                10 * (agentsCount - Math.abs(index - agentsCount / 2))
+              ),
+        y:
+          agentsCount === 1 ||
+          (agentsCount > 3 &&
+            agentsCount % 2 === 0 &&
+            index > agentsCount / 2 &&
+            index - 1 < agentsCount / 2)
+            ? 20
+            : NODE_HEIGHT / 2 +
+              10 +
+              Math.abs(index - agentsCount / 2) *
+                (index > agentsCount / 2 ? NODE_HEIGHT : -NODE_HEIGHT),
+      },
+      data: {
+        ...agent,
+        label: agent.identifier,
+        onClick: () => onAgentClick(agent),
+      },
+      selected: selectedAgent?.id === agent.id,
+      type: "agent",
+    }));
+    // Create `NoAgents` node if no agents are present
+    const noAgentsNodes: NoAgentsNode[] =
+      agentsCount === 0
+        ? [
+            {
+              data: {},
+              id: "no-agents",
+              position: {
+                x: -NODE_WIDTH,
+                y: 33,
+              },
+              type: "noAgents",
+            },
+          ]
+        : [];
+    // Create MCP edges
+    const mcpServersEdges: Edge[] = mcpServersData.map(({ id, status }) => {
+      const isRunning = status === "connected_running";
+      const isSelected = selectedServer?.id === id;
+      return {
+        animated: isRunning,
+        className: isRunning
+          ? "text-[var(--color-fg-success)]"
+          : "text-[var(--color-gray-1)]",
+        id: `e-mcpx-${id}`,
+        selected: isSelected,
+        source: "mcpx",
+        style: {
+          stroke: "currentColor",
+          strokeWidth: isRunning && isSelected ? 1.5 : 1,
+        },
+        target: id,
+      };
+    });
+    // Create Agent edges
+    const agentsEdges: Edge[] = agents.map(({ id, last_activity, status }) => {
+      const isActive = last_activity !== undefined;
+      const isConnected = status === "connected";
+      const isSelected = selectedAgent?.id === id;
+      return {
+        animated: isActive,
+        className:
+          isActive && isConnected
+            ? "text-[var(--color-fg-success)]"
+            : "text-[var(--color-gray-1)]",
+        id: `e-${id}`,
+        selected: isSelected,
+        source: id,
+        style: {
+          stroke: "currentColor",
+          strokeWidth: isActive && isSelected ? 1.5 : 1,
+        },
+        target: "mcpx",
+      };
+    });
+
+    setNodes([mcpxNode, ...serverNodes, ...agentNodes, ...noAgentsNodes]);
+    setEdges([...mcpServersEdges, ...agentsEdges]);
+  }, [
+    agents,
+    mcpServersData,
+    mcpxStatus,
+    onAgentClick,
+    onMcpServerClick,
+    selectedAgent,
+    selectedServer,
+    setEdges,
+    setNodes,
+  ]);
+
+  const maxCount = Math.max(mcpServersCount, agentsCount);
+  const dynamicTranslateExtent: CoordinateExtent = [
+    [-(maxCount * NODE_WIDTH * 3), -(NODE_HEIGHT * 5)],
+    [maxCount * NODE_WIDTH * 3, NODE_HEIGHT * 5],
+  ];
+
+  return {
+    edges,
+    nodes,
+    onEdgesChange,
+    onNodesChange,
+    translateExtent:
+      // The dynamic translate extent work well for up to 9 nodes,
+      // but for more than that the most extreme nodes get clipped and become unreachable.
+      // This is a limitation of the current implementation, so we set it to undefined for infinite scrolling.
+      // TODO: Fix the dynamic translate extent to handle more than 9 nodes,
+      //  or implement a better way to position large numbers of nodes
+      //  (e.g. some internal grid system).
+      maxCount > 9 ? undefined : dynamicTranslateExtent,
+  };
+};
---
+++ proxy/src/services/lunar-engine/metrics/metric_manager.go
@@ -0,0 +1,283 @@
+package metrics
+
+import (
+	"context"
+	"fmt"
+	"lunar/toolkit-core/otel"
+	"sync"
+
+	"go.opentelemetry.io/otel/attribute"
+	"golang.org/x/exp/slices"
+
+	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/otel/metric"
+)
+
+type MetricManager struct {
+	config           *Config
+	meter            metric.Meter
+	metricObjects    map[Metric]interface{}
+	generalMetricReg metric.Registration
+	systemMetricReg  metric.Registration
+
+	labelManager                  *LabelManager
+	apiCallMetricMng              *apiCallCountMetricManager
+	transactionMetricsManager     *transactionMetricsManager
+	remainingConnectionsMetricMng *remainingConnectionsMetricManager
+	providerData                  *metricsProviderData
+
+	metricManagerActive bool
+
+	mu sync.Mutex
+}
+
+func NewMetricManager() (*MetricManager, error) {
+	log.Info().Msg("Initializing metrics manager")
+	meter := otel.GetMeter()
+	config, err := loadMetricsConfig()
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get metrics config")
+		return &MetricManager{}, err
+	}
+
+	labeledEndpointMng := NewLabeledEndpointManager(config.LabeledEndpoints)
+	mng := &MetricManager{
+		config:        config,
+		meter:         meter,
+		metricObjects: make(map[Metric]interface{}),
+		providerData:  newMetricsProviderData(),
+		labelManager: NewLabelManager(config.GeneralMetrics.LabelValue).
+			WithLabeledEndpointManager(labeledEndpointMng),
+	}
+
+	// api call count metric - special treatment
+	mng.apiCallMetricMng, err = newAPICallMetricManager(meter, mng.labelManager)
+	if err != nil {
+		return &MetricManager{}, fmt.Errorf("failed to initialize APICallCountMetric: %w", err)
+	}
+
+	mng.remainingConnectionsMetricMng, err = newRemainingConnectionsMetricManager(meter)
+	if err != nil {
+		return mng, fmt.Errorf("failed to initialize remaining connections metric: %w", err)
+	}
+
+	mng.transactionMetricsManager, err = newTransactionMetricsManager(meter, config, mng.labelManager)
+	if err != nil {
+		return mng, fmt.Errorf("failed to initialize transaction metrics: %w", err)
+	}
+
+	// general metrics
+	if err := mng.initializeGeneralMetrics(); err != nil {
+		return mng, fmt.Errorf("failed to initialize general metrics: %w", err)
+	}
+
+	// system metrics
+	if err := mng.initializeSystemMetrics(); err != nil {
+		return mng, fmt.Errorf("failed to initialize system metrics: %w", err)
+	}
+
+	mng.metricManagerActive = true
+	log.Info().Msg("Metrics manager initialized")
+	return mng, nil
+}
+
+func (m *MetricManager) ReloadMetricsConfig() error {
+	log.Info().Msg("Reloading metrics config")
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	config, err := loadMetricsConfig()
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get metrics config")
+		return err
+	}
+
+	if m.config == nil {
+		m.metricManagerActive = true
+		m.config = config
+		log.Info().Msg("Metrics manager reloaded")
+		return nil
+	}
+
+	if !slices.Equal(config.GeneralMetrics.LabelValue, m.config.GeneralMetrics.LabelValue) {
+		log.Info().Msg("Reloading labels")
+		m.labelManager.SetLabels(config.GeneralMetrics.LabelValue)
+	}
+
+	if !config.EqualLabeledEndpoints(m.config) {
+		log.Info().Msg("Reloading labeled endpoints")
+		m.labelManager.labeledEndpointMng.SetLabeledEndpoints(config.LabeledEndpoints)
+	}
+
+	if !config.GeneralMetrics.Equal(m.config.GeneralMetrics) {
+		log.Info().Msg("Reloading general metrics")
+		if err = m.initializeGeneralMetrics(); err != nil {
+			return fmt.Errorf("failed to initialize general metrics: %w", err)
+		}
+	}
+
+	if !config.EqualSystemMetrics(*m.config) {
+		log.Info().Msg("Reloading system metrics")
+		if err = m.initializeSystemMetrics(); err != nil {
+			return fmt.Errorf("failed to initialize system metrics: %w", err)
+		}
+	}
+
+	m.metricManagerActive = true
+	log.Info().Msg("Metrics manager reloaded")
+	return nil
+}
+
+// UpdateMetricsForAPICall updates the general metrics - relevant for the API calls
+func (m *MetricManager) UpdateMetricsForAPICall(provider APICallMetricsProviderI) {
+	if !m.metricManagerActive {
+		return
+	}
+
+	if provider.GetType().IsResponseType() {
+		m.providerData.UpdateAPICallData(provider)
+	}
+}
+
+// UpdateMetricsForFlow updates the system metrics - relevant for the flows
+func (m *MetricManager) UpdateMetricsForFlow(provider FlowMetricsProviderI) {
+	if !m.metricManagerActive {
+		return
+	}
+	if m.providerData == nil {
+		m.providerData = newMetricsProviderData()
+	}
+
+	m.providerData.UpdateFlowData(provider)
+}
+
+// initializeMetrics initializes the metrics by parsing
+func (m *MetricManager) initializeMetrics(metrics []MetricValue) ([]metric.Observable, error) {
+	log.Info().Msgf("Initializing metrics: %+v", metrics)
+
+	var meterObjs []metric.Observable
+	for _, metricValue := range metrics {
+		if _, ok := accessLogBasedMetrics[metricValue.Name]; ok {
+			continue
+		}
+
+		meterObj, err := registerObservableMetric(m.meter, metricValue)
+		if err != nil {
+			return nil, fmt.Errorf("failed to initialize general metric %s: %w", metricValue.Name, err)
+		}
+		meterObjs = append(meterObjs, meterObj)
+		m.metricObjects[metricValue.Name] = meterObj
+	}
+	return meterObjs, nil
+}
+
+func (m *MetricManager) observeGeneralMetrics(_ context.Context, observer metric.Observer) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	err := observeMetric(m, APICallSizeMetric, m.providerData.GetAvgAPICallSize(), observer)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *MetricManager) observeSystemMetrics(_ context.Context, observer metric.Observer) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	data := m.providerData.GetFlowData()
+	if err := observeMetric(m, ActiveFlowsMetric, data.GetActiveFlows(), observer); err != nil {
+		log.Error().Err(err).Msgf("Failed to observe %v", ActiveFlowsMetric)
+	}
+
+	for flowName, counter := range data.GetFlowInvocations() {
+		if err := observeMetric(m, FlowsInvocationsMetric,
+			counter, observer, attribute.String(FlowName, flowName)); err != nil {
+			log.Error().Err(err).Msgf("Failed to observe %v", FlowsInvocationsMetric)
+		}
+	}
+
+	if err := observeMetric(m, RequestsThroughFlowsMetric,
+		data.GetRequestsThroughFlows(), observer); err != nil {
+		log.Error().Err(err).Msgf("Failed to observe %v", RequestsThroughFlowsMetric)
+	}
+
+	if err := observeMetric(m, AvgFlowExecutionTimeMetric,
+		data.GetAvgFlowExecutionTime(), observer); err != nil {
+		log.Error().Err(err).Msgf("Failed to observe %v", AvgFlowExecutionTimeMetric)
+	}
+
+	if err := observeMetric(m, AvgProcessorExecutionTimeMetric,
+		data.GetAvgProcessorExecutionTime(), observer); err != nil {
+		log.Error().Err(err).Msgf("Failed to observe %v", AvgProcessorExecutionTimeMetric)
+	}
+
+	return nil
+}
+
+func observeMetric[T int64 | float64](
+	mng *MetricManager,
+	metricName Metric,
+	value T,
+	observer metric.Observer,
+	attributes ...attribute.KeyValue,
+) error {
+	meterObjRaw, found := mng.metricObjects[metricName]
+	if !found {
+		return fmt.Errorf("metric %s not found", metricName)
+	}
+
+	attributes = appendGatewayIDAttribute(attributes)
+
+	switch meterObj := meterObjRaw.(type) {
+	case metric.Float64ObservableCounter, metric.Float64ObservableUpDownCounter, metric.Float64ObservableGauge: //nolint:lll
+		if observable, ok := meterObj.(metric.Float64Observable); ok {
+			observer.ObserveFloat64(observable, float64(value), metric.WithAttributes(attributes...))
+		} else {
+			log.Error().Msgf("metric %s is not a Float64Observable", metricName)
+		}
+	case metric.Int64ObservableUpDownCounter, metric.Int64ObservableCounter, metric.Int64ObservableGauge: //nolint:lll
+		if observable, ok := meterObj.(metric.Int64Observable); ok {
+			observer.ObserveInt64(observable, int64(value), metric.WithAttributes(attributes...))
+		} else {
+			log.Error().Msgf("metric %s is not an Int64Observable", metricName)
+		}
+	default:
+		log.Error().Msgf("unsupported metric type for %s", metricName)
+	}
+	return nil
+}
+
+func (m *MetricManager) initializeGeneralMetrics() error {
+	meterObjs, err := m.initializeMetrics(m.config.GeneralMetrics.MetricValue)
+	if err != nil {
+		return err
+	}
+
+	if m.generalMetricReg != nil {
+		_ = m.generalMetricReg.Unregister()
+	}
+	m.generalMetricReg, err = m.meter.RegisterCallback(m.observeGeneralMetrics, meterObjs...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *MetricManager) initializeSystemMetrics() error {
+	meterObjs, err := m.initializeMetrics(m.config.SystemMetrics)
+	if err != nil {
+		return fmt.Errorf("failed to initialize system metrics: %w", err)
+	}
+
+	if m.systemMetricReg != nil {
+		_ = m.systemMetricReg.Unregister()
+	}
+	m.systemMetricReg, err = m.meter.RegisterCallback(m.observeSystemMetrics, meterObjs...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
