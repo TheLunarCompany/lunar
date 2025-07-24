d5b7d3dc056e671c65dc2c046eb0d42cab6ab76c
roygab <108218987+roygab@users.noreply.github.com>
2025-07-24 11:57:46 +0300

    Studio support for all-in-one (#1880)
    * Studio support for all-in-one
    
    * changed log level

---
+++ mcpx/packages/mcpx-server/src/interception.ts
@@ -0,0 +1,48 @@
+import { env } from "./env.js";
+import { StdioTargetServer } from "./model/target-servers.js";
+import { DockerService } from "./services/docker.js";
+
+export async function prepareCommand(
+  targetServer: StdioTargetServer,
+  dockerService: DockerService,
+): Promise<{
+  command: string;
+  args: string[];
+}> {
+  const command = targetServer.command;
+  const args = targetServer.args || [];
+
+  if (args.length === 0) {
+    return Promise.reject(`No arguments specified.`);
+  }
+
+  switch (command) {
+    case "npx":
+      return { command, args };
+    case "docker": {
+      if (!env.DIND_ENABLED) {
+        return Promise.reject(
+          "Docker in Docker is not enabled. Cannot start docker mcp server.",
+        );
+      }
+
+      if (!env.INTERCEPTION_ENABLED) {
+        return { command, args };
+      }
+      const modifiedArgs = await dockerService
+        .createImageWithCa(args)
+        .catch((error) => {
+          return Promise.reject(
+            `Failed to create Docker image with CA: ${error}`,
+          );
+        });
+      return {
+        command,
+        args: modifiedArgs,
+      };
+    }
+
+    default:
+      return { command, args };
+  }
+}
