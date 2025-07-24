8a48c4de84f55592706e5f8ba979f9de1e881833
eitangol <108608289+eitangol@users.noreply.github.com>
2025-07-24 19:01:08 +0300

    Added telemetry logger that reports back to our Loki server (#1879)
    * Added telemetry logger that reports back to our Loki server
    
    * Now if we cannot create the DinD interception we revert to without interception
    
    * Update entrypoint.sh
    
    * .

---
+++ mcpx/packages/mcpx-server/src/model/config/tool-extensions.ts
@@ -0,0 +1,33 @@
+export interface ToolExtensions {
+  services: {
+    [serviceName: string]: ServiceToolExtensions;
+  };
+}
+
+export interface ServiceToolExtensions {
+  [toolName: string]: {
+    childTools: ToolExtension[];
+  };
+}
+
+export interface ToolExtension {
+  name: string;
+  description?: ToolExtensionDescription;
+  overrideParams: {
+    [paramName: string]: ToolExtensionOverrideValue;
+  };
+}
+
+export interface ToolExtensionDescription {
+  action: "append" | "rewrite";
+  text: string;
+}
+
+export type ToolExtensionOverrideValue =
+  | null
+  | undefined
+  | string
+  | number
+  | boolean
+  | { [key: string]: ToolExtensionOverrideValue }
+  | Array<ToolExtensionOverrideValue>;
