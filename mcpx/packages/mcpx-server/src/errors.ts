25e953fe939374651900cbe3ad48389bfd44ce04
eitangol <108608289+eitangol@users.noreply.github.com>
2025-07-27 21:17:34 +0300

    Bump version to v.0.2.3

---
+++ mcpx/packages/mcpx-server/src/errors.ts
@@ -0,0 +1,4 @@
+export class AlreadyExistsError extends Error {}
+export class NotFoundError extends Error {}
+export class FailedToConnectToTargetServer extends Error {}
+export class InvalidConfigError extends Error {}
---
+++ mcpx/packages/toolkit-core/src/logging/logger.ts
@@ -0,0 +1,114 @@
+import { format, Logger, transports, createLogger } from "winston";
+import {
+  NextFunction,
+  Request as ExpressRequest,
+  Response as ExpressResponse,
+  RequestHandler,
+} from "express";
+
+import LokiTransport from "winston-loki";
+import { env } from "../env.js";
+
+const { combine, timestamp, label, printf, splat, metadata } = format;
+
+interface LunarLogger extends Logger {
+  get telemetry(): Logger;
+}
+
+const logFormat = printf(({ level, message, label, metadata, timestamp }) => {
+  const metaString = metadata
+    ? Object.entries(metadata)
+        .map(([key, value]) => `${key}=${JSON.stringify(value)}`)
+        .join(" ")
+    : "";
+  return `${timestamp} [${label}] ${level.toUpperCase()}: ${message} ${metaString}`;
+});
+
+const defaultTelemetryLabels = {
+  service: "mcpx",
+  version: env.VERSION,
+  instance_id: env.INSTANCE_ID,
+  lunar_key: env.LUNAR_API_KEY,
+};
+
+export const noOpLogger: Logger = createLogger({ silent: true });
+
+export function buildLogger(
+  props: { logLevel: string; label?: string } = { logLevel: "info" }
+): LunarLogger {
+  const { logLevel, label: loggerLabel } = props;
+  const combinedFormat = combine(
+    label({ label: loggerLabel }),
+    timestamp(),
+    splat(),
+    metadata({ fillExcept: ["message", "level", "timestamp", "label"] }),
+    logFormat
+  );
+
+  const baseLogger = createLogger({
+    level: logLevel.toLowerCase(),
+    format: combinedFormat,
+    transports: [new transports.Console()],
+  });
+
+  if (!env.LUNAR_TELEMETRY) {
+    return Object.assign(baseLogger, {
+      telemetry: noOpLogger,
+    });
+  }
+
+  const telemetryLogger = createLogger({
+    level: logLevel.toLowerCase(),
+    format: combinedFormat,
+    transports: [
+      new LokiTransport({
+        host: env.LOKI_URL,
+        labels: { ...defaultTelemetryLabels, component: loggerLabel },
+        json: true,
+      }),
+    ],
+  });
+
+  const originalClose = baseLogger.close.bind(baseLogger);
+
+  return Object.assign(baseLogger, {
+    telemetry: telemetryLogger,
+    close: () => {
+      telemetryLogger.transports.forEach((transport) => transport.close?.());
+      baseLogger.transports.forEach((transport) => transport.close?.());
+      telemetryLogger.close();
+      return originalClose();
+    },
+  });
+}
+
+// Middleware to log requests and responses
+export function accessLogFor(
+  logger: Logger,
+  ignore: { method: string; path: string }[] = []
+): RequestHandler {
+  return function accessLog(
+    req: ExpressRequest,
+    res: ExpressResponse,
+    next: NextFunction
+  ): void {
+    const { method, originalUrl } = req;
+    const primitiveIgnore = new Set(
+      ignore.map((i) => `${i.method}:::${i.path}`)
+    );
+    if (primitiveIgnore.has(`${method}:::${originalUrl}`)) {
+      return next();
+    }
+
+    const start = Date.now();
+
+    res.on("finish", () => {
+      const duration = Date.now() - start;
+      logger.info(
+        `[access-log] ${method} ${originalUrl} ${res.statusCode} - ${duration}ms`
+      );
+    });
+
+    next();
+  };
+}
