d5435ba7caad4d1ee9c5bea5ff5feb32f4208dd6
RotemY-Lunar <rotemy@lunar.dev>
2025-08-21 21:27:23 +0300

    Rotemy.mcp 201/revert changes (#2014)
    * Revert "Mcp 201 automate the mcpx release process (#2008)"
    
    This reverts commit aa1192909f5265c99acad07e8590268261cd7c24.
    
    * update the version to v0.2.9-fix2

---
+++ mcpx/packages/ui/src/data/mcp-server.ts
@@ -0,0 +1,57 @@
+import {
+  createTargetServerRequestSchema,
+  UpdateTargetServerRequest,
+} from "@mcpx/shared-model";
+import { useMutation } from "@tanstack/react-query";
+import { z } from "zod/v4";
+import { axiosClient } from "./axios-client";
+
+export type TargetServerInput = z.input<typeof createTargetServerRequestSchema>;
+
+export async function addMcpServer({
+  payload,
+}: {
+  payload: TargetServerInput;
+}) {
+  const response = await axiosClient.post("/target-server", payload);
+  return response.data;
+}
+
+export const useAddMcpServer = () =>
+  useMutation({
+    mutationKey: ["add-mcp-Server"],
+    mutationFn: addMcpServer,
+  });
+
+export async function deleteMcpServer({ name }: { name: string }) {
+  const response = await axiosClient.delete(
+    `/target-server/${encodeURIComponent(name)}`,
+  );
+  return response.data;
+}
+
+export const useDeleteMcpServer = () =>
+  useMutation({
+    mutationKey: ["delete-mcp-Server"],
+    mutationFn: deleteMcpServer,
+  });
+
+export async function editMcpServer({
+  name,
+  payload,
+}: {
+  name: string;
+  payload: UpdateTargetServerRequest;
+}) {
+  const response = await axiosClient.patch(
+    `/target-server/${encodeURIComponent(name)}`,
+    payload,
+  );
+  return response.data;
+}
+
+export const useEditMcpServer = () =>
+  useMutation({
+    mutationKey: ["edit-mcp-Server"],
+    mutationFn: editMcpServer,
+  });
