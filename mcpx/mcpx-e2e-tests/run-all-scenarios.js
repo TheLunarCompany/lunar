77283a55654b64283f502f4ca9cc7b5ef6ba9b8a
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2025-08-22 18:44:20 +0200

    [mcpx] Dont shutdown connections on updates (#2016)
    * dont shutdown connections on updates
    
    * fix dockerfile

---
+++ mcpx/mcpx-e2e-tests/run-all-scenarios.js
@@ -0,0 +1,16 @@
+// run-all-scenarios.js
+var execSync = require('child_process').execSync;
+var readdirSync = require('fs').readdirSync;
+var lstatSync = require('fs').lstatSync;
+var join = require('path').join;
+
+var testsDir = join(__dirname, 'tests');
+var entries = readdirSync(testsDir);
+
+entries.forEach(function (name) {
+  var fullPath = join(testsDir, name);
+  if (lstatSync(fullPath).isDirectory()) {
+    console.log('\n➡️  Running test suite: ' + name + '\n');
+    execSync('npm run test-scenario -- tests/' + name, { stdio: 'inherit' });
+  }
+});
