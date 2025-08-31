7b6ed7615a665277addaf66f84d8410e1e767f75
maya-barak <133572647+maya-barak@users.noreply.github.com>
2025-08-31 16:15:18 +0300

    fix error (#2077)

---
+++ mcpx/packages/mcpx-server/src/interception.ts
@@ -0,0 +1,53 @@
+import { env } from "./env.js";
+import { FailedToConnectToTargetServer } from "./errors.js";
+import { StdioTargetServer } from "./model/target-servers.js";
+import { DockerService } from "./services/docker.js";
+
+// The returned error should be a FailedToConnectToTargetServer.
+export async function prepareCommand(
+  targetServer: StdioTargetServer,
+  dockerService: DockerService,
+): Promise<{
+  command: string;
+  args: string[];
+}> {
+  const command = targetServer.command;
+  const args = targetServer.args || [];
+
+  if (args.length === 0) {
+    return Promise.reject(
+      new FailedToConnectToTargetServer("No arguments specified."),
+    );
+  }
+
+  switch (command) {
+    case "npx":
+      return { command, args };
+    case "docker": {
+      if (!env.DIND_ENABLED) {
+        return Promise.reject(
+          new FailedToConnectToTargetServer(
+            "Docker in Docker is not enabled. Cannot start docker mcp server. Please try to run MCPX server with '--privileged' access and try again.",
+          ),
+        );
+      }
+
+      if (!env.INTERCEPTION_ENABLED) {
+        return { command, args };
+      }
+
+      try {
+        const modifiedArgs = await dockerService.createImageWithCa(args);
+        return {
+          command,
+          args: modifiedArgs,
+        };
+      } catch (_) {
+        return { command, args };
+      }
+    }
+
+    default:
+      return { command, args };
+  }
+}
---
+++ mcpx/packages/webserver/eslint.config.js
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { project: true },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      // allowing only for `_` to be an unused var
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/switch-exhaustiveness-check": [
+        "error",
+        { considerDefaultExhaustiveForUnions: true },
+      ],
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { arguments: false } },
+      ],
+    },
+  },
+  globalIgnores([
+    "./lunar-interceptor.ts",
+    "./eslint.config.js",
+    "./jest.config.cjs",
+    "./loader.mjs",
+    "./dist",
+  ]),
+  tseslint.configs.recommended,
+]);
