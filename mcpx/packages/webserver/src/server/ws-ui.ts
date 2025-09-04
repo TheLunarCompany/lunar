6aa75467631b909d6555d0dd70bcfdeab5dac5e0
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2025-09-04 17:45:27 +0200

    [mcpx] connected client cluster (by name) (#2094)
    * connected clients cluster grouping by name
    
    * refactoring
    
    * rm comments

---
+++ mcpx/packages/webserver/src/server/ws-ui.ts
@@ -0,0 +1,77 @@
+import {
+  UI_ClientBoundMessage,
+  UI_ServerBoundMessage,
+} from "@mcpx/shared-model/api";
+import { Server as HTTPServer } from "http";
+import { Socket, Server as WSServer } from "socket.io";
+import { Logger } from "winston";
+import { Services } from "../services/services.js";
+
+export function bindUIWebsocket(
+  server: HTTPServer,
+  services: Services,
+  logger: Logger,
+): void {
+  const io = new WSServer(server, {
+    path: "/ws-ui",
+    cors: {
+      origin: true, // Allow all origins in development/container environments
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    logger.debug("UI connected:", { id: socket.id });
+    services.connections.uiSocket = socket;
+
+    socket.on("disconnect", () => {
+      services.connections.uiSocket = null;
+      logger.debug("UI disconnected:", { id: socket.id });
+    });
+
+    // Handle events from UI to the webserver
+    Object.entries(UI_ServerBoundMessage).forEach(([_, eventName]) => {
+      socket.on(eventName, async (payload) => {
+        handleWsEvent(services, logger, socket, eventName, payload);
+      });
+    });
+
+    io.on("disconnect", () => {
+      logger.debug("WebSocket server disconnected");
+    });
+  });
+}
+
+async function handleWsEvent(
+  services: Services,
+  logger: Logger,
+  socket: Socket,
+  eventName: UI_ServerBoundMessage,
+  _payload: unknown,
+): Promise<void> {
+  logger.debug(`Received event: ${eventName}`, {
+    payload: _payload,
+    id: socket.id,
+  });
+
+  // Here you can handle the event based on its type
+  switch (eventName) {
+    case UI_ServerBoundMessage.GetAppConfig: {
+      logger.debug("Fetching current app config");
+      await services.dal.fetchCurrentAppConfig().then((config) => {
+        socket.emit(UI_ClientBoundMessage.AppConfig, config);
+      });
+      break;
+    }
+    case UI_ServerBoundMessage.GetSystemState: {
+      logger.debug("Fetching current system state");
+      await services.dal.fetchCurrentSystemState().then((state) => {
+        socket.emit(UI_ClientBoundMessage.SystemState, state);
+      });
+      break;
+    }
+  }
+
+  logger.debug(`Handled event: ${eventName}`, { id: socket.id });
+}
