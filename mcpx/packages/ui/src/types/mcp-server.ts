2d3793b729bb3d514e45a1a07ed3c0eb28f75cb2
Nikita(coverboy) <47757195+koverboy@users.noreply.github.com>
2025-09-30 10:49:03 +0200

    MCP-366-Align-the-implementation (#2157)
    * MCP-366-Align-the-implementation
    
    * MCP-366-Align-the-implementation
    
    * MCP-366-Align-the-implementation
    
    * MCP-366-Align-the-implementation
    
    ---------
    
    Co-authored-by: Mikita Kauryha <mikita.kauryha@constructor.tech>

---
+++ mcpx/packages/ui/src/types/mcp-server.ts
@@ -0,0 +1,36 @@
+export type McpServerTool = {
+  name: string;
+  description: string;
+  lastCalledAt?: Date | string | number | null;
+  invocations: number;
+};
+
+export type McpServerUsage = {
+  callCount: number;
+  lastCalledAt?: Date | string | number | null;
+};
+
+export type McpServerStatus =
+  | "connected_running"
+  | "connected_stopped"
+  | "connection_failed"
+  | "pending_auth";
+
+export type McpServerType = "stdio" | "sse" | "streamable-http";
+
+export type McpServer = {
+  args: string[];
+  command?: string;
+  configuration?: Record<string, any>;
+  env?: Record<string, string>;
+  icon?: string;
+  id: string;
+  name: string;
+  status: McpServerStatus;
+  connectionError?: string | null;
+  tools: Array<McpServerTool>;
+  usage: McpServerUsage;
+  type: McpServerType;
+  url?: string;
+  headers?: Record<string, string>;
+};
