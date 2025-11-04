4df8525545b903601f3dc6b8d25c7382b95c39cc
maya-barak <133572647+maya-barak@users.noreply.github.com>
2025-11-04 10:34:10 +0200

    Add assigned users list to Profile page (#2268)
    * add assigned users
    
    * new component
    
    * new component

---
+++ mcpx/packages/mcpx-server/src/server/auth-mcpx.ts
@@ -0,0 +1,48 @@
+import { Request, Response, Router } from "express";
+import { Logger } from "winston";
+import { HubService } from "../services/hub.js";
+import { loggableError } from "@mcpx/toolkit-core/logging";
+import { env } from "../env.js";
+
+export function buildAuthMcpxRouter(
+  hubService: HubService,
+  logger: Logger,
+): Router {
+  const router = Router();
+
+  router.get("/auth/mcpx", (_req: Request, res: Response) => {
+    logger.debug("Getting auth status");
+    const status = hubService.status;
+    res.status(200).json(status);
+  });
+
+  router.post("/auth/mcpx", async (_req: Request, res: Response) => {
+    try {
+      logger.info("Connecting to Hub with provided token");
+      const hubAuthStatus = await hubService.connect(env.INSTANCE_ID);
+
+      res.json(hubAuthStatus);
+    } catch (e) {
+      const error = loggableError(e);
+      logger.error("Authentication failed", { error });
+      res.status(401).json({
+        error: "Authentication failed",
+        message: error,
+      });
+    }
+  });
+
+  router.delete("/auth/mcpx", async (_req: Request, res: Response) => {
+    try {
+      logger.info("Disconnecting from Hub");
+      await hubService.disconnect();
+      res.status(204).send();
+    } catch (e) {
+      const error = loggableError(e);
+      logger.error("Disconnection failed", { error });
+      res.status(500).json({ error: "Disconnection failed", message: error });
+    }
+  });
+
+  return router;
+}
