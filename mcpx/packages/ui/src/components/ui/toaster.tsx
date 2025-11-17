3a542713ccf0931530a361280939f0c47f105ad2
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2025-11-17 18:30:43 +0100

    [mcpx] delete attrribute config on target server deletion (#2309)

---
+++ mcpx/packages/ui/src/components/ui/toaster.tsx
@@ -0,0 +1,35 @@
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+
+export function Toaster() {
+  const { toasts } = useToast();
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, domain, ...props }) {
+        return (
+          <ToastViewport key={id} position={props.position}>
+            <Toast {...props} domain={domain}>
+
+              <div className="grid">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+              {action }
+              <ToastClose />
+            </Toast>
+          </ToastViewport>
+        );
+      })}
+    </ToastProvider>
+  );
+}
