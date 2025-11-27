b28f2a6bf533ba00eb954751978e777365377626
eitangol <108608289+eitangol@users.noreply.github.com>
2025-11-27 18:45:56 +0200

    Mcp 507 emailclaimauth bff make the email claim be configurable (#2351)
    * Added emailClaim configuration
    
    * Update well-known.ts
    
    * Added support in lua for user specific email claim
    
    * Update nginx.conf
    
    * Update oidc-callback-processor.ts
    
    * Update oidc-callback-processor.ts

---
+++ mcpx/packages/toolkit-core/src/data/collections.ts
@@ -0,0 +1,43 @@
+// A function to remove `null`s and/or `undefined`s from an array (immutable)
+export function compact<X>(xs: Array<X | null | undefined>): X[] {
+  return xs.flatMap((x) => (x ? [x] : []));
+}
+
+// A function to remove `null`s and/or `undefined`s from an object (immutable)
+export function compactRecord<T>(
+  record: Record<string, T | null | undefined>
+): Record<string, T> {
+  return Object.fromEntries(
+    Object.entries(record).flatMap(([key, value]) =>
+      value ? [[key, value]] : []
+    )
+  );
+}
+
+// A function to index array items by a string that can be
+// extracted from each item. In case of plural items, the last item
+// will be used for the index.
+// (immutable)
+export function indexBy<X>(
+  xs: X[],
+  groupByF: (x: X) => string
+): { [group: string]: X } {
+  return xs.reduce<{ [group: string]: X }>((res, item) => {
+    const group = groupByF(item);
+    res[group] = item;
+    return res;
+  }, {});
+}
+
+// A function to group array items by a string that can be
+// extracted from each item
+export function groupBy<X>(
+  xs: X[],
+  groupByF: (x: X) => string
+): { [group: string]: X[] } {
+  return xs.reduce<{ [group: string]: X[] }>((res, item) => {
+    const group = groupByF(item);
+    res[group] = [...(res[group] || []), item];
+    return res;
+  }, {});
+}
