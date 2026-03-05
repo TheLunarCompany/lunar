b92e039abd6004893309117ce3d80f54402d7385
eitangol <108608289+eitangol@users.noreply.github.com>
2026-03-05 18:57:08 +0200

    Addded git actions (#2672)

---
+++ mcpx/packages/ui/src/components/dashboard/EnvVarsEditor/index.ts
@@ -0,0 +1,3 @@
+export { EnvVarsEditor } from "./EnvVarsEditor";
+export { FixedInput, FromEnvInput, LiteralInput } from "./inputs";
+export type { UserEnvVarsEditorProps as EnvVarsEditorProps } from "./types";
