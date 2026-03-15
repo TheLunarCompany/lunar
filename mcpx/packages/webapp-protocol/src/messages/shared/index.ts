140d85812683e9ffe093e6455e072c9942ab6797
maya-barak <133572647+maya-barak@users.noreply.github.com>
2026-03-15 15:47:09 +0200

    [MCPX] Add fromCatalog (Server Origin) for Setup Target Servers (#2679)
    * add migrations
    
    * add tests
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix for cr
    
    * fix for cr 0.2
    
    * fix lint
    
    * fix tests
    
    * fix tests
    
    * fix for cr 0.3
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix for cr 0.3
    
    * fix for cr 0.3
    
    * fix for cr 0.4
    
    * fix ui
    
    * fix ui

---
+++ mcpx/packages/webapp-protocol/src/messages/shared/index.ts
@@ -0,0 +1,33 @@
+export {
+  targetServerSchema,
+  targetServerEntrySchema,
+  targetServerStdioSchema,
+  targetServerSseSchema,
+  targetServerStreamableHttpSchema,
+  normalizedToolGroupSchema,
+  setupConfigSchema,
+  envValueSchema,
+  type TargetServer,
+  type TargetServerEntry,
+  type EnvValue,
+} from "./setup.js";
+
+// Re-export schemas from shared-model for convenience
+export {
+  consumerConfigSchema,
+  singleServerAttributesSchema,
+  staticOAuthProviderSchema,
+  staticOAuthSchema,
+  toolExtensionSchema,
+  toolExtensionsSchema,
+  catalogMCPServerSchema,
+  catalogMCPServerListSchema,
+  catalogConfigSchema,
+  catalogStdioConfigSchema,
+  envRequirementSchema,
+  envRequirementsSchema,
+  type CatalogConfig,
+  type CatalogMCPServerItem,
+  type EnvRequirement,
+  type EnvRequirements,
+} from "@mcpx/shared-model";
