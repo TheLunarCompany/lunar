d9eb7d08d2a2c78d1bec795b0d342aa626495d57
tamarp-lunar <tamarp@lunar.dev>
2026-03-29 16:44:18 +0300

    Tamarp.rnd 235/env vars admin config issues (#2764)
    * disabled pre-filled values overwrite while editing, pre-filled value updated only upon saving
    
    * added guard for circle sync
    
    * made secrets initiate in password mode
    
    * cleaning up and prettier:fix
    
    * made onSync a ref so it wont trigger the useEffct again when updating the json
    
    * replced empty string with explicit null before moving server to websocket
    
    * added another ref, now in catalogItemDialog to break the circular refs
    
    * cleaning comments
    
    * remove comment
    
    * added envreq to is dirty depedndencies

---
+++ proxy/src/services/lunar-engine/metrics/remaining_connections_metric.go
@@ -0,0 +1,72 @@
+package metrics
+
+import (
+	"context"
+	"fmt"
+	"lunar/toolkit-core/haproxy"
+
+	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+)
+
+const haproxySocketPath = "/var/run/haproxy/haproxy.sock"
+
+type remainingConnectionsMetricManager struct {
+	haproxy    *haproxy.Stats
+	observer   metric.Int64ObservableGauge
+	attributes []attribute.KeyValue
+}
+
+func newRemainingConnectionsMetricManager(
+	meter metric.Meter,
+) (*remainingConnectionsMetricManager, error) {
+	mng := &remainingConnectionsMetricManager{
+		attributes: []attribute.KeyValue{},
+	}
+
+	if err := mng.init(meter); err != nil {
+		return nil, fmt.Errorf("failed to initialize remaining connections metric manager: %w", err)
+	}
+
+	log.Debug().Msg("Remaining connections metric manager created successfully")
+	return mng, nil
+}
+
+func (m *remainingConnectionsMetricManager) remainingConnectionsCallback(
+	_ context.Context,
+	observer metric.Int64Observer,
+) error {
+	remainingConns, err := m.haproxy.QueryRemainingConnections()
+	if err != nil {
+		return err
+	}
+
+	observer.Observe(
+		int64(remainingConns),
+		metric.WithAttributes(m.attributes...),
+	)
+
+	return nil
+}
+
+func (m *remainingConnectionsMetricManager) init(meter metric.Meter) error {
+	haproxyClient, err := haproxy.NewHAProxyStatsClient(haproxySocketPath)
+	if err != nil {
+		return fmt.Errorf("failed to create HAProxy stats client: %w", err)
+	}
+
+	m.haproxy = haproxyClient
+	m.attributes = appendGatewayIDAttribute(m.attributes...)
+	m.observer, err = meter.Int64ObservableGauge(
+		MetricPrefix+string(RemainingConnectionsMetric),
+		metric.WithDescription("The number of remaining connections in the pool"),
+		metric.WithInt64Callback(m.remainingConnectionsCallback),
+	)
+	if err != nil {
+		return err
+	}
+
+	log.Debug().Msg("Remaining connections metric manager initialized successfully")
+	return nil
+}
