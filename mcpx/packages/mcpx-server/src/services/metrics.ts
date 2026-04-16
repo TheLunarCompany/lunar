20c548699baf1d6d454e46199c4cefebf70bee75
eitangol <108608289+eitangol@users.noreply.github.com>
2026-04-16 14:32:53 +0300

    Replace user deletion with suspension for IDP-deprovisioned users (#2821)
    * Revert user delete and change to suspended
    
    * Fix
    
    * fix
    
    * Move the suspended filter to the DAL
    
    * Update user.ts
    
    * CR Fixes
    
    * Create down.sql
    
    * Update user.ts

---
+++ mcpx/packages/mcpx-server/src/services/metrics.ts
@@ -0,0 +1,31 @@
+import { Histogram } from "@opentelemetry/api";
+import { MeterProvider } from "@opentelemetry/sdk-metrics";
+import { METER_NAME } from "../server/prometheus.js";
+export class MetricRecorder {
+  private toolCallDurationHistogram: Histogram;
+
+  constructor(
+    meterProvider: MeterProvider,
+    private readonly getUserName: () => string | undefined,
+  ) {
+    const meter = meterProvider.getMeter(METER_NAME);
+    this.toolCallDurationHistogram = meter.createHistogram(
+      "tool_call_duration_ms",
+      {
+        description: "Duration of tool calls in ms",
+        unit: "ms",
+      },
+    );
+  }
+
+  public recordToolCallDuration(
+    durationMs: number,
+    labels: Record<string, string>,
+  ): void {
+    const userName = this.getUserName();
+    this.toolCallDurationHistogram.record(durationMs, {
+      ...labels,
+      ...(userName ? { user_name: userName } : {}),
+    });
+  }
+}
