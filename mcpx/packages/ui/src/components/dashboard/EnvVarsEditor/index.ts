5b92ebeb1ef525ea8bc41cabbcb945ed1814ece8
Eliav Lavi <113017999+EliavLunar@users.noreply.github.com>
2026-04-24 15:01:36 +0200

    fix (#2846)

---
+++ mcpx/packages/ui/src/components/dashboard/EnvVarsEditor/index.ts
@@ -0,0 +1,8 @@
+export { EnvVarsEditor } from "./EnvVarsEditor";
+export {
+  FixedInput,
+  FromEnvInput,
+  FromSecretInput,
+  LiteralInput,
+} from "./inputs";
+export type { UserEnvVarsEditorProps as EnvVarsEditorProps } from "./types";
