b1fb1575ccbbbe510c4c6e8067e41546c0d51d01
tamarp-lunar <tamarp@lunar.dev>
2026-05-07 11:35:44 +0300

    Release workflows notifications fix and update (#2903)
    * fixed missing commit info
    
    * added commit message and date to mcpx workflow
    
    * edited text
    
    * added message and date to mcpx-webapp workflow

---
+++ mcpx/packages/toolkit-core/src/config/errors.ts
@@ -0,0 +1,40 @@
+export class ConfigConsumerError extends Error {}
+export class ConfigConsumerAlreadyRegisteredError extends ConfigConsumerError {
+  constructor(consumerName: string) {
+    super(`Config consumer "${consumerName}" is already registered.`);
+    this.name = "ConfigConsumerAlreadyRegisteredError";
+  }
+}
+export class ConfigUpdateRejectedError extends ConfigConsumerError {
+  readonly failedUpdates: { consumerName: string; error: Error }[];
+
+  constructor(failedUpdates: { consumerName: string; error: Error }[]) {
+    const aggErrors = failedUpdates
+      .map((f) => `${f.consumerName}: ${f.error.message}`)
+      .join(", ");
+    super(`Failed to update config for consumers [${aggErrors}]`);
+
+    this.failedUpdates = failedUpdates;
+    this.name = "ConfigUpdateRejectedError";
+  }
+}
+
+export class ConfigFailedToCommitError extends ConfigConsumerError {
+  readonly consumerName: string;
+  readonly error: Error;
+  constructor(consumerName: string, error: Error) {
+    super(`Failed to commit config for consumer: ${consumerName}`);
+    this.consumerName = consumerName;
+    this.error = error;
+    this.name = "ConfigFailedToCommitError";
+  }
+}
+
+export class ConfigInTransitError extends ConfigConsumerError {
+  constructor() {
+    super(
+      "Config is currently being updated, please wait for the update to complete.",
+    );
+    this.name = "ConfigInTransitError";
+  }
+}
