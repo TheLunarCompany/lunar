50064e396435a98fac630f694edee009937b340d
denysn-spec <denysn@lunar.dev>
2026-05-19 15:09:01 +0200

    RND-545 [POV][Open Issues] General fixes (#2936)
    * fix animation duration
    
    * fix animation duration
    
    * fix animation duration
    
    ---------
    
    Co-authored-by: Denys Netosin <46863519+DenysNeto@users.noreply.github.com>

---
+++ mcpx/packages/mcpx-server/src/internal-tools/index.ts
@@ -0,0 +1,14 @@
+export {
+  DynamicCapabilitiesService,
+  INTERNAL_SERVICE_NAME,
+  InternalToolName,
+  type InternalToolNameType,
+} from "./dynamic-capabilities.js";
+
+export {
+  createLLMService,
+  type LLMService,
+  type LLMServiceFactoryDeps,
+  type AvailableToolInfo,
+  type MatchedTool,
+} from "./llm-service.js";
